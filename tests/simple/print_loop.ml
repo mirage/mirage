5911b5663c32022d08773e14fa3d9ee4eae74827
Anil Madhavapeddy <anil@recoil.org>
2009-12-09 02:06:26 +0000

    test function which triggers a linear page fault in xen

--- tests/simple/print_loop.ml
+++ tests/simple/print_loop.ml
@@ -1,10 +1,16 @@
 open Pervasives
 
-let main arg =
-  for i = 0 to 1000000 do
-    let _ = String.create 10000 in
-    if i mod 10000 = 0 then print_endline (string_of_int i)
-  done;
-  print_endline "game over"
+let tm = Sys.time
 
-let _ = Callback.register "main" main
+let main () =
+  let t1 = tm () in
+  for i = 0 to 10000000 do
+    let _ = String.create 10000 in
+    if i mod 50000 = 0 then
+      Printf.printf "%d\n%!" i
+  done;
+  let t2 = tm () in
+  let t = int_of_float ( (t2 -. t1) *. 100. ) in
+  Printf.printf "game over: %d\n%!" t
+
+let _ = main ()

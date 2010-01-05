e3724b5a664078f6790e39745a4968a843d8a978
Anil Madhavapeddy <anil@recoil.org>
2010-01-05 15:15:18 +0000

    [kernel] ifdefed out test hook for sqlite3 block layer

--- runtime/xen/stubdom/caml/main-caml.c
+++ runtime/xen/stubdom/caml/main-caml.c
@@ -16,6 +16,7 @@ FILE *_stderr asm("stderr");
 int *__errno_location;
 /* Will probably break everything, probably need to fetch from glibc */
 void *__ctype_b_loc;
+void sqlite3_test(void);
 
 int main(int argc, char *argv[], char *envp[])
 {
@@ -29,9 +30,12 @@ int main(int argc, char *argv[], char *envp[])
 
     /* Wait before things might hang up */
     sleep(1);
-
+#if 0
+    sqlite3_test();
+#else
     caml_startup(argv);
     printf("caml_startup is done\n");
+#endif
 #if 0
     val = caml_named_value("main");
     if (!val) {

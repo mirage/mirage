b979164b7211635dbcd35a18384455a87b4738e7
Anil Madhavapeddy <anil@recoil.org>
2010-01-09 18:22:19 +0000

    [tests] make the sqlite_stress more useful as a benchmark, by doing combinations of insert/update/delete with different sized iterations, and structure the output so it can be fed to ploticus

--- tests/sqlite_stress/test.ml
+++ tests/sqlite_stress/test.ml
@@ -1,38 +1,44 @@
 open Sqlite3
 open Printf
 
-let iters = 4000
-
 let cmd ?(cb=(fun _ _ -> printf "???\n%!")) db sql =
   match exec db sql ~cb with
   | Rc.OK -> ()
   | _ -> failwith (sql ^ " : failed")
 
-let with_time fn =
+let with_time i label fn =
   let t1 = Mir.gettimeofday () in
   fn ();
   let t2 = Mir.gettimeofday () in
-  printf "time: %f\n%!" (t2 -. t1)
+  printf "%s:%d:%s:%f\n%!" Sys.os_type i label (t2 -. t1)
 
-let _ =
-  let db = db_open "test" in
-  cmd db "CREATE TABLE IF NOT EXISTS foo (a TEXT, b INTEGER, c FLOAT)";
-  cmd db "DELETE FROM foo";
-  with_time (fun () ->
-    for i = 1 to iters do
-      cmd db (sprintf "INSERT INTO foo VALUES(\"hello%d\", %d+1, %d.%d)" i i i i)
-    done
-  );
+let with_trans db fn =
+  cmd db "begin";
+  fn db;
+  cmd db "end"
+
+(*let with_trans db fn = fn db *)
+
+let delete_all db =
+  cmd db "DELETE from foo"
+
+let insert_mass iters db =
+  for i = 1 to iters do
+    cmd db (sprintf "INSERT INTO foo VALUES(\"hello%d\", %d+1, %d.%d)" i i i i)
+  done
+
+let select_check sz db =
   let cnt = ref 0 in
-  let cb row header = incr cnt in
+  let cb _ _ = incr cnt in
   cmd ~cb db "SELECT * from foo";
-  assert(!cnt = iters);
-  printf "count=%d\n%!" !cnt;
-  with_time (fun () ->
-    for i = 1 to iters do 
-      cmd db (sprintf "UPDATE foo SET b=%d WHERE a=\"hello%d\"" (i+1000) i)
-    done
-  );
+  if !cnt != sz then failwith (sprintf "cnt %d != sz %d" !cnt sz)
+
+let update_mass iters db =
+  for i = 1 to iters do 
+    cmd db (sprintf "UPDATE foo SET b=%d WHERE a=\"hello%d\"" (i+1000) i)
+  done
+
+let check_update db =
   let cnt = ref 0 in
   let cb row header = 
     incr cnt;
@@ -40,5 +46,25 @@ let _ =
     let y = int_of_string (gr 1) in
     assert(sprintf "hello%d" (y-1000) = (gr 0))
   in
-  cmd ~cb db "SELECT a,b from foo order by b";
-  printf "count postupdate=%d\n%!" !cnt
+  cmd ~cb db "SELECT a,b from foo order by b"
+
+let ins_up_del iters db =
+  select_check 0 db;
+  insert_mass iters db;
+  select_check iters db;
+  update_mass iters db;
+  delete_all db;
+  select_check 0 db
+
+let sets = [ 1; 10; 50; 100; 200; 500; 1000; 2500; 5000; 7500 ]
+let _ =
+  try
+    let db = db_open "test" in
+    cmd db "CREATE TABLE IF NOT EXISTS foo (a TEXT, b INTEGER, c FLOAT)";
+    delete_all db;
+    List.iter (fun i ->   
+      with_time i "trans" (fun () -> with_trans db (ins_up_del i));
+      with_time i "notrans" (fun () -> ins_up_del i db);
+    ) sets
+  with e -> (print_endline (Printexc.to_string e); raise e)
+

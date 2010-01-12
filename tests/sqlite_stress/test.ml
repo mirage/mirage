257f6f27f2b481a416009379591a54371380ef8f
Anil Madhavapeddy <anil@recoil.org>
2010-01-12 14:06:51 +0000

    [tests] update the sqlite stress with a variety of sizes as well as iterations

--- tests/sqlite_stress/test.ml
+++ tests/sqlite_stress/test.ml
@@ -1,6 +1,8 @@
 open Sqlite3
 open Printf
 
+let sizes = [ 4; 256; 1024; 2048 ]
+
 let cmd ?(cb=(fun _ _ -> printf "???\n%!")) db sql =
   match exec db sql ~cb with
   | Rc.OK -> ()
@@ -27,6 +29,11 @@ let insert_mass iters db =
     cmd db (sprintf "INSERT INTO foo VALUES(\"hello%d\", %d+1, %d.%d)" i i i i)
   done
 
+let insert_large_mass sz iters db =
+  for i = 1 to iters do
+    cmd db (sprintf "INSERT INTO foo VALUES(\"%s%d\", %d+1, %d.%d)" (String.make sz 'x') i i i i);
+  done
+
 let select_check sz db =
   let cnt = ref 0 in
   let cb _ _ = incr cnt in
@@ -35,7 +42,7 @@ let select_check sz db =
 
 let update_mass iters db =
   for i = 1 to iters do 
-    cmd db (sprintf "UPDATE foo SET b=%d WHERE a=\"hello%d\"" (i+1000) i)
+    cmd db (sprintf "UPDATE foo SET b=%d WHERE c=%d.%d" (i+1000) i i)
   done
 
 let check_update db =
@@ -48,13 +55,14 @@ let check_update db =
   in
   cmd ~cb db "SELECT a,b from foo order by b"
 
-let ins_up_del iters db =
+let ins_up_del insertfn iters db =
   select_check 0 db;
-  insert_mass iters db;
+  insertfn iters db;
   select_check iters db;
   update_mass iters db;
   delete_all db;
-  select_check 0 db
+  select_check 0 db;
+  ignore(Gc.stat ())
 
 let sets = [ 1; 10; 50; 100; 200; 500; 1000; 2500; 5000; 7500 ]
 let _ =
@@ -63,8 +71,8 @@ let _ =
     cmd db "CREATE TABLE IF NOT EXISTS foo (a TEXT, b INTEGER, c FLOAT)";
     delete_all db;
     List.iter (fun i ->   
-      with_time i "trans" (fun () -> with_trans db (ins_up_del i));
-      with_time i "notrans" (fun () -> ins_up_del i db);
+      List.iter (fun sz -> 
+        with_time i (sprintf "%d" sz) 
+          (fun () -> with_trans db (ins_up_del (insert_large_mass sz) i))) sizes
     ) sets
   with e -> (print_endline (Printexc.to_string e); raise e)
-

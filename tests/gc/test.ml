52ff6aaa801efef401d8a346b4e17112ec55186f
Anil Madhavapeddy <anil@recoil.org>
2010-02-01 14:02:50 +0000

    [tests] tweak the GC tests

--- tests/gc/test.ml
+++ tests/gc/test.ml
@@ -7,13 +7,12 @@ let with_time i label fn =
   printf "%s,%d,%d,%.3f\n%!" Sys.os_type i label (t2 -. t1)
 
 let _ =
-  Gc.set { (Gc.get ()) with Gc.verbose = 0x01 };;
-  let sizes = [1000; ] in
+  (* Gc.set { (Gc.get ()) with Gc.verbose = 0x3 };; *)
+  let sizes = [10000; ] in
   List.iter (fun sz ->
     with_time sz sz (fun () -> 
-      for i = 0 to 100000 do
-        let _ = String.create sz in
-        ()
+      for i = 0 to 100000000 do
+        let p = String.create sz in
       done;
     )
   ) sizes

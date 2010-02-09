b786ff23dc9f91e819a46f035095287b2a390006
Anil Madhavapeddy <anil@recoil.org>
2010-02-09 23:06:35 +0000

    [kernel] unifdef lib.h

--- runtime/kernel/include/lib.h
+++ runtime/kernel/include/lib.h
@@ -60,46 +60,15 @@
 #include <xen/xen.h>
 #include <xen/event_channel.h>
 #include "gntmap.h"
-
-#ifdef HAVE_LIBC
+#include <sys/types.h>
 #include <stdio.h>
-#else
-/* printing */
-#define _p(_x) ((void *)(unsigned long)(_x))
-int vsnprintf(char *buf, size_t size, const char *fmt, va_list args);
-int vscnprintf(char *buf, size_t size, const char *fmt, va_list args);
-int snprintf(char * buf, size_t size, const char *fmt, ...);
-int scnprintf(char * buf, size_t size, const char *fmt, ...);
-int vsprintf(char *buf, const char *fmt, va_list args);
-int sprintf(char * buf, const char *fmt, ...);
-int vsscanf(const char * buf, const char * fmt, va_list args);
-int sscanf(const char * buf, const char * fmt, ...);
-#endif
+#include <string.h>
 
 long simple_strtol(const char *cp,char **endp,unsigned int base);
 unsigned long simple_strtoul(const char *cp,char **endp,unsigned int base);
 long long simple_strtoll(const char *cp,char **endp,unsigned int base);
 unsigned long long simple_strtoull(const char *cp,char **endp,unsigned int base);
 
-#ifdef HAVE_LIBC
-#include <string.h>
-#else
-/* string and memory manipulation */
-int    memcmp(const void *cs, const void *ct, size_t count);
-void  *memcpy(void *dest, const void *src, size_t count);
-int    strncmp(const char *cs, const char *ct, size_t count);
-int    strcmp(const char *cs, const char *ct);
-char  *strcpy(char *dest, const char *src);
-char  *strncpy(char *dest, const char *src, size_t count);
-void  *memset(void *s,int c, size_t count);
-size_t strnlen(const char *s, size_t count);
-size_t strlen(const char *s);
-char  *strchr(const char *s, int c);
-char  *strrchr(const char *s, int c);
-char  *strstr(const char *s1, const char *s2);
-char * strcat(char * dest, const char * src);
-char  *strdup(const char *s);
-#endif
 #include <mini-os/console.h>
 
 #define RAND_MIX 2654435769U

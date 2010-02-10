303a427962e15f539cf32613e5a71838dfd4f478
Anil Madhavapeddy <anil@recoil.org>
2010-02-10 11:43:22 +0000

    [kernel] remove some of the newlib ctor/dtor

--- runtime/kernel/main.c
+++ runtime/kernel/main.c
@@ -15,10 +15,6 @@
 #include <events.h>
 
 extern int main(int argc, char *argv[], char *envp[]);
-extern void __libc_init_array(void);
-extern void __libc_fini_array(void);
-extern unsigned long __CTOR_LIST__[];
-extern unsigned long __DTOR_LIST__[];
 
 void _init(void)
 {
@@ -28,52 +24,13 @@ void _fini(void)
 {
 }
 
-extern char __app_bss_start, __app_bss_end;
 static void call_main(void *p)
 {
-    int argc;
-    char **argv;
-    char *envp[] = { NULL };
-    int i;
-
-    /* Let other parts initialize (including console output) before maybe
-     * crashing. */
-    sleep(1);
-
-    sparse((unsigned long) &__app_bss_start, &__app_bss_end - &__app_bss_start);
-#if defined(HAVE_LWIP)
-    // start_networking();
-#endif
-
-    argc = 1;
-
-    argv = alloca((argc + 1) * sizeof(char *));
-    argv[0] = "main";
-    argc = 1;
-
-    argv[argc] = NULL;
-
-    for (i = 0; i < argc; i++)
-	printf("\"%s\" ", argv[i]);
-    printf("\n");
-
-    __libc_init_array();
-    environ = envp;
-    for (i = 1; i <= __CTOR_LIST__[0]; i++)
-        ((void((*)(void)))__CTOR_LIST__[i]) ();
-    tzset();
-
-    exit(main(argc, argv, envp));
+    exit(main(0, NULL, NULL));
 }
 
 void _exit(int ret)
 {
-    int i;
-
-    for (i = 1; i <= __DTOR_LIST__[0]; i++)
-        ((void((*)(void)))__DTOR_LIST__[i]) ();
-    close_all_files();
-    __libc_fini_array();
     printk("main returned %d\n", ret);
 #ifdef HAVE_LWIP
     stop_networking();

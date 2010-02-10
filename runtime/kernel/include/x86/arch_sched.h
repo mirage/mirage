8fac5cb76fd6fa9f11ef9a9102c5d5d8f39283b9
Anil Madhavapeddy <anil@recoil.org>
2010-02-10 09:04:09 +0000

    [kernel] unifdef arch_sched.h

--- runtime/kernel/include/x86/arch_sched.h
+++ runtime/kernel/include/x86/arch_sched.h
@@ -7,11 +7,7 @@
 static inline struct thread* get_current(void)
 {
     struct thread **current;
-#ifdef __i386__    
-    register unsigned long sp asm("esp");
-#else
     register unsigned long sp asm("rsp");
-#endif 
     current = (void *)(unsigned long)(sp & ~(__STACK_SIZE-1));
     return *current;
 }

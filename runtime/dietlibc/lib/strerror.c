85052a738fb86e76fbecd7b6a87bf248a5b27a82
Anil Madhavapeddy <anil@recoil.org>
2010-02-10 12:51:34 +0000

    [dietlibc] stub out sterror

--- runtime/dietlibc/lib/strerror.c
+++ runtime/dietlibc/lib/strerror.c
@@ -7,16 +7,9 @@
 #define __attribute_dontuse__
 #include <errno.h>
 
-extern const char __sys_err_unknown[];
+extern const char  __sys_err_unknown [];
 
 char*strerror(int errnum) {
   register const char*message=__sys_err_unknown;
-
-  if ( (unsigned int)errnum < (unsigned int)__SYS_NERR )
-    message=sys_errlist[errnum];
-#if defined(__mips__)
-  if ( errnum == 1133 )
-    message="Quota exceeded";
-#endif
   return (char*)message;
 }

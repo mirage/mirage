16f3bd5a626135da31acf394301fef9fd7fe9d4d
Anil Madhavapeddy <anil@recoil.org>
2010-02-12 16:09:22 +0000

    [build] fixup build Makefile gunk (WIP)

--- mk/xen.mk
+++ mk/xen.mk
@@ -1,19 +1,11 @@
 # before including this, define ROOTDIR to the repository root
 include $(ROOTDIR)/mk/base.mk
 
-CROSS_PREFIX = $(ROOTDIR)/runtime/xen/stubdom/cross-root-x86_64/x86_64-xen-elf/include
 GCC_INSTALL = $(shell LANG=C $(CC) -print-search-dirs | sed -n -e 's/install: \(.*\)/\1/p')
 CFLAGS = -U __linux__ -U __FreeBSD__ -U __sun__
 CFLAGS += $(DEBUG_CFLAGS)
 CFLAGS += -D__MiniOS__ -DHAVE_LIBC -D__x86_64__
 CFLAGS += -nostdinc
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/extras/mini-os/include/posix
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/extras/mini-os/include
-CFLAGS += -isystem $(CROSS_PREFIX) 
-CFLAGS += -isystem $(GCC_INSTALL)/include
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/stubdom/lwip/src/include
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/stubdom/lwip/src/include/ipv4
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/extras/mini-os/include/x86
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/extras/mini-os/include/x86/x86_64
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/xen/include
 CFLAGS += -fno-stack-protector
+CFLAGS += -isystem $(GCC_INSTALL)/include
+CFLAGS += -isystem $(ROOTDIR)/runtime/include

4eb27fab8686113f9adf64b9265f377a0aee20a4
Anil Madhavapeddy <anil@recoil.org>
2010-06-28 17:34:33 +0100

    add Random module, with a seed init stub to fill in later

---
+++ runtime/bindings/random.c
@@ -0,0 +1,9 @@
+/* XXX static random seed for OCaml random, to replace when minios settles down */
+
+#include <mlvalues.h>
+
+CAMLprim value caml_sys_random_seed(value n)
+{
+   intnat seed = 1;
+   return Val_long(seed);
+}

f069ccab7ae96a688dede7833cace281488bcdb4
Anil Madhavapeddy <anil@recoil.org>
2010-07-14 23:04:36 +0100

    add some adapted bindings from xen-api-libs.hg for xenbus/xs_ring

---
+++ runtime/bindings/xb_stubs.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2006-2009 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ */
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <errno.h>
+
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+#include <caml/alloc.h>
+#include <caml/custom.h>
+#include <caml/fail.h>
+#include <caml/callback.h>
+
+#define __XEN_TOOLS__
+
+#define u32 uint32_t
+#include <xen/io/xs_wire.h>
+
+#if !HAVE_DECL_XS_RESTRICT
+#define XS_RESTRICT 128
+#endif
+
+CAMLprim value stub_get_internal_offset(void)
+{
+	CAMLparam0();
+	CAMLreturn(Val_int(XS_RESTRICT));
+}
+
+CAMLprim value stub_header_size(void)
+{
+	CAMLparam0();
+	CAMLreturn(Val_int(sizeof(struct xsd_sockmsg)));
+}
+
+CAMLprim value stub_header_of_string(value s)
+{
+	CAMLparam1(s);
+	CAMLlocal1(ret);
+	struct xsd_sockmsg *hdr;
+
+	if (caml_string_length(s) != sizeof(struct xsd_sockmsg))
+		caml_failwith("xb header incomplete");
+	ret = caml_alloc_tuple(4);
+	hdr = (struct xsd_sockmsg *) String_val(s);
+	Store_field(ret, 0, Val_int(hdr->tx_id));
+	Store_field(ret, 1, Val_int(hdr->req_id));
+	Store_field(ret, 2, Val_int(hdr->type));
+	Store_field(ret, 3, Val_int(hdr->len));
+	CAMLreturn(ret);
+}
+
+CAMLprim value stub_string_of_header(value tid, value rid, value ty, value len)
+{
+	CAMLparam4(tid, rid, ty, len);
+	CAMLlocal1(ret);
+	struct xsd_sockmsg xsd = {
+		.type = Int_val(ty),
+		.tx_id = Int_val(tid),
+		.req_id = Int_val(rid),
+		.len = Int_val(len),
+	};
+
+	ret = caml_alloc_string(sizeof(struct xsd_sockmsg));
+	memcpy(String_val(ret), &xsd, sizeof(struct xsd_sockmsg));
+
+	CAMLreturn(ret);
+}
---
+++ runtime/bindings/xs_ring_stubs.c
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2006-2009 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <os.h>
+
+#define __XEN_TOOLS__
+
+//#include <xenctrl.h>
+#define u32 uint32_t
+#include <xen/io/xs_wire.h>
+#include <xen/io/ring.h>
+
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+#include <caml/alloc.h>
+#include <caml/custom.h>
+#include <caml/fail.h>
+#include <caml/callback.h>
+
+#include "mmap_stubs.h"
+
+#define GET_C_STRUCT(a) ((struct mmap_interface *) a)
+
+static int xs_ring_read(struct mmap_interface *interface,
+                             char *buffer, int len)
+{
+	struct xenstore_domain_interface *intf = (void *)interface->addr;
+	XENSTORE_RING_IDX cons, prod;
+	int to_read;
+
+	cons = intf->req_cons;
+	prod = intf->req_prod;
+	mb();
+	if (prod == cons)
+		return 0;
+        printk("xs_ring_read: start\n");
+	if (MASK_XENSTORE_IDX(prod) > MASK_XENSTORE_IDX(cons)) 
+		to_read = prod - cons;
+	else
+		to_read = XENSTORE_RING_SIZE - MASK_XENSTORE_IDX(cons);
+	if (to_read < len)
+		len = to_read;
+	memcpy(buffer, intf->req + MASK_XENSTORE_IDX(cons), len);
+	mb();
+	intf->req_cons += len;
+        printk("xs_ring_read: done %d\n", len);
+	return len;
+}
+
+static int xs_ring_write(struct mmap_interface *interface,
+                              char *buffer, int len)
+{
+	struct xenstore_domain_interface *intf = interface->addr;
+	XENSTORE_RING_IDX cons, prod;
+	int can_write;
+
+	cons = intf->rsp_cons;
+	prod = intf->rsp_prod;
+	mb();
+	if ( (prod - cons) >= XENSTORE_RING_SIZE )
+		return 0;
+        printk("xs_ring_write\n");
+	if (MASK_XENSTORE_IDX(prod) >= MASK_XENSTORE_IDX(cons))
+		can_write = XENSTORE_RING_SIZE - MASK_XENSTORE_IDX(prod);
+	else 
+		can_write = MASK_XENSTORE_IDX(cons) - MASK_XENSTORE_IDX(prod);
+	if (can_write < len)
+		len = can_write;
+	memcpy(intf->rsp + MASK_XENSTORE_IDX(prod), buffer, len);
+	mb();
+	intf->rsp_prod += len;
+        printk("xs_ring_write: done\n");
+	return len;
+}
+
+CAMLprim value ml_interface_read(value interface, value buffer, value len)
+{
+	CAMLparam3(interface, buffer, len);
+	CAMLlocal1(result);
+	int res;
+
+	res = xs_ring_read(GET_C_STRUCT(interface),
+	                   String_val(buffer), Int_val(len));
+	if (res == -1)
+		caml_failwith("huh");
+	result = Val_int(res);
+	CAMLreturn(result);
+}
+
+CAMLprim value ml_interface_write(value interface, value buffer, value len)
+{
+	CAMLparam3(interface, buffer, len);
+	CAMLlocal1(result);
+	int res;
+
+	res = xs_ring_write(GET_C_STRUCT(interface),
+	                    String_val(buffer), Int_val(len));
+	result = Val_int(res);
+	CAMLreturn(result);
+}

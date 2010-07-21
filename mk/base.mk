d966ea9b94fd75aebd6335010b31b7a57c004f70
Anil Madhavapeddy <anil@recoil.org>
2010-07-21 16:53:10 +0100

    do not omit the frame pointer, it makes it easier to debug rather than the .eh_frame ELF stuff

--- mk/base.mk
+++ mk/base.mk
@@ -5,5 +5,5 @@ OS = $(shell uname -s | tr '[A-Z]' '[a-z]' | sed -e 's/darwin/macosx/g')
 ifeq ($(debug),y)
 DEBUG_CFLAGS = -O1 -fno-omit-frame-pointer
 else
-DEBUG_CFLAGS = -O3 -fomit-frame-pointer
+DEBUG_CFLAGS = -O3 -fno-omit-frame-pointer
 endif

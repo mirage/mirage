b63df9d3d730508c4ef8d9d00f57f63ffcff4eb0
Anil Madhavapeddy <anil@recoil.org>
2010-07-27 18:33:08 +0100

    tweak test, no need to init gnttab anymore as its done in the ocaml ctor, and sleep for a while

--- tests/netfront/main.ml
+++ tests/netfront/main.ml
@@ -2,9 +2,8 @@ open Lwt
 
 let main () =
     let xsh = Xs.make_mmap () in
-    Gnttab.init ();
     lwt vifs = Netfront.enumerate xsh in
     lwt nfs = Lwt_list.map_s (Netfront.create xsh) vifs in
-    return ()
+    Lwt_mirage.sleep 20.
 
 let _ = Lwt_mirage_main.run (main ())

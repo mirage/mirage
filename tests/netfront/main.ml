98a9cc0825bfd8bce2821c5a11c97c672503c5ea
Anil Madhavapeddy <anil@recoil.org>
2010-07-30 16:00:44 +0100

    update netfront test

--- tests/netfront/main.ml
+++ tests/netfront/main.ml
@@ -8,7 +8,7 @@ let main () =
        fun nid ->
          lwt nf = Netfront.create xsh nid in
          Netfront.set_recv nf (fun buf -> 
-            Ethernet.recv nf buf >>
+            Ethernet.Frame.recv nf buf >>
             Lwt_mirage.sleep 2. >>
             return (printf "%s: slept\n%!" (Netfront.mac nf))
          );

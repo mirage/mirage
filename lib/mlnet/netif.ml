32c6d7f06447f9599cf351d77ff894372ed5eed8
Anil Madhavapeddy <anil@recoil.org>
2010-07-31 23:14:25 +0100

    parse the xenstore pretty-printed mac address into a typed version in Netif

--- lib/mlnet/netif.ml
+++ lib/mlnet/netif.ml
@@ -8,9 +8,14 @@ let default_netif = ref None
 (* all registered interfaces (both active and inactive) *)
 let all_netif = ref []
 
+exception Invalid_ethernet_mac of string
+
 let create ?(default=true) ?(up=true) ~ip ~netmask ~gw vif_id  =
     lwt nf = Netfront.create vif_id in
-    let t = { Mlnet_types.nf=nf; up=up; ip=ip; netmask=netmask; gw=gw } in
+    lwt mac = match MT.ethernet_mac_of_string (Netfront.mac nf) with
+        | Some mac -> return mac
+        | None -> fail (Invalid_ethernet_mac (Netfront.mac nf)) in
+    let t = { Mlnet_types.nf=nf; up=up; ip=ip; netmask=netmask; gw=gw; mac=mac } in
     if default then default_netif := Some t;
     all_netif := t :: !all_netif;
     Netfront.set_recv nf (Frame.recv t);

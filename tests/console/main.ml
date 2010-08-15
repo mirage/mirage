c6a83b60905702a11b3e5c25f442bb257751ffcc
Anil Madhavapeddy <anil@recoil.org>
2010-08-16 00:28:53 +0100

    pedant: \r\n in console output

--- tests/console/main.ml
+++ tests/console/main.ml
@@ -3,7 +3,7 @@ open Xen
 
 let main () =
    let cons = Console.t in
-   let s = "abcdefghi\n" in
+   let s = "abcdefghi\r\n" in
    Lwt_mirage.sleep 4. >>
    Console.sync_write cons s 0 (String.length s) >>
    Lwt_mirage.sleep 4. >>

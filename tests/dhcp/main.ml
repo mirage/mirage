10a7f44afd10c5eba1e1335887662c7c3d33b42d
Anil Madhavapeddy <anil@recoil.org>
2010-08-20 14:09:16 +0100

    fix typo in test

--- tests/dhcp/main.ml
+++ tests/dhcp/main.ml
@@ -5,7 +5,7 @@ open Xen
 let main () =
     lwt vifs = Netfront.enumerate () in
     Lwt_list.iter_s (fun vif_id ->
-       lwt (netif,recv_t) = Mlnet.Netif.create_dhcp vif_id in
+       Mlnet.Netif.create_dhcp vif_id >>
        Time.sleep 20.
     ) vifs
 

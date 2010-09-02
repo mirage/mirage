a7b0dbc70f384182df188213cfd938ac6d27612a
Anil Madhavapeddy <anil@recoil.org>
2010-09-02 13:56:01 +0100

    move around Makefiles to support x86_32 Linux better

--- mk/base.mk
+++ mk/base.mk
@@ -2,8 +2,9 @@ debug ?= n
 CC ?= gcc
 
 OS = $(shell uname -s | tr '[A-Z]' '[a-z]' | sed -e 's/darwin/macosx/g')
+ARCH = $(shell uname -m)
 ifeq ($(debug),y)
 DEBUG_CFLAGS = -O1 -fno-omit-frame-pointer
 else
-DEBUG_CFLAGS = -O3 -fno-omit-frame-pointer
+DEBUG_CFLAGS = -O3
 endif

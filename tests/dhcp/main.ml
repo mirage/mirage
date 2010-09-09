b125fb5db4c7ab92a04fd77af2c9d15e41886b2a
Anil Madhavapeddy <anil@recoil.org>
2010-09-09 17:55:45 +0100

    let tests/dhcp compile again

--- tests/dhcp/main.ml
+++ tests/dhcp/main.ml
@@ -1,22 +1,22 @@
 open Lwt 
 open Printf
 
-module OS = Xen
+module OS = Unix
 module Eth = Mlnet.Ethif.Ethernet(OS.Ethif)
 module Arp = Mlnet.Arp.ARP(Eth)
 module IPv4 = Mlnet.Ipv4.IPv4(Eth)(Arp)
 module ICMP = Mlnet.Icmp.ICMP(IPv4)
 module UDP = Mlnet.Udp.UDP(IPv4)
-module DHCP = Mlnet.Dhcp.Client(IPv4)(UDP)
+module DHCP = Mlnet.Dhcp.Client(IPv4)(UDP)(OS.Time)
 
 let main () =
     lwt vifs = Eth.enumerate () in
     let vif_t = List.map (fun id ->
        lwt (ip,thread) = IPv4.create id in
-       let icmp = ICMP.create ip in
-       let udp = UDP.create ip in
+       let icmp,icmp_t = ICMP.create ip in
+       let udp,udp_t = UDP.create ip in
        lwt () = OS.Time.sleep 5. in
-       lwt dhcp = DHCP.create ip udp in
+       lwt _ = DHCP.create ip udp in
        thread
     ) vifs in
     pick (OS.Time.sleep 120. :: vif_t) >>

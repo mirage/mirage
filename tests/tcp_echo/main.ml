f9e1dec3673c0daf342932d01026583af1dbba8d
Anil Madhavapeddy <anil@recoil.org>
2010-09-09 13:40:41 +0100

    update for mlnet interface changes

--- tests/tcp_echo/main.ml
+++ tests/tcp_echo/main.ml
@@ -1,6 +1,3 @@
-open Lwt 
-open Printf
-
 module OS = Unix
 module Eth = Mlnet.Ethif.Ethernet(OS.Ethif)
 module Arp = Mlnet.Arp.ARP(Eth)
@@ -8,23 +5,25 @@ module IPv4 = Mlnet.Ipv4.IPv4(Eth)(Arp)
 module ICMP = Mlnet.Icmp.ICMP(IPv4)
 module UDP = Mlnet.Udp.UDP(IPv4)
 module TCP = Mlnet.Tcp.TCP(IPv4)
-module DHCP = Mlnet.Dhcp.Client(IPv4)(UDP)
+module DHCP = Mlnet.Dhcp.Client(IPv4)(UDP)(OS.Time)
+
+open Lwt 
+open Printf
 
 let main () =
     lwt vifs = Eth.enumerate () in
     let vif_t = List.map (fun id ->
-       lwt (ip,thread) = IPv4.create id in
-       let icmp = ICMP.create ip in
-       let udp = UDP.create ip in
-       lwt () = OS.Time.sleep 5. in
-       lwt dhcp = DHCP.create ip udp in
-       let tcp = TCP.create ip in
+       lwt (ip,ip_t) = IPv4.create id in
+       let (icmp,icmp_t) = ICMP.create ip in
+       let (udp,udp_t) = UDP.create ip in
+       lwt (dhcp,dhcp_t) = DHCP.create ip udp in
+       let (tcp,tcp_t) = TCP.create ip in
        TCP.listen tcp 23 (fun ip tcp ->
          print_endline "TCP: listen 23 input received";
          Mpl.Tcp.prettyprint tcp;
          return ()
        );
-       thread
+       join [ ip_t; icmp_t; udp_t; dhcp_t; tcp_t ]
     ) vifs in
     pick (OS.Time.sleep 120. :: vif_t) >>
     return (printf "success\n%!")

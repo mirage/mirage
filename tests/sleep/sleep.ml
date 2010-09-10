b373d0bb0c179356ade874183d15ea5cc0e9e79b
Anil Madhavapeddy <anil@recoil.org>
2010-09-10 19:44:04 +0100

    update sleep test with Browser signature

--- tests/sleep/sleep.ml
+++ tests/sleep/sleep.ml
@@ -1,5 +1,5 @@
+module OS = Browser
+
 let _ =
-  let t1 = Mir.gettimeofday () in
-  Mir.sleep 10;
-  let t2 = Mir.gettimeofday () in
-  Printf.printf "slept: %.3fs\n%!" (t2 -. t1)
+  let x = "foo bar" in
+  OS.Console.(write t x 0 (String.length x))

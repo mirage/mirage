468e4d4595c2dda1189c09ac3c08ac5c5520c05a
Anil Madhavapeddy <anil@recoil.org>
2010-09-11 21:13:31 +0100

    syntax fixes to javascript runtimes

--- runtime/browser/clock_stubs.js
+++ runtime/browser/clock_stubs.js
@@ -1,8 +1,8 @@
 function unix_gettimeofday() {
-		var t = new Date();
-		return t.getTime() / 1000;
+  var t = new Date();
+  return t.getTime() / 1000;
 }
 
 function unix_block_domain(t) {
-	console.log("unix_block_domain: " + t);
+  console.log("unix_block_domain: " + t);
 }
--- runtime/browser/console_stubs.js
+++ runtime/browser/console_stubs.js
@@ -1,21 +1,18 @@
-// Need to have a node whose ID is console
+// Need to have a node whose ID is console_window to hold all the consoles
 function console_create() {
-		var new_console = dom.createElement('div');
-		var console = dom.getElementById("console");
-		if (console) {
-				console.appendChild(new_console);
-		} else {
-				console = dom.createElement('div');
-				console.id = "console";
-		};
-		return new_console;
+    var new_console = document.createElement('div');
+    var con = document.getElementById("console_window");
+    if (con) {
+        con.appendChild(new_console);
+    } else {
+        con = document.createElement('div');
+        con.id = "console_1";
+    };
+    return new_console;
 }
 
-function console_write(console, data, off, len) {
-		text = data.substring(off, off+len);
-		text.replace(/\n/gi, "<br/>\n");
-		console.innerHTML += text;
-		console.log(data);
+function console_write(con, data, off, len) {
+    text = data.substring(off, off+len);
+    con.innerHTML += "<pre>"+text+"</pre>";
+    console.log(con.id + ": " + text);
 }
-		
-		

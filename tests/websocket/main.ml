c2921d451f23457c5b2bd0bc69c3986b4759852e
samoht <thomas@gazagnaire.com>
2010-09-14 17:59:42 +0200

    Update tests/websocket to start debugging websocket (which doesn't really work as expected :-)

--- tests/websocket/main.ml
+++ tests/websocket/main.ml
@@ -18,21 +18,25 @@
   Need a websocket server to make this test works.
   A nice one is available at:
   http://github.com/miksago/node-websocket-server
+
+  once node.js and node-websocket-server installed, on an other terminal write:
+  sudo node examples/echo-server.js
 *)
 
 module OS = Browser
 
 open Lwt
 
-let nb_cons = 10
-let ws_server = "ws://localhost:8080"
+let nb_cons = 1
+let ws_server = "ws://localhost:8000"
 
 type connection = {
   mutable con : OS.Websocket.t option;
+  mutable id : string;
   mutable messages : int;
 }
 
-let empty_connection _ = { con = None; messages = 0 }
+let empty_connection _ = { con = None; id = "<empty>"; messages = 0 }
 
 let cons = Array.init nb_cons empty_connection
 
@@ -40,8 +44,14 @@ let log = OS.Console.printf
 
 let init () =
   for_lwt i = 0 to nb_cons - 1 do
+    (* This will block until the connection is up *)
     lwt con = OS.Websocket.create ws_server i in
-    return (cons.(i).con <- Some con)
+    (* When the connection is up, the echo server should send back the connection ID *)
+    lwt id  = OS.Websocket.read con in
+    log "[%i] %s" i id;
+    cons.(i).con <- Some con;
+    cons.(i).id  <- id;
+    return ()
   done
 
 let random_string () =
@@ -56,7 +66,7 @@ let random_write i id =
     | Some con ->
       let str = random_string () in
       cons.(i).messages <- cons.(i).messages + 1;
-      log "[%d|%s] Sending %d bytes: <%s>" i id (String.length str) str;
+      log "[%d|%s] Sending %d bytes: %s" i id (String.length str) str;
       OS.Websocket.write con str
     | None     ->
       log "[%d|%s] WebSocket not initialized" i id
@@ -67,27 +77,27 @@ let random_read i id =
       log "[%i|%s] Reading ..." i id;
       lwt str = OS.Websocket.read con in
       cons.(i).messages <- cons.(i).messages - 1;
-      log "[%i|%s] Read %d bytes: <%s>" i id (String.length str) str;
+      log "[%i|%s] Read %d bytes: %s" i id (String.length str) str;
       return ()
     | None    ->
       log "[%d|%s] WebSocket not initialized" i id;
       return ()
 
-let rec iter id n =
-  let i = Random.int nb_cons in
-  log "[%d|%s] iter %n" i id n;
-  if n <= 0 then
-    return ()
-  else
-    if cons.(i).messages > 0 && Random.int 2 = 0 then
-      ( random_read i id >> iter id (n-1) )
+let iter id n =
+  for_lwt j = 0 to n do
+    OS.Time.sleep 1. >>
+    let i = Random.int nb_cons in
+    if cons.(i).messages > 0 && Random.int 5 <> 0 then
+      random_read i id
     else
-      ( random_write i id; iter id (n-1) )
+      return (random_write i id)
+  done
+   
 
 let main () =
   try_lwt 
     init () >>
-    join [iter "one" 10; iter "two" 15; iter "three" 10; iter "four" 15] >>
+    join [iter "one" 10] >> (*; iter "two" 15; iter "three" 10; iter "four" 15] >>*)
     return ()
   with OS.Websocket.Not_supported ->
     return (OS.Console.printf "[ERROR] Websockets are not supported by this browser")

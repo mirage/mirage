ee3b052a086569d202344077be187b40ea84f673
Anil Madhavapeddy <anil@recoil.org>
2010-09-22 11:02:29 +0100

    update unix_tcp test to listen on 8081

--- tests/unix_tcp/main.ml
+++ tests/unix_tcp/main.ml
@@ -5,17 +5,25 @@ open Lwt
 open Mlnet.Types
 
 let main () =
-  let sa = Channel.TCP (ipv4_localhost, 8080) in
-  lwt ic,oc = IO.open_connection sa in
-  try_lwt
-  for_lwt i = 0 to 10 do
-    IO.write_line oc ("foo bar " ^ (string_of_int i)) >>
-    (print_endline "wrote"; return ())
-  done >>
- ( print_endline "about to sleep";
-  OS.Time.sleep 120.)
-  with Lwt.Canceled ->
-    print_endline "cancelled!"; return ()
+(*
+  let connect_thread =
+    let sa = Channel.TCP (ipv4_localhost, 8080) in
+    lwt ic,oc = IO.open_connection sa in
+    for_lwt i = 0 to 10 do
+     IO.write_line oc ("foo bar " ^ (string_of_int i)) >>
+     (print_endline "wrote"; return ())
+    done 
+  in
+*)
+  let lsa = Channel.TCP (ipv4_localhost, 8081) in
+  let listen_thread = Channel.listen (fun c ->
+    print_endline "new connection";
+    OS.Time.sleep 2. >>
+    let msg = "byebye" in
+    lwt _ = Channel.write c msg 0 (String.length msg) in
+    return ()
+  ) lsa in
+  join [ listen_thread ]
 
 let _ = OS.Main.run (main ())
 

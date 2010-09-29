e751d5e09f818fdf9d451646690c0db11b8d2192
Anil Madhavapeddy <anil@recoil.org>
2010-09-29 11:23:11 -0400

    add an mlcrunch utility which converts a directory of files into an ML "filesystem" module that serves them directly out of RAM

---
+++ tools/crunch/custom_unix_stubs.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2009 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <sys/param.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+#include <caml/fail.h>
+#include <caml/alloc.h>
+#include <caml/custom.h>
+#include <caml/signals.h>
+#include <caml/unixsupport.h>
+
+CAMLprim value unix_realpath(value path)
+{
+  char buffer[PATH_MAX];
+  char *r;
+  r = realpath(String_val(path), buffer);
+  if (r == NULL) uerror("realpath", path);
+  return copy_string(buffer);
+}
+
+

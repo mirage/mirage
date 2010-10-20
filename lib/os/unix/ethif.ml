cd9490da36bdb8c5d60ab6ca24365838cd5de98a
Anil Madhavapeddy <anil@recoil.org>
2010-10-20 04:52:30 -0700

    UNIX: in the tap interface, handle non-blocking IO and add a permanent packet listener loop

--- lib/os/unix/ethif.ml
+++ lib/os/unix/ethif.ml
@@ -28,6 +28,7 @@ type t = {
   id: string;
   dev: Tap.t;
   rx_cond: unit Lwt_condition.t;
+  mutable active: bool;
   mac: string;
 }
 
@@ -48,27 +49,54 @@ let generate_local_mac () =
   x.[5] <- i ();
   x
 
-let create id =
+let create (id:id) =
   let dev = Tap.opendev id in
   let rx_cond = Lwt_condition.create () in
-  let cb = fun _ -> Lwt_condition.signal rx_cond () in
-  Activations.register ~rx:true ~tx:false dev cb;
   let mac = generate_local_mac () in
-  return { id; dev; rx_cond; mac }
+  let active = true in
+  let t = { id; dev; rx_cond; active; mac } in
+  let cb = fun mask ->
+    Lwt_condition.signal rx_cond ();
+    t.active
+  in
+  Activations.register ~rx:true ~tx:false dev cb;
+  return t
 
-(* Input all available pages from receive ring and return detached page list *)
-let input t =
+(* Input a frame, and block if nothing is available *)
+let rec input t =
   let sub = Hw_page.alloc_sub () in
   let len = Tap.read t.dev sub.Hw_page.page 0 4096 in
-  let sub = { sub with Hw_page.len=len } in
-  [ sub ]
+  match len with
+  | (-1) -> (* EAGAIN or EWOULDBLOCK *)
+      Lwt_condition.wait t.rx_cond >>
+      input t
+  | 0 -> (* EOF *)
+      t.active <- false;
+      Lwt_condition.wait t.rx_cond >>
+      input t
+  | n ->
+      let sub = { sub with Hw_page.len=len } in
+      return sub
+
+(* Loop and listen for packets permanently *)
+let rec listen t fn =
+  match t.active with
+  |true ->
+    lwt frame = input t in
+    let ft = fn frame in
+    listen t fn <&> ft
+  |false ->
+    return ()
 
 (* Shutdown a netfront *)
 let destroy nf =
   printf "tap_destroy\n%!";
   return ()
 
-(* Transmit a packet from a Hw_page, with offset and length *)  
+(* Transmit a packet from a Hw_page, with offset and length 
+   For now, just assume the Tap write wont block for long as this
+   is not a performance-critical backend
+*)
 let output t sub =
   Hw_page.(Tap.write t.dev sub.page sub.off sub.len);
   return ()  
@@ -82,4 +110,3 @@ let wait nf =
 
 let mac t =
   t.mac
-

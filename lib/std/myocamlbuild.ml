344e6e7701668cc568db3d39d8aca60f77848053
samoht <thomas@gazagnaire.com>
2010-11-25 16:08:52 +0100

    Simplify myocamlbuild.ml, still a warning, don't know how to get ride of it yet ...

--- lib/std/myocamlbuild.ml
+++ lib/std/myocamlbuild.ml
@@ -4,10 +4,6 @@ open Command
 let _ = dispatch begin function
   | After_rules ->
 
-    (* use camlp4 for syntax *)
-    flag ["ocaml"; "compile" ; "use_camlp4"] & S[ A"-pp"; A"camlp4o"];
-    flag ["ocaml"; "ocamldep"; "use_camlp4"] & S[ A"-pp"; A"camlp4o"];
-
     (* do not compile and pack with the standard lib *)
     flag ["ocaml"; "compile"; "nostdlib"] & S [A"-I"; A"lib/"; A"-nostdlib"];
     flag ["ocaml"; "pack"   ; "nostdlib"] & S [A"-I"; A"lib/"; A"-nostdlib"];

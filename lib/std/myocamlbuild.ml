36485b715546d36f8b75a3d92f87b2ca9332c5ac
Anil Madhavapeddy <anil@recoil.org>
2010-11-29 17:01:18 +0000

    fix ocamlbuild comment

--- lib/std/myocamlbuild.ml
+++ lib/std/myocamlbuild.ml
@@ -8,7 +8,7 @@ let _ = dispatch begin function
     flag ["ocaml"; "compile"; "nostdlib"] & S [A"-I"; A"lib/"; A"-nostdlib"];
     flag ["ocaml"; "pack"   ; "nostdlib"] & S [A"-I"; A"lib/"; A"-nostdlib"];
 
-    (* use pa_mirage syntax extension *)
+    (* use pa_lwt syntax extension if the _tags file specifies it *)
     flag ["ocaml"; "compile" ; "pa_lwt"] & S[A"-pp"; A"camlp4o -I syntax pa_lwt.cmo"];
     flag ["ocaml"; "ocamldep"; "pa_lwt"] & S[A"-pp"; A"camlp4o -I syntax pa_lwt.cmo"];
 

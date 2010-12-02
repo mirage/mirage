41589d5ad90bd0f82b8c5d7c108359a4b3337674
Anil Madhavapeddy <anil@recoil.org>
2010-12-02 00:07:21 +0000

    let xen runtime library build again through makefile (not ocamlbuild)

--- lib/os/runtime_xen/xen.mk
+++ lib/os/runtime_xen/xen.mk
@@ -1,6 +1,3 @@
-# before including this, define ROOTDIR to the repository root
-include $(ROOTDIR)/mk/base.mk
-
 GCC_INSTALL = $(shell LANG=C $(CC) -print-search-dirs | sed -n -e 's/install: \(.*\)/\1/p')
 CFLAGS = -U __linux__ -U __FreeBSD__ -U __sun__
 CFLAGS += $(DEBUG_CFLAGS)
@@ -8,6 +5,6 @@ CFLAGS += -D__MiniOS__ -DHAVE_LIBC -D__x86_64__
 CFLAGS += -nostdinc -std=gnu99
 CFLAGS += -fno-stack-protector
 CFLAGS += -isystem $(GCC_INSTALL)/include
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/include
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/include/mini-os
-CFLAGS += -isystem $(ROOTDIR)/runtime/xen/include/mini-os/x86
+CFLAGS += -isystem $(ROOTDIR)/include
+CFLAGS += -isystem $(ROOTDIR)/include/mini-os
+CFLAGS += -isystem $(ROOTDIR)/include/mini-os/x86
--- mk/macosx.mk
+++
@@ -1,4 +0,0 @@
-# before including this, define ROOTDIR to the repository root
-include $(ROOTDIR)/mk/base.mk
-
-CFLAGS += $(DEBUG_CFLAGS)

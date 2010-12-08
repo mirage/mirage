fdc8862d07b3cd6b0b68cee22451a772f1c929ec
samoht <thomas@gazagnaire.com>
2010-12-08 18:43:54 +0100

    Merge with https://github.com/barko/ocaml-cohttp/commit/923169f445a21cc632e39531099a6f4585fd1095

--- lib/net/http/client.ml
+++ lib/net/http/client.ml
@@ -49,33 +49,39 @@ let parse_url url =
       (sprintf "Can't parse url: %s (exception: %s)"
         url (Printexc.to_string exc))
 
-let build_req_string headers meth address path body =
-  let content_type = ("Content-Type", "application/x-www-form-urlencoded") in
-  let content_length s = ("Content-Length", string_of_int (String.length s)) in
-  let body, headers = match body with
-    | None -> "", headers
-    | Some s -> s, content_type::(content_length s)::headers in
-  let headers = ("Host", address)::headers in
+let content_length_header s = 
+  "Content-Length", s 
+
+let default_content_type_h =
+  "Content-Type", "application/x-www-form-urlencoded"
+
+let build_req_header headers meth address path =
+  let content_length_h s = content_length_header (string_of_int (String.length s)) in
+	let headers = default_content_type_h :: content_length_h :: headers in
   let hdrcnt = List.length headers in
   let add_header ht (n, v) = (Hashtbl.replace ht n v; ht) in
   let hdrht = List.fold_left add_header (Hashtbl.create hdrcnt) headers in
   let serialize_header name value prev =
     sprintf "%s\r\n%s: %s" prev name value in
   let hdrst = Hashtbl.fold serialize_header hdrht "" in
-    sprintf "%s %s HTTP/1.0%s\r\n\r\n%s" meth path hdrst body
+  sprintf "%s %s HTTP/1.0%s\r\n\r\n" meth path hdrst
 
 let request outchan headers meth body (address, _, path) =
   let headers = match headers with None -> [] | Some hs -> hs in
-    Flow.write_all outchan (build_req_string headers meth address path body)
+  let req_header = build_req_header headers meth address path in
+  lwt () = Flow.write_all outchan req_header in
+  lwt () = match body with
+    | None   -> return ()
+    | Some s -> Flow.write_all outchan s
 
 let read inchan =
   lwt (_, status) = Http_parser.parse_response_fst_line inchan in
   lwt headers = Http_parser.parse_headers inchan in
   let headers = List.map (fun (h, v) -> (String.lowercase h, v)) headers in
   lwt resp = Flow.read_all inchan in
-    match code_of_status status with
-      | 200 -> return (headers, resp)
-      | code -> fail (Http_error (code, headers, resp))
+  match code_of_status status with
+    | 200 -> return (headers, resp)
+    | code -> fail (Http_error (code, headers, resp))
 
 let connect (address, port, _) iofn =
   lwt sockaddr = Http_misc.build_sockaddr (address, port) in
@@ -89,21 +95,21 @@ let call headers kind body url =
     | `DELETE -> "DELETE" 
     | `POST -> "POST" in
   let endp = parse_url url in
-    try_lwt connect endp
-      (fun t ->
-	 (try_lwt request t headers meth body endp
-	  with exn -> fail (Tcp_error (Write, exn))
-	 ) >> (try_lwt read t
-	       with
-		 | (Http_error _) as e -> fail e
-		 | exn -> fail (Tcp_error (Read, exn))
-	      ))
-    with
-      | (Tcp_error _ | Http_error _) as e -> fail e
-      | exn -> fail (Tcp_error (Connect, exn))
+  try_lwt connect endp
+    (fun t ->
+	    (try_lwt request t headers meth body endp
+	     with exn -> fail (Tcp_error (Write, exn))
+	    ) >>
+      (try_lwt read t
+	     with
+		     | (Http_error _) as e -> fail e
+		     | exn                 -> fail (Tcp_error (Read, exn))))
+  with
+    | (Tcp_error _ | Http_error _) as e -> fail e
+    | exn                               -> fail (Tcp_error (Connect, exn))
 
-let head ?headers url = call headers `HEAD None url
-let get  ?headers url = call headers `GET None url
-let post ?headers ?body url = call headers `POST body url
-let put  ?headers ?body url = call headers `PUT body url
-let delete  ?headers url = call headers `DELETE None url
+let head   ?headers       url = call headers `HEAD   None url
+let get    ?headers       url = call headers `GET    None url
+let post   ?headers ?body url = call headers `POST   body url
+let put    ?headers ?body url = call headers `PUT    body url
+let delete ?headers       url = call headers `DELETE None url
--- lib/net/http/client.mli
+++ lib/net/http/client.mli
@@ -27,45 +27,58 @@
 
 type headers = (string * string) list
 
+type request_body = [ 
+| `None 
+| `String of string 
+| `InChannel of (int * Lwt_io.input_channel) 
+  (* a channel from which to fill the request's body, and its length,
+     which must be known in advance *)
+]
+
 type tcp_error_source = Connect | Read | Write
 exception Tcp_error of tcp_error_source * exn
 exception Http_error of (int * headers * string)  (* code, headers, body *)
 
-  (**
-     @param headers optional overriding headers
-     @param url an HTTP url
-     @return HTTP GET response's body
-     @raise Http_error when response code <> 200 *)
+(**
+   @param headers optional overriding headers
+   @param url an HTTP url
+   @return HTTP GET response's body
+   @raise Http_error when response code <> 200 
+*)
 val get : ?headers:headers -> string -> (headers * string) Lwt.t
 
-  (**
-     @param headers optional overriding headers
-     @param url an HTTP url
-     @return HTTP HEAD raw response
-     @raise Http_error when response code <> 200 *)
+(**
+   @param headers optional overriding headers
+   @param url an HTTP url
+   @return HTTP HEAD raw response
+   @raise Http_error when response code <> 200 
+*)
 val head : ?headers:headers -> string -> (headers * string) Lwt.t
 
-  (**
-     @param headers optional overriding headers
-     @param body optional message
-     @param url an HTTP url
-     @return HTTP POST raw response
-     @raise Http_error when response code <> 200 *)
-val post : ?headers:headers -> ?body:string -> string -> (headers * string) Lwt.t
+(**
+   @param headers optional overriding headers
+   @param body optional message, whose source is either a string or an input channel
+   @param url an HTTP url
+   @return HTTP POST raw response
+   @raise Http_error when response code <> 200 
+*)
+val post : ?headers:headers -> ?body:request_body -> string -> (headers * string) Lwt.t
 
-  (**
-     @param headers optional overriding headers
-     @param body optional message
-     @param url an HTTP url
-     @return HTTP PUT raw response
-     @raise Http_error when response code <> 200 *)
-val put : ?headers:headers -> ?body:string -> string -> (headers * string) Lwt.t
+(**
+   @param headers optional overriding headers
+   @param body optional message, whose source is either a string or an input channel
+   @param url an HTTP url
+   @return HTTP PUT raw response
+   @raise Http_error when response code <> 200 
+*)
+val put : ?headers:headers -> ?body:request_body -> string -> (headers * string) Lwt.t
 
-  (**
-     @param headers optional overriding headers
-     @param body optional message
-     @param url an HTTP url
-     @return HTTP DELETE raw response
-     @raise Http_error when response code <> 200 *)
+(**
+   @param headers optional overriding headers
+   @param body optional message
+   @param url an HTTP url
+   @return HTTP DELETE raw response
+   @raise Http_error when response code <> 200 
+*)
 val delete : ?headers:headers -> string -> (headers * string) Lwt.t
 

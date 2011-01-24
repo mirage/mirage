1397bd4d1463108afc9962c666023b6bce31fb63
Anil Madhavapeddy <anil@recoil.org>
2011-01-24 12:06:36 +0000

    tests: update the tcp echo test to the latest

--- tests/net/tcp_echo/main.ml
+++ tests/net/tcp_echo/main.ml
@@ -10,29 +10,37 @@ let nm = match ipv4_addr_of_string "255.255.255.0" with Some x -> x |None -> ass
 let use_dhcp = false
 
 let main () =
-    lwt vifs = OS.Ethif.enumerate () in
-    let vif_t = List.map (fun id ->
-       lwt (ip,ip_t) = Ipv4.create id in
-       let (icmp,icmp_t) = Icmp.create ip in
-       let (udp,udp_t) = Udp.create ip in
-       let ipaddr_t = (match use_dhcp with
-        | true -> 
-            lwt (dhcp,dhcp_t) = Dhcp.Client.create ip udp in
-            dhcp_t
-        | false ->
-            Ipv4.set_netmask ip nm >>
-            Ipv4.set_ip ip ipaddr >>
-            return ()
-       ) in
-       let (tcp,tcp_t) = Tcp.create ip in
-       Tcp.listen tcp 23 (fun ip tcp ->
-         print_endline "TCP: listen 23 input received";
-         Mpl.Tcp.prettyprint tcp;
-         return ()
-       );
-       join [ ip_t; icmp_t; udp_t; ipaddr_t; tcp_t ]
-    ) vifs in
-    pick (OS.Time.sleep 120. :: vif_t) >>
-    return (printf "success\n%!")
+  lwt vifs = OS.Ethif.enumerate () in
+  let vif_t = List.map (fun id ->
+    lwt (ip,ip_t) = Ipv4.create id in
+    let (icmp,icmp_t) = Icmp.create ip in
+    let (udp,udp_t) = Udp.create ip in
+    let ipaddr_t = (match use_dhcp with
+    | true -> 
+      lwt (dhcp,dhcp_t) = Dhcp.Client.create ip udp in
+      dhcp_t
+    | false ->
+      Ipv4.set_netmask ip nm >>
+      Ipv4.set_ip ip ipaddr >>
+      return ()
+    ) in
+    let (tcp,tcp_t) = Tcp.create ip in
+    Tcp.listen tcp 23 (fun pcb ->
+      print_endline "TCP: new connection on port 23";
+      let rec fn () =
+        lwt r = Tcp.read pcb in
+        match r with
+        |None ->
+           print_endline "RX Connection closed";
+           Tcp.close pcb
+        |Some l ->
+          List.iter (fun v -> printf "Recv: %d\n%!" (OS.Istring.View.length v)) l;
+          fn ()
+      in fn ()
+    );
+    join [ ip_t; icmp_t; udp_t; ipaddr_t; tcp_t ]
+  ) vifs in
+  pick (OS.Time.sleep 120. :: vif_t) >>
+  return (printf "success\n%!")
 
 let _ = OS.Main.run (main ())

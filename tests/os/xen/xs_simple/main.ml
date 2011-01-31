91ca0a78a9fe92be507ee71759e77287d86c70ed
Anil Madhavapeddy <anil@recoil.org>
2011-01-31 10:10:29 +0000

    tests: various fixes and add mir files to let them build

--- tests/os/xen/xs_simple/main.ml
+++ tests/os/xen/xs_simple/main.ml
@@ -1,49 +1,50 @@
 open Lwt
 open Printf
 
-let p x = print_endline x; Gc.compact ()
+let p x = OS.Console.log x; Gc.compact ()
 
 let xs_test () =
    p "starting";
-   let xsh = Xs.t in
+   let xsh = OS.Xs.t in
    let try_xs fn a =
-      try_lwt fn a
+      try_lwt
+        fn a
       with 
-      |  Xs_packet.Error e -> return (sprintf "ERROR(%s)" e)
-      |  Xb.Noent -> return "NOENT"
+      |OS.Xs_packet.Error e -> return (sprintf "ERROR(%s)" e)
+      |OS.Xb.Noent -> return "NOENT"
    in
    let try_xs_unit fn a =
-      try_lwt fn a
-      with _ -> return ()
+    try_lwt fn a
+    with _ -> return ()
    in
    p "read /vm";
-   lwt vm = try_xs xsh.Xs.read "vm" in
+   lwt vm = try_xs xsh.OS.Xs.read "vm" in
    p ("/vm: " ^ vm);
 
-   let vif n = try_xs xsh.Xs.read (sprintf "device/vif/%d/backend-id" n) in
+   let vif n = try_xs xsh.OS.Xs.read (sprintf "device/vif/%d/backend-id" n) in
    lwt vif0 = vif 0 in
    p ("vif0: " ^ vif0);
 
-   try_xs_unit (xsh.Xs.write "device/vif/0/foo") "bar" >> 
-   lwt viffoo0 = try_xs xsh.Xs.read "device/vif/0/foo" in
+   try_xs_unit (xsh.OS.Xs.write "device/vif/0/foo") "bar" >> 
+   lwt viffoo0 = try_xs xsh.OS.Xs.read "device/vif/0/foo" in
    p (sprintf "vif0 write: %s" viffoo0);
 
    let watchpath = "device/vif" in
    let timeout = 9.5 in
    p ("sleeping to watch " ^ watchpath ^ " for " ^ (string_of_float timeout));
    try_lwt
-       Xs.monitor_paths xsh [ watchpath, "XXX" ] timeout
+       OS.Xs.monitor_paths xsh [ watchpath, "XXX" ] timeout
            (fun (k,v) ->
                printf "watch callback: [ %s = %s ]\n%!" k v;
                match k with
                | "device/vif/foo" -> true
                | _ -> false)
    with
-       Xs.Timeout -> begin
-           print_endline "timed out";
+       OS.Xs.Timeout -> begin
+           OS.Console.log "timed out";
            return ()
        end
          
 
 let _ =
-   Lwt_mirage.run (xs_test ())
+   OS.Main.run (xs_test ())

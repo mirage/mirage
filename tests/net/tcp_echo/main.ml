1b274f84979892ca8677f4b654c5a8576d5e4fd5
Anil Madhavapeddy <anil@recoil.org>
2011-01-31 10:12:15 +0000

    tests: make the tcp_echo test use the OS console log

--- tests/net/tcp_echo/main.ml
+++ tests/net/tcp_echo/main.ml
@@ -10,7 +10,9 @@ let nm = match ipv4_addr_of_string "255.255.255.0" with Some x -> x |None -> ass
 let use_dhcp = false
 
 let main () =
+  OS.Console.log (sprintf "start");
   lwt vifs = OS.Ethif.enumerate () in
+  OS.Console.log (sprintf "found %d VIFs" (List.length vifs));
   let vif_t = List.map (fun id ->
     lwt (ip,ip_t) = Ipv4.create id in
     let (icmp,icmp_t) = Icmp.create ip in
@@ -26,12 +28,12 @@ let main () =
     ) in
     let (tcp,tcp_t) = Tcp.create ip in
     Tcp.listen tcp 23 (fun pcb ->
-      print_endline "TCP: new connection on port 23";
+      OS.Console.log "TCP: new connection on port 23";
       let rec fn () =
         lwt r = Tcp.read pcb in
         match r with
         |None ->
-          print_endline "MAIN: RX Connection closed"; 
+          OS.Console.log "MAIN: RX Connection closed"; 
           Tcp.close pcb
         |Some v ->
           let len = Int32.of_int (OS.Istring.View.length v) in
@@ -43,6 +45,6 @@ let main () =
     join [ ip_t; icmp_t; udp_t; ipaddr_t; tcp_t ]
   ) vifs in
   pick (OS.Time.sleep 120. :: vif_t) >>
-  return (printf "success\n%!")
+  return (OS.Console.log "success\n%!")
 
 let _ = OS.Main.run (main ())

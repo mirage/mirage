995cd63934cf69d8814183a07aa14b8e997b8eaf
Anil Madhavapeddy <anil@recoil.org>
2011-01-31 10:11:17 +0000

    tests: let the console test loop indefinitely to ensure some load is generated

--- tests/basic/console/main.ml
+++ tests/basic/console/main.ml
@@ -1,15 +1,13 @@
 open Lwt
 open OS
 
-let main () =
-   let cons = Console.t in
-   let s = "abcdefghi\r\n" in
-   Time.sleep 4. >>
-   Console.sync_write cons s 0 (String.length s) >>
-   Time.sleep 4. >>
-   Console.sync_write cons s 0 (String.length s) >>
-   Time.sleep 4. >>
-   Console.sync_write cons s 0 (String.length s) >>
-   return ()
+let num = ref 0
+let cons = Console.t
+
+let rec main () =
+   incr num;
+   let s = (string_of_int !num) ^ "\n" in
+   OS.Console.log_s s >>
+   main ()
 
 let _ = OS.Main.run (main ())

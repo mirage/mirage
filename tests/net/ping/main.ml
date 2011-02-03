b649327cd97c15bd5122c2d8168879ea4f26dcf8
Anil Madhavapeddy <anil@recoil.org>
2011-02-03 14:22:48 +0000

    tests: let the ping test run forever instead of timing out after a few minutes

--- tests/net/ping/main.ml
+++ tests/net/ping/main.ml
@@ -6,17 +6,17 @@ open Mlnet
 
 let ip = match ipv4_addr_of_string "10.0.0.2" with Some x -> x |None -> assert false
 let nm = match ipv4_addr_of_string "255.255.255.0" with Some x -> x |None -> assert false
- 
+
 let main () =
   lwt vifs = OS.Ethif.enumerate () in
   let arp_t = List.map (fun id ->
     lwt (t,thread) = Ipv4.create id in
     Ipv4.set_ip t ip >>
     Ipv4.set_netmask t nm >>
-    let _ = Icmp.create t in
+    let _ = Icmp.create t in 
     thread
   ) vifs in
-  pick (OS.Time.sleep 360. :: arp_t) >>
+  join arp_t >>
   return (printf "success\n%!")
 
 let _ = OS.Main.run (main ())

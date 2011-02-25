d9dc384f61860dc50b187a695373210103c0cdd5
Anil Madhavapeddy <anil@recoil.org>
2011-02-25 16:16:40 +0000

    tests: add a channel echo test

---
+++ tests/net/channel_echo/echo.ml
@@ -0,0 +1,28 @@
+(* Simple echo server that listens on 8081 and sends back everything it reads *)
+
+open Lwt
+open Printf
+open Net
+
+let echo () =
+  lwt mgr,mgr_t = Manager.create () in
+  (* Listen on all interfaces, port 8081 *)
+  let src = (None, 8081) in 
+  Channel.listen mgr (`TCPv4 (src,
+    (fun (remote_addr, remote_port) t ->
+       OS.Console.log (sprintf "Connection from %s:%d"
+         (Nettypes.ipv4_addr_to_string remote_addr) remote_port);
+       let rec echo () =
+         let res = Channel.read_crlf t in
+         lwt ts = OS.Istring.View.ts_of_stream res in
+         let str = OS.Istring.View.ts_to_string ts in
+         printf "read: %s\n%!" str;
+         Channel.write_line t str >>
+         Channel.flush t >>
+         echo ()
+       in
+       echo () 
+    )
+  ))
+
+let _ = OS.Main.run (echo ())

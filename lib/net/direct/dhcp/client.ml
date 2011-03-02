646fc2835cada18ff04394f20fd917a7aaa15611
Anil Madhavapeddy <anil@recoil.org>
2011-03-02 17:00:54 +0000

    DHCP: when running the DHCP client, block the creation thread until the first offer comes through (temporary fix)

--- lib/net/direct/dhcp/client.ml
+++ lib/net/direct/dhcp/client.ml
@@ -39,6 +39,7 @@ type t = {
   udp: Udp.t;
   ip: Ipv4.t;
   mutable state: state;
+  new_offer: offer -> unit Lwt.t;
 }
 
 (* Send a client broadcast packet *)
@@ -67,12 +68,12 @@ let output_broadcast t ~xid ~yiaddr ~siaddr ~options =
   in
   Udp.output t.udp ~dest_ip udpfn
 
-  (* Receive a DHCP UDP packet *)
-  let input t (ip:Mpl.Ipv4.o) (udp:Mpl.Udp.o) =
-    let dhcp = Mpl.Dhcp.unmarshal udp#data_sub_view in 
-    let packet = Option.Packet.of_bytes dhcp#options in
-    (* See what state our Netif is in and if this packet is useful *)
-    Option.Packet.(match t.state with
+(* Receive a DHCP UDP packet *)
+let input t (ip:Mpl.Ipv4.o) (udp:Mpl.Udp.o) =
+  let dhcp = Mpl.Dhcp.unmarshal udp#data_sub_view in 
+  let packet = Option.Packet.of_bytes dhcp#options in
+  (* See what state our Netif is in and if this packet is useful *)
+  Option.Packet.(match t.state with
     | Request_sent xid -> begin
         (* we are expecting an offer *)
         match packet.op, dhcp#xid with 
@@ -93,9 +94,8 @@ let output_broadcast t ~xid ~yiaddr ~siaddr ~options =
                 `Requested_ip ip_addr;
                 `Server_identifier siaddr;
               ] } in
-            output_broadcast t ~xid ~yiaddr ~siaddr ~options >>
-            (t.state <- Offer_accepted offer;
-            return ())
+            t.state <- Offer_accepted offer;
+            output_broadcast t ~xid ~yiaddr ~siaddr ~options
         end
         |_ -> printf "DHCP: offer not for us"; return ()
     end
@@ -110,12 +110,7 @@ let output_broadcast t ~xid ~yiaddr ~siaddr ~options =
             let info = { info with lease=lease } in
             (* TODO also merge in additional requested options here *)
             t.state <- Lease_held info;
-            Ipv4.set_ip t.ip info.ip_addr >>
-            (match info.netmask with 
-             | Some x -> Ipv4.set_netmask t.ip x 
-             | None -> return ()) >>
-            Ipv4.set_gateways t.ip info.gateways >>
-            return ()
+            t.new_offer info
        end
        |_ -> printf "DHCP: ack not for us\n%!"; return ()
     end
@@ -123,46 +118,63 @@ let output_broadcast t ~xid ~yiaddr ~siaddr ~options =
     |_ -> printf "DHCP: unknown DHCP state\n%!"; return ()
   )
  
-  (* Start a DHCP discovery off on an interface *)
-  let start_discovery t =
-    (* XXX TODO this Random needs to be functorized! *)
-    let xid = Random.int32 Int32.max_int in
-    let yiaddr = ipv4_blank in
-    let siaddr = ipv4_blank in
-    let options = { Option.Packet.op=`Discover; opts= [
-       (`Parameter_request [`Subnet_mask; `Router; `DNS_server; `Broadcast]);
-       (`Host_name "miragevm")
-     ] } in
-    output_broadcast t ~xid ~yiaddr ~siaddr ~options >>
-    (t.state <- Request_sent xid;
-    return ())
+(* Start a DHCP discovery off on an interface *)
+let start_discovery t =
+  (* XXX TODO this Random needs to be functorized! *)
+  let xid = Random.int32 Int32.max_int in
+  let yiaddr = ipv4_blank in
+  let siaddr = ipv4_blank in
+  let options = { Option.Packet.op=`Discover; opts= [
+    (`Parameter_request [`Subnet_mask; `Router; `DNS_server; `Broadcast]);
+    (`Host_name "miragevm")
+  ] } in
+  Printf.printf "DHCP: start discovery\n%!";
+  output_broadcast t ~xid ~yiaddr ~siaddr ~options >>
+  return (t.state <- Request_sent xid)
 
-  (* DHCP state thred *)
-  let rec dhcp_thread t =
-    (* For now, just send out regular discoveries until we have a lease *)
-    match t.state with
-    |Disabled ->
-      start_discovery t >>
-      OS.Time.sleep 60. >>
-      dhcp_thread t
-    |Shutting_down ->
-      printf "DHCP thread: done\n%!";
-      return ()
-    |_ -> 
-      (* TODO: This should be looking at the lease time *)
-      OS.Time.sleep 3600. >>
-      dhcp_thread t
+(* DHCP state thred *)
+let rec dhcp_thread t =
+  (* For now, just send out regular discoveries until we have a lease *)
+  match t.state with
+  |Disabled ->
+    start_discovery t >>
+    OS.Time.sleep 60. >>
+    dhcp_thread t
+  |Shutting_down ->
+    printf "DHCP thread: done\n%!";
+    return ()
+  |_ -> 
+    (* TODO: This should be looking at the lease time *)
+    OS.Time.sleep 3600. >>
+    dhcp_thread t
 
 (* Create a DHCP thread *)
 let create ip udp =
   let thread,_ = Lwt.task () in
   let state = Disabled in
-  let t = { ip; udp; state } in
+  (* For now, just block on the first offer
+     and shut down DHCP after. TODO: full protocol *)
+  let first_t, first_u = Lwt.task () in
+  let new_offer info =
+    Printf.printf "DHCP: offer %s %s [%s]\n%!"
+      (ipv4_addr_to_string info.ip_addr)
+      (match info.netmask with |Some ip -> ipv4_addr_to_string ip |None -> "None")
+      (String.concat ", " (List.map ipv4_addr_to_string info.gateways));
+    Ipv4.set_ip ip info.ip_addr >>
+    (match info.netmask with 
+     |Some nm -> Ipv4.set_netmask ip nm
+     |None -> return ()) >>
+    Ipv4.set_gateways ip info.gateways >>
+    return (Lwt.wakeup first_u ())
+  in
+  let t = { ip; udp; state; new_offer } in
   let listen_t = Udp.listen t.udp 68 (input t) in
   Lwt.on_cancel thread (fun () ->
     printf "DHCP: shutting down\n%!";
     t.state <- Shutting_down;
     Lwt.cancel listen_t
   );
-  let thread = join [ dhcp_thread t; thread ] in
-  return (t, thread)
+  let th = dhcp_thread t <?> thread in
+  Printf.printf "DHCP: waiting for first offer\n%!";
+  first_t >>
+  return (t, th)

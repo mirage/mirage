c36627b9ec3b1539ae5249cab3d5157936972b2b
Anil Madhavapeddy <anil@recoil.org>
2011-04-09 16:56:18 -0700

    Build: add support for the UNIX runtime C, as well as symlinking the right platform files in

--- lib/myocamlbuild.ml
+++ lib/myocamlbuild.ml
@@ -7,6 +7,8 @@ open Ocamlbuild_pack.Tools
 let ps = Printf.sprintf
 let ep = Printf.eprintf
 
+let debug = false
+
 (* This decides the global OS backend. It could be moved into explicit
    dependencies in the future, but for now is set as an environment
    variable *)
@@ -61,6 +63,12 @@ module OS = struct
     | "linux"  -> Unix Linux
     | "darwin" -> Unix Darwin
     | os -> Printf.eprintf "`%s` is not a supported host OS\n" os; exit (-1)
+
+  let unix_ext = match host with
+    |Unix Linux -> "linux"
+    |Unix Darwin -> "macosx"
+    | _ -> Printf.eprintf "unix_ext called on a non-UNIX host OS\n"; exit (-1)
+
   let target = match String.lowercase os with
     | "unix" -> host (* Map the target to the current host, as cross-compiling is no use *)
     | "xen"  -> Xen
@@ -87,6 +95,107 @@ module MPL = struct
       ~dep:"%.mpl"
       (mpl_compile "%.mpl" "%.ml")
 end
+
+(* Rules to directly invoke GCC rather than go through OCaml. *)
+module CC = struct
+
+  let cc = getenv "CC" ~default:"cc"
+  let ar = getenv "AR" ~default:"ar"
+  let cflags = ref ["-Wall"; "-g"; "-O3"]
+
+  (* All the xen cflags for compiling against an embedded environment *)
+  let xen_incs =
+    (* base GCC standard include dir *)
+    let gcc_install =
+      let cmd = ps "LANG=C %s -print-search-dirs | sed -n -e 's/install: \\(.*\\)/\\1/p'" cc in
+      Util.run_and_read cmd in
+    (* root dir of xen bits *)
+    let rootdir = ps "os/%s/runtime_xen" Pathname.pwd in
+    let root_incdir = ps "os/%s/include" rootdir in
+    (* Basic cflags *)
+    let all_cflags = List.map (fun x -> A x)
+      [ "-U"; "__linux__"; "-U"; "__FreeBSD__";
+        "-U"; "__sun__"; "-D__MiniOS__";
+        "-D__MiniOS__"; "-D__x86_64__";
+        "-D__XEN_INTERFACE_VERSION__=0x00030205";
+        "-D__INSIDE_MINIOS__";
+        "-nostdinc"; "-std=gnu99"; "-fno-stack-protector";
+        "-m64"; "-mno-red-zone"; "-fno-reorder-blocks";
+        "-fstrict-aliasing"; "-momit-leaf-frame-pointer"; "-mfancy-math-387"
+      ] in
+    (* Include dirs *)
+    let incdirs= A ("-I"^gcc_install^"include") :: List.flatten (
+      List.map (fun x ->[A"-isystem"; A (ps "%s/%s" root_incdir x)])
+        [""; "mini-os"; "mini-os/x86"]) in
+    all_cflags @ incdirs
+
+  (* The private libm include dir *)
+  let libm_incs =
+    [ A (ps "-Ios/%s/runtime_xen/libm" Pathname.pwd) ]
+
+  (* defines used by the ocaml runtime, as well as includes *)
+  let ocaml_debug_inc = if debug then [A "-DDEBUG"] else []
+  let ocaml_incs = [
+    A "-DCAML_NAME_SPACE"; A "-DNATIVE_CODE"; A "-DTARGET_amd64"; A "-DSYS_xen";
+    A (ps "-Ios/%s/runtime_xen/ocaml" Pathname.pwd) ] @ ocaml_debug_inc
+
+  (* dietlibc bits, mostly extra warnings *)
+  let dietlibc_incs = [
+    A "-Wextra"; A "-Wchar-subscripts"; A "-Wmissing-prototypes";
+    A "-Wmissing-declarations"; A "-Wno-switch"; A "-Wno-unused"; A "-Wredundant-decls"; A "-D__dietlibc__";
+    A (ps "-Ios/%s/runtime_xen/dietlibc" Pathname.pwd)
+  ]
+
+  let cc_cflags = List.map (fun x -> A x) !cflags
+
+  let cc_c tags arg out =
+    let tags = tags++"cc"++"c" in
+    Cmd (S (A cc :: [ A"-c"; T(tags++"compile"); A"-I"; Px (Lazy.force stdlib_dir); A"-o"; Px out; P arg]))
+
+  let cc_compile_c_implem ?tag c o env build =
+    let c = env c and o = env o in
+    cc_c (tags_of_pathname c++"implem"+++tag) c o
+
+  let cc_archive clib a path env build =
+    let clib = env clib and a = env a and path = env path in
+    let objs = List.map (fun x -> path / x) (string_list_of_file clib) in
+    let resluts = build (List.map (fun x -> [x]) objs) in
+    let objs = List.map (function
+      | Outcome.Good o -> o
+      | Outcome.Bad exn -> raise exn) resluts in
+    Cmd(S[A ar; A"rc"; Px a; T(tags_of_pathname a++"c"++"archive"); atomize objs])
+
+  let () =
+    rule "cc: .c -> .o include ocaml dir"
+      ~tags:["cc"; "c"]
+      ~prod:"%.o" ~dep:"%.c"
+      (cc_compile_c_implem "%.c" "%.o");
+
+    rule "cc: .S -> .o assembly compile"
+      ~prod:"%.o" ~dep:"%.S"
+      (cc_compile_c_implem ~tag:"asm" "%.S" "%.o");
+
+    rule "cc: _linux.c -> _os.c platform file"
+      ~prod:"%_os.c"
+      ~dep:("%_" ^ OS.unix_ext ^ ".c")
+      (fun env _ ->
+        let filename = env "%" in
+        ln_s (ps "%s_%s.c" (Pathname.basename filename) OS.unix_ext) (ps "%s_os.c" filename);
+      );
+
+    rule "archive: cclib .o -> .a archive"
+      ~prod:"%(path:<**/>)lib%(libname:<*> and not <*.*>).a"
+      ~dep:"%(path)lib%(libname).cclib"
+      (cc_archive "%(path)lib%(libname).cclib" "%(path)lib%(libname).a" "%(path)")
+
+end
+
+(* Need to register manual dependency on libev included files/
+   The C files below are #included, so need to be present but are
+   not picked up by dependency analysis *)
+let libev_files = List.map (fun x -> "os/runtime_unix/" ^ x)
+  ["ev.h"; "ev_vars.h"; "ev_wrap.h"; "ev.c";
+   "ev_select.c"; "ev_epoll.c"; "ev_kqueue.c"; "ev_poll.c"; "ev_port.c"]
 
 let libexts = (match OS.target with |OS.Node ->  "cmo" |OS.Xen |OS.Unix _ -> "cmx") :: ["cmi";"a";"o"]
 let libbits dir name = List.map (fun e -> dir / name ^ "." ^ e) libexts
@@ -128,6 +237,7 @@ let _ = dispatch begin function
 
      dep ["pa_lwt"] ["std/syntax/pa_lwt.cma"];
 
+     (* for building syntax extensions *)
      flag ["ocaml"; "ocamldep"; "camlp4o_syntax"] & camlp4_pp `orig;
      flag ["ocaml"; "compile"; "camlp4o_syntax"] & camlp4_pp `orig;
      flag ["ocaml"; "ocamldep"; "camlp4_syntax"] & camlp4_pp `rev; 
@@ -150,6 +260,23 @@ let _ = dispatch begin function
      Pathname.define_context "net/direct/tcp" ["net/direct/mpl"; "net/direct"];
      Pathname.define_context "net/direct" ["net/direct/mpl"; "net/direct/tcp"; "net/direct/dhcp"];
      Pathname.define_context "net/direct/dhcp" ["net/direct/mpl"; "net/direct" ];
+
+     (* some C code will use local ev.h *)
+     dep  ["c"; "compile"; "include_libev"] libev_files;
+
+     (* unix code deps *)
+     dep ["c"; "compile"; "unix_header"] ["os/runtime_unix/istring.h"];
+
+     (* base cflags for C code *)
+     flag ["c"; "compile"] & S CC.cc_cflags;
+     flag ["asm"; "compile"] & S [A "-D__ASSEMBLY__"];
+
+     (* xen code needs special cflags *)
+     flag ["c"; "compile"; "include_xen"] & S CC.xen_incs;
+     flag ["c"; "compile"; "include_libm"] & S CC.libm_incs;
+     flag ["c"; "compile"; "include_ocaml"] & S CC.ocaml_incs;
+     flag ["c"; "compile"; "include_dietlibc"] & S CC.dietlibc_incs;
+     flag ["c"; "compile"; "pic"] & S [A"-fPIC"];
 
      ()
   | _ -> ()

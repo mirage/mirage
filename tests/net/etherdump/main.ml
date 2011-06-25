0e4718237f57adf453f7c9e8e14b1e8525d7e80b
Anil Madhavapeddy <anil@recoil.org>
2011-06-25 18:27:55 +0100

    [tests] update to etherdump to compile

--- tests/net/etherdump/main.ml
+++ tests/net/etherdump/main.ml
@@ -2,19 +2,15 @@ open Lwt
 open Printf
 open Net.Nettypes
 
-let input_arp frame bits =
-  let open Net.Ethif in
-  let dmac = ethernet_mac_to_string frame.dmac in
-  let smac = ethernet_mac_to_string frame.smac in
-  printf "dmac=%s smac=%s etype=%x\n%!" dmac smac frame.etype;
-  return ()
-  
+let addr = match ipv4_addr_of_string "10.0.0.2" with Some x -> x |None -> assert false 
+
 let main () =
   match_lwt OS.Netif.enumerate () with
   |[id] -> 
      lwt netif = OS.Netif.create id in
      let ethif, ethif_t = Net.Ethif.create netif in
-     Net.Ethif.attach ethif (`ARP input_arp);
+     let ipv4 = Net.Ipv4.create ethif in
+     Net.Ipv4.set_ip ipv4 addr >>
      ethif_t
   |_ -> fail (Failure "only 1 netif supported")
 

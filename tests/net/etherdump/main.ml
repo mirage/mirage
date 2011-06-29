eeadcb6cd0b3a86048d923803daf31fd6a0069b5
Anil Madhavapeddy <anil@recoil.org>
2011-06-29 22:16:00 +0100

    [tests] updates for tcp changes

--- tests/net/etherdump/main.ml
+++ tests/net/etherdump/main.ml
@@ -4,6 +4,10 @@ open Net.Nettypes
 
 let addr = match ipv4_addr_of_string "10.0.0.2" with Some x -> x |None -> assert false 
 
+let listen_fn (src_addr, src_port) pcb =
+  OS.Time.sleep 5. >>
+  return ()
+
 let main () =
   match_lwt OS.Netif.enumerate () with
   |[id] -> 
@@ -12,7 +16,9 @@ let main () =
      let ipv4 = Net.Ipv4.create ethif in
      let icmp = Net.Icmp.create ipv4 in
      Net.Ipv4.set_ip ipv4 addr >>
-     ethif_t
+     let tcp, tcp_t = Net.Tcp.Pcb.create ipv4 in
+     let listen_t = Net.Tcp.Pcb.listen tcp 23 listen_fn in
+     ethif_t 
   |_ -> fail (Failure "only 1 netif supported")
 
 let _ = OS.Main.run (main ())

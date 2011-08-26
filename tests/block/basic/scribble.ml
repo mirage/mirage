6b1d6ade44253b21ee1f60ff85b07f0b7942d80c
David Scott <dave.scott@eu.citrix.com>
2011-08-26 22:15:20 +0100

    Fix up 'scribble' disk I/O test.

--- tests/block/basic/scribble.ml
+++ tests/block/basic/scribble.ml
@@ -1,6 +1,24 @@
 open Lwt
 open Printf
 
+let i = ref 0
+
+(* Copy over this page with grafitti *)
+let scribble_over_string (s, off, length) =
+  let off = off / 8 and length = length / 8 in (* assume byte alignment *)
+  let dstoff = ref 0 in
+  begin
+    try
+      while !dstoff < length do
+        let msg = Printf.sprintf "%d," !i in
+        let len = min (String.length msg) (length - !dstoff) in
+        String.blit msg 0 s (off + !dstoff) len;
+        dstoff := !dstoff + len;
+        incr i;
+      done;
+    with _ -> ()
+  end
+
 let main () =
   lwt ids = OS.Blkif.enumerate () in
   printf "VM has %d block devices configured\n%!" (List.length ids);
@@ -8,19 +26,8 @@ let main () =
   lwt vbd,vbd_t = OS.Blkif.create id in
   printf "Connected block device\n%!";
   lwt bs = OS.Blkif.read_page vbd 0L in
-  let s, _, _ = bs in
   printf "Read page at offset 0\n%!";
-  (* Copy over this page with grafitti *)
-  let msg = "hello there\n" in
-  let dstoff = ref 0 in
-  begin
-    try
-      while true do
-        String.blit msg 0 s !dstoff (String.length msg);
-         dstoff := !dstoff + (String.length msg);
-      done;
-    with _ -> ()
-  end;
+  scribble_over_string bs;
   lwt (_: unit) = OS.Blkif.write_page vbd 0L bs in
   printf "Written page at offset 0\n%!";
   OS.Time.sleep 5.

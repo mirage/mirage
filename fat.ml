8ea869d5df0fb30e41117a916d2294206f0264be
David Scott <dave@recoil.org>
2011-08-29 16:08:34 +0100

    Add the ability to read a regular (not yet extended) boot sector.

--- fat.ml
+++ fat.ml
@@ -1,6 +1,6 @@
 open Printf
 
-let block_size = 1024
+let sector_size = 512
 
 let f = ref Unix.stdin
 
@@ -14,9 +14,9 @@ let rec really_read fd string off n =
     really_read fd string (off+m) (n-m)
 
 let read_block n =
-  Unix.lseek !f (n * block_size) Unix.SEEK_SET;
-  let results = String.create block_size in
-  really_read !f results 0 block_size;
+  Unix.lseek !f (n * sector_size) Unix.SEEK_SET;
+  let results = String.create sector_size in
+  really_read !f results 0 sector_size;
   results
 
 let () =
@@ -34,6 +34,29 @@ let () =
 
   opendevice !fs;  
 
-  (* Load a real fat structure *)
-  let bits = Bitstring.bitstring_of_string (read_block 1) in
-  ()
+  (* Load the boot sector *)
+  let bits = Bitstring.bitstring_of_string (read_block 0) in
+  bitmatch bits with
+  | { _: 24: string; (* JMP instruction *)
+      oem_name: (8 * 8): string;
+      bytes_per_sector: (2 * 8): littleendian;
+	  sectors_per_cluster: (1 * 8): littleendian;
+      reserved_sectors: (2 * 8): littleendian;
+	  number_of_fats: (1 * 8): littleendian;
+	  number_of_root_dir_entries: (2 * 8): littleendian;
+	  total_sectors_small: (2 * 8): littleendian;
+      media_descriptor: (1 * 8): littleendian;
+      sectors_per_fat: (2 * 8): littleendian;
+      sectors_per_track: (2 * 8): littleendian;
+      heads: (2 * 8): littleendian;
+      hidden_preceeding_sectors: (4 * 8): littleendian;
+      total_sectors_large: (4 * 8): littleendian
+    } ->
+      printf "OEM: [%s]\n" oem_name;
+      printf "bytes_per_sector: %d\n" bytes_per_sector;
+      printf "sectors_per_cluster: %d\n" sectors_per_cluster;
+      printf "total_sectors_small: %d\n" total_sectors_small;
+      printf "total_sectors_large: %ld\n" total_sectors_large;
+	  ()
+  | { _ } -> failwith "Failed to read a boot sector"
+

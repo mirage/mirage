14b91b1700961497e26ac44bd842cc4179e331ba
Anil Madhavapeddy <anil@recoil.org>
2011-09-11 20:20:28 +0100

    [block] clean up direct/
    Rename Simple->SimpleKV and remove the now-unnecessary Manager and Types modules

--- lib/block/direct/types.ml
+++
@@ -1,9 +0,0 @@
-module type RO = sig
-  type t
-  exception Error
-  val create : Manager.interface -> t Lwt.t
-  val iter_s : t -> (string -> unit Lwt.t) -> unit Lwt.t
-
-  val size : t -> string -> int64 Lwt.t
-  val read : t -> string -> Bitstring.t Lwt_stream.t Lwt.t
-end

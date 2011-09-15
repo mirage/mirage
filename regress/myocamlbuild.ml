467e550fda01db20fbc12b885378dab065807e61
Richard Mortier <mort@cantab.net>
2011-09-15 17:15:51 +0100

    Revert "[regress] obey name: setting for external target sh script"
    This reverts commit fdb5b5902a869126c16abb9786da2517663ed780.
    
    Having now read log msg of a7677 :)

--- regress/myocamlbuild.ml
+++ regress/myocamlbuild.ml
@@ -300,11 +300,7 @@ module Spec = struct
         let root_target = match spec.target with
           |None -> env "%(test)"
           |Some x -> Pathname.dirname (env "%(test)") / x in
-        let test_sh = Pathname.dirname (env "%(test)") /
-          match spec.target with
-            | None -> env "%(test).sh"
-            | Some t -> t ^ ".sh"
-        in
+        let test_sh = env "%(test).sh" in
         let exec_cmd = match is_supported backend with
         |`Yes ->
           (* Build the target for this backend *)

37e08ba171221111e53df3ccaf474892f7fe2d58
Richard Mortier <mort@cantab.net>
2011-09-23 17:00:39 +0100

    [regress] port channel_echo

--- regress/net/channel_echo.ml
+++ regress/net/channel_echo.ml
@@ -17,12 +17,13 @@
 
 open Lwt 
 open Printf
-open Net
 
 let port = 55555
 
-let rec echo dst chan = 
+let rec echo (dip,dpt) chan = 
   Log.info "Echo" "callback!";
+
+    (* doesn't seem to respect ^D closing the connection
   try_lwt
     lwt buf = Channel.read_crlf chan in
     Channel.write_bitstring chan buf
@@ -30,12 +31,29 @@ let rec echo dst chan =
         Channel.write_char chan '\n'
     )
     >> Channel.flush chan
-    >> echo ()
+    >> echo dst chan
   with Nettypes.Closed -> return (Log.info "Echo" "closed!")
+    *)
+
+  try_lwt
+    lwt more, buf = Net.Channel.read_until chan '\n' in
+    let s = Bitstring.string_of_bitstring buf in
+    
+    Net.Channel.write_string chan (sprintf "%s\n" s)
+    >> Net.Channel.flush chan
+    >> (Log.info "Echo"
+          "rem:%s:%d buf:%s"
+          (Net.Nettypes.ipv4_addr_to_string dip) dpt s;
+      if more then echo (dip,dpt) chan 
+      else
+        return(Log.info "Echo" "closed!")
+    )
+  with Net.Nettypes.Closed -> return (Log.info "Echo" "closed!")
 
 let main () =
   Log.info "Echo" "starting server";
   Net.Manager.create (fun mgr interface id ->
-    Channel.listen mgr (`TCPv4 ((None, port), echo))
-    >> return (Log.info "Echo" "done")
+    (* no need to configure the interface *)
+    Net.Channel.listen mgr (`TCPv4 ((None, port), echo))
+    >> return (Log.info "Echo" "done!")
   )

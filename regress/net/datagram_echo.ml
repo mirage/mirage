c9ac5362d130a6f098941ea13ec5f54c288d38f4
Richard Mortier <mort@cantab.net>
2011-09-24 20:29:59 +0100

    [regress] port flow_udp as datagram_echo

--- regress/net/datagram_echo.ml
+++ regress/net/datagram_echo.ml
@@ -1,28 +1,34 @@
-(* Simple echo server that listens on 8081/UDP
-   and sends back everything it reads after a short delay *)
+(*
+ * Copyright (c) 2011 Richard Mortier <mort@cantab.net>
+ * Derived from code by Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
 
 open Lwt
 open Printf
-open Net
 
-let rec watchdog () =
-  let open Gc in
-  Gc.compact ();
-  let s = stat () in
-  printf "blocks: l=%d f=%d\n%!" s.live_blocks s.free_blocks;
-  OS.Time.sleep 2. >>
-  watchdog ()
+let port = 55555
 
-let echo () =
-  lwt mgr,_ = Manager.create () in
-  (* Listen on all interfaces, port 8081 *)
-  let port = 8081 in
-  let src = (None, port) in 
-  Datagram.UDPv4.recv mgr src
-    (fun (addr,rport) req ->
-(*      let raddr = Nettypes.ipv4_addr_to_string addr in
-      OS.Console.log (sprintf "Msg <- %s:%d" raddr rport);
-      OS.Console.log (sprintf "Msg -> to %s:%d" raddr (port+1)); *)
-      Datagram.UDPv4.send mgr (addr, rport) req
-    )
-let _ = OS.Main.run (echo () <&> (watchdog ()))
+let main () =
+  Log.info "Datagram_echo" "starting server";
+  Net.(Manager.create (fun mgr interface id ->
+    let loc = (None, port) in 
+    Datagram.UDPv4.recv mgr loc
+      (fun (rip,rpt) bits ->
+        let s = Bitstring.string_of_bitstring bits in
+        Log.info "Datagram_echo" "recv:%s:%d buf:\n%s"
+          (Nettypes.ipv4_addr_to_string rip) rpt s;
+        Datagram.UDPv4.send mgr ~src:loc (rip,rpt) bits
+      )
+  ))

d624bb5f259c5bee8c6e5ffe447ce6c8f89d3a59
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-02-02 15:21:11 +0000

    Add header + version number

--- lib/mirari.ml
+++ lib/mirari.ml
@@ -1,4 +1,22 @@
-(* #!/usr/local/bin/ocaml *)
+(*
+ * Copyright (c) 2013 Thomas Gazagnaire <thomas@gazagnaire.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+let version () =
+  Printf.printf "%s\n%!" Path_generated.project_version;
+  exit 0
 
 let conf_file, xen =
   let xen = ref false in
@@ -10,7 +28,8 @@ let conf_file, xen =
       Printf.eprintf "%s\n" usage_msg;
       exit 1 in
   let specs = Arg.align [
-      "--xen", Arg.Set xen, " Generate xen image."
+      "--xen"    , Arg.Set xen     , " Generate xen image.";
+      "--version", Arg.Unit version, " Display version information.";
     ] in
   Arg.parse specs anon_fun usage_msg;
   match !file with

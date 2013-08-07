bd9bb7b122668a5d6d1c67c96297b6f31a94075e
Vincent Bernardoff <vincent.bernardoff@citrix.com>
2013-08-07 12:18:52 +0100

    atomic_stubs.c are now safe.

--- xen/runtime/kernel/atomic_stubs.c
+++ xen/runtime/kernel/atomic_stubs.c
@@ -16,19 +16,30 @@
 
 #include <stdint.h>
 #include <caml/mlvalues.h>
+#include <caml/memory.h>
+#include <caml/fail.h>
 #include <caml/bigarray.h>
 
 CAMLprim value stub_atomic_or_fetch(value buf, value idx, value val)
 {
+  CAMLparam3(buf, idx, val);
   // Finding the address of buf+idx
   char *ptr = Caml_ba_data_val(buf) + idx;
 
-  return Val_int(__sync_or_and_fetch(ptr, Int_val(val)));
+  if (Int_val(idx) >= Caml_ba_array_val(buf)->dim[0])
+    caml_invalid_argument("idx");
+
+  CAMLreturn(Val_int(__sync_or_and_fetch(ptr, Int_val(val))));
 }
 
 CAMLprim value stub_atomic_fetch_and(value buf, value idx, value val)
 {
+  CAMLparam3(buf, idx, val);
+
   char *ptr = Caml_ba_data_val(buf) + idx;
 
-  return Val_int(__sync_fetch_and_and(ptr, Int_val(val)));
+  if (Int_val(idx) >= Caml_ba_array_val(buf)->dim[0])
+    caml_invalid_argument("idx");
+
+  CAMLreturn(Val_int(__sync_fetch_and_and(ptr, Int_val(val))));
 }

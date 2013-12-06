2dd6f9322d322e84111e1364a5f49e640426c583
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-12-06 19:06:07 +0000

    Add a basic test for FAT

---
+++ lib_test/fat/handler.ml
@@ -0,0 +1,12 @@
+open Mirari.V1
+
+open Lwt
+
+module Main (C: CONSOLE) (F: FS) = struct
+
+  let start c fs =
+    F.listdir fs "foo" >>= function
+    | `Ok s    -> C.log_s c (String.concat " " s)
+    | `Error e -> C.log_s c "error"
+
+end

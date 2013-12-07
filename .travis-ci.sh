80818328d9b790cef5a587a45558d65f798b9a98
Anil Madhavapeddy <anil@recoil.org>
2013-12-07 20:42:04 +0000

    Add Travis scripts

---
+++ .travis-ci.sh
@@ -0,0 +1,19 @@
+case "$OCAML_VERSION,$OPAM_VERSION" in
+3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+export OPAMVERBOSE=1
+
+opam init
+opam install ipaddr mirage-types lwt cstruct re
+eval `opam config env`
+make
+make install
+cd lib_test && make

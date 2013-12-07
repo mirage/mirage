031a39e2662e2f921f17d6daf01dd7dd12f5fd3d
Anil Madhavapeddy <anil@recoil.org>
2013-12-07 17:55:08 +0000

    kv_ro: fix test for latest interfaces

--- lib_test/kv_ro/handler.ml
+++ lib_test/kv_ro/handler.ml
@@ -2,7 +2,6 @@ open Mirari.V1
 open Lwt
 
 let string_of_stream s =
-  Lwt_stream.to_list s >>= fun s ->
   let s = List.map Cstruct.to_string s in
   return (String.concat "" s)
 
@@ -10,15 +9,15 @@ module Main (C: CONSOLE) (X: KV_RO) (Y: KV_RO) = struct
 
   let start c x y =
     let rec aux () =
-      X.read x "a" >>= fun vx ->
-      Y.read y "a" >>= fun vy ->
+      X.read x "a" 0 4096 >>= fun vx ->
+      Y.read y "a" 0 4096 >>= fun vy ->
       begin match vx, vy with
       | `Ok sx, `Ok sy ->
         string_of_stream sx >>= fun sx ->
         string_of_stream sy >>= fun sy ->
-        if sx = sy then C.log_s "YES!"
-        else C.log_s "NO!"
-      | _              -> C.log_s "NO! NO!"
+        if sx = sy then C.log_s c "YES!"
+        else C.log_s c "NO!"
+      | _              -> C.log_s c "NO! NO!"
       end >>= fun () ->
       OS.Time.sleep 1. >>= fun () ->
       aux () in

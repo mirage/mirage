9f6792e4079062428a8162de1b1286e2a8e5005d
David Scott <dave.scott@eu.citrix.com>
2013-12-09 12:12:29 +0000

    Add some instructions to the FAT example

--- lib_test/fat/handler.ml
+++ lib_test/fat/handler.ml
@@ -4,7 +4,7 @@ open Lwt
 module Main (C: CONSOLE) (F: FS) = struct
 
   let start c fs =
-    F.listdir fs "foo" >>= function
+    F.listdir fs "/" >>= function
     | `Ok s    -> C.log_s c (String.concat " " s)
     | `Error e -> C.log_s c "error"
 

db8f2433392c2d48bf08231ca7024c262f9b7175
Anil Madhavapeddy <anil@recoil.org>
2013-12-10 20:12:34 +0000

    Travis: fix dependencies

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -12,7 +12,7 @@ export OPAMYES=1
 export OPAMVERBOSE=1
 
 opam init git://github.com/ocaml/opam-repository
-opam install ocamlfind cstruct ounit mirage-types cmdliner ipaddr re lwt
+opam install ocamlfind cstruct ounit mirage-types cmdliner ipaddr re lwt io-page-xen io-page-unix
 eval `opam config env`
 make
 sudo make install
@@ -21,6 +21,6 @@ opam install mirage-block-unix
 opam install mirage-console-unix mirage-console-xen \
     mirage-clock-unix mirage-clock-xen \
     mirage-block-unix mirage-block-xen \
-    fat-filesystem io-page-xen io-page-unix
+    fat-filesystem
 cd lib_test && make MODE=unix
 cd lib_test && make MODE=xen

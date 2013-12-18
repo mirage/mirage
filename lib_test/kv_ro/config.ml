f5995d2c23468cf33f0dbe3d8f22360f5cc95835
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-12-18 17:20:57 +0000

    Fix lib_test/ after the move from KV_RO to Crunch

--- lib_test/kv_ro/config.ml
+++ lib_test/kv_ro/config.ml
@@ -1,11 +1,7 @@
 open Mirage
 
-let kv_ro name dirname =
-  Driver.KV_RO { KV_RO.name; dirname }
-
-let x = kv_ro "x" "t"
-
-let y = kv_ro "y" "t"
+let x = Driver.crunch ~name:"x" "t"
+let y = Driver.crunch ~name:"y" "t"
 
 let () =
   Job.register [

f58386796079eb1dc5b47d9ac488b124b1ae7b76
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-01-08 00:10:59 +0100

    Complexify the HTTP test (to make it break)

--- lib_test/http/config.ml
+++ lib_test/http/config.ml
@@ -2,7 +2,7 @@ open Mirage
 
 let main = foreign "Handler.Main" (console @-> kv_ro @-> http @-> job)
 
-let fs = crunch "../kv_ro/t"
+let fs = kv_ro_of_fs (fat (block_of_file "fat.img"))
 
 let http = http_server 8080 (default_ip [tap0])
 

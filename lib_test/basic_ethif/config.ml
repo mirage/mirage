c8a4a5561de849bc7e3c10f93695e49d90671603
Anil Madhavapeddy <anil@recoil.org>
2014-01-23 15:21:39 +0000

    Adapt Basic_ethif to respond to TCP requests

--- lib_test/basic_ethif/config.ml
+++ lib_test/basic_ethif/config.ml
@@ -3,7 +3,7 @@ open Mirage
 let basic = foreign "Test.Basic" (console @-> network @-> job)
 
 let () =
-  add_to_ocamlfind_libraries ["tcpip.ethif"; "tcpip.udpv4-unix"];
+  add_to_ocamlfind_libraries ["tcpip.ethif"; "tcpip.udpv4-unix"; "tcpip.tcpv4-unix"];
   register "basic_ethif" [
     basic $ default_console $ tap0
   ]
--- lib_test/basic_ethif/test.ml
+++ lib_test/basic_ethif/test.ml
@@ -1,4 +1,4 @@
-open Mirage_types.V1
+open V1_LWT
 open Lwt
 
 let red fmt = Printf.sprintf ("\027[31m"^^fmt^^"\027[m")
@@ -11,6 +11,7 @@ module Basic (C: CONSOLE) (N: NETWORK) = struct
   module E = Ethif.Make(N)
   module I = Ipv4.Make(E)
   module U = Udpv4.Make(I)
+  module T = Tcpv4.Flow.Make(I)(OS.Time)(Clock)(Random)
 
   let start c net =
     E.connect net
@@ -29,17 +30,42 @@ module Basic (C: CONSOLE) (N: NETWORK) = struct
           >>= function
           |`Error _ -> C.log_s c (red "UDPv4 err")
           |`Ok udp ->
-            N.listen net (
-              E.input 
-                ~ipv4:(I.input
-                         ~tcp:(fun ~src ~dst b -> C.log_s c (green "tcp"))
-                         ~udp:(
-                           U.input ~listeners:
-                             (fun ~dst_port ->
-                                C.log c (blue "udp packet on port %d" dst_port);
-                                None)
-                             udp
-                         ) i)
-                ~ipv6:(fun b -> C.log_s c (yellow "ipv6")) e)
+            T.connect i
+            >>= function
+            |`Error _ -> C.log_s c (red "TCPv4 err")
+            |`Ok tcp ->
+              (* Listen on TCP port 80 *)
+              let _ = 
+                T.listen tcp 80
+                  (fun (dst,dst_port) flow ->
+                     C.log_s c (green "new tcp connection from %s %d" (Ipaddr.V4.to_string dst) dst_port)
+                     >>= fun () ->
+                     T.read flow
+                     >>= function
+                     | `Ok b ->
+                          C.log_s c (yellow "read: %d\n%s" (Cstruct.len b) (Cstruct.to_string b))
+                          >>= fun () ->
+                          T.close flow
+                     | `Eof -> C.log_s c (red "read: eof")
+                     | `Error e -> C.log_s c (red "read: error")
+                  )
+              in
+              N.listen net (
+                E.input 
+                  ~ipv4:(
+                    I.input
+                      ~tcp:(fun ~src ~dst b ->
+                          C.log_s c (green "tcp packet")
+                          >>= fun () ->
+                          T.input tcp ~src ~dst b 
+                        )
+                      ~udp:(
+                        U.input ~listeners:
+                          (fun ~dst_port ->
+                             C.log c (blue "udp packet on port %d" dst_port);
+                             None)
+                          udp
+                      ) i)
+                  ~ipv6:(fun b -> C.log_s c (yellow "ipv6")) e)
       end
 end

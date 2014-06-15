ea2b21b3b58037ce0c8bb3a43ab7a7bf2f2d8200
David Scott <dave.scott@citrix.com>
2014-06-15 12:38:08 +0000

    travis: install mirage-types from here, check everything still builds
    Previously we were installing the 1.1.0 release version of mirage-types
    and building other repos against that. It's probably better to install
    this version of mirage-types to detect pull requests which make incompat
    changes.
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -11,12 +11,18 @@ sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 export OPAMVERBOSE=1
 
-opam init git://github.com/mirage/opam-repository#mirage-1.1.0 >/dev/null 2>&1
-opam install cstruct ounit mirage-types cmdliner ipaddr re lwt io-page
-eval `opam config env`
-make
-sudo make install
+# when preparing a set of synchronised updates for a Mirage release:
+#opam init git://github.com/mirage/opam-repository#mirage-1.1.0 >/dev/null 2>&1
+# for regular minor updates:
+opam init >/dev/null 2>&1
 
+opam install cstruct ounit cmdliner ipaddr re lwt io-page
+eval `opam config env`
+opam pin mirage .
+opam pin mirage-types .
+opam install mirage mirage-types
+
+# install users of the signatures, to make sure we haven't broken something obvious
 opam install mirage-console-unix mirage-console-xen \
     mirage-clock-unix mirage-clock-xen \
     mirage-block-unix mirage-block-xen \

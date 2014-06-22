da6ae112d7e630fba968d9fb17c30ce08011f008
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-06-22 15:51:10 +0100

    Fix tests to compile with latest cohttp

--- lib_test/basic_http/test.ml
+++ lib_test/basic_http/test.ml
@@ -14,7 +14,7 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
     let module CH = Channel.Make(T) in
     let module H = HTTP.Make(CH) in
 
-    let http_callback conn_id ?body req =
+    let http_callback conn_id req body =
       let path = Uri.path (H.Server.Request.uri req) in
       C.log_s console (Printf.sprintf "Got a request for %s\n" path) >>= fun () ->
       H.Server.respond_string ~status:`OK ~body:"helllp" ()

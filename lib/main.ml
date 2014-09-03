c51550ecf9aea9ca094e2e29e81ddbeabadaa703
Anil Madhavapeddy <anil@recoil.org>
2014-09-03 16:00:25 +0200

    bump working version to 2.0.0

--- lib/main.ml
+++ lib/main.ml
@@ -163,7 +163,7 @@ let default =
       configure_doc build_doc run_doc clean_doc in
   Term.(pure usage $ pure ()),
   Term.info "mirage"
-    ~version:"1.2.0"
+    ~version:"2.0.0"
     ~sdocs:global_option_section
     ~doc
     ~man

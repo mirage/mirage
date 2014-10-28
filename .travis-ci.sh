b261b8255d0220fc55bb974950a9bbf931f95156
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-10-28 10:40:57 +0000

    Intall libxen-dev in Travis CI scripts

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -10,7 +10,7 @@ esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libxen-dev
 export OPAMYES=1
 
 # when preparing a set of synchronised updates for a Mirage release:

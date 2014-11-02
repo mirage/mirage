110ab670b80d44f6c1112e627c982c717976ea57
Anil Madhavapeddy <anil@recoil.org>
2014-11-02 00:12:48 +0000

    Travis: remove the temporary avsm pin

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -17,7 +17,6 @@ export OPAMYES=1
 # for regular minor updates:
 opam init >/dev/null 2>&1
 opam remote add mirage-dev git://github.com/mirage/mirage-dev
-opam pin add conduit git://github.com/avsm/ocaml-conduit
 opam install cstruct ounit cmdliner ipaddr re lwt io-page xenstore xenstore_transport
 eval `opam config env`
 opam pin add mirage .

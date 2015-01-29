d30e37070fa283fe77847ffe3cc3febdca454983
Anil Madhavapeddy <anil@recoil.org>
2015-01-29 10:44:29 +0000

    Bump version and test OCaml 4.02.1 as well

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -5,6 +5,7 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.2.0) ppa=avsm/ocaml312+opam12 ;;
 4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
 4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
+4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 

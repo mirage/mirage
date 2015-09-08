593acb8e1f3055706808745d2536fd12293126dc
Matt Gray <matt.gray@7digital.com>
2015-09-08 19:51:36 +0100

    Add current_tz_offset_s to CLOCK

--- types/V1.mli
+++ types/V1.mli
@@ -131,6 +131,10 @@ module type CLOCK = sig
       at [d] * 86'400e12 + [ps] POSIX picoseconds from the epoch
       1970-01-01 00:00:00 UTC. [ps] is in the range
       \[[0];[86_399_999_999_999_999L]\]. *)
+
+  val current_tz_offset_s : unit -> int
+  (** [current_tz_offset_s ()] is the clock's current local time zone
+      offset to UTC in seconds. *)
 end
 
 (** {1 Connection between endpoints} *)

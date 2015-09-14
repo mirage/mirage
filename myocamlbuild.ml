903f5c33bd250238c3d0a1f690fb1b29411625f5
Drup <drupyog@zoho.com>
2015-09-14 04:53:19 +0200

    Add an embryo of intro.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,4 +4,15 @@ open Ocamlbuild_plugin
 let () =
   pflag ["ocaml"; "link"] "dontlink" (fun pkg -> S[A"-dontlink"; A pkg]) ;;
 
-dispatch dispatch_default;;
+let doc_intro = "index.text"
+
+let () =
+  dispatch
+    (function hook ->
+      dispatch_default hook ;
+      match hook with
+        | After_rules ->
+            dep ["ocaml"; "doc"; "extension:html"] & [doc_intro] ;
+            flag ["ocaml"; "doc"; "extension:html"] & S[A"-intro"; P doc_intro];
+        | _ -> ()
+    )

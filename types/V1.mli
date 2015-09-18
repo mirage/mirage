8c9c74bf3860d2bcded561964fdf2bf390e0c169
Matt Gray <matt.gray@7digital.com>
2015-09-18 18:56:45 +0100

    Add period_d_ps to CLOCK

--- types/V1.mli
+++ types/V1.mli
@@ -135,6 +135,12 @@ module type CLOCK = sig
   val current_tz_offset_s : unit -> int
   (** [current_tz_offset_s ()] is the clock's current local time zone
       offset to UTC in seconds. *)
+
+  val period_d_ps : unit -> (int * int64) option
+  (** [period_d_ps ()] is if available [Some (d, ps)] representing the
+      clock's picosecond period [d] * 86'400e12 + [ps]. [ps] is in the
+      range \[[0];[86_399_999_999_999_999L]\]. *)
+
 end
 
 (** {1 Connection between endpoints} *)

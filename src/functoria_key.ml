42d98be1b477f0203f5d32ea5e2d061b62061cc3
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-10-12 16:07:52 +0100

    Cleanup Functoria_key API

--- src/functoria_key.ml
+++ src/functoria_key.ml
@@ -15,7 +15,6 @@
  *)
 
 open Functoria_misc
-open Cmdliner
 
 module Emit = struct
   let string fmt s = Format.fprintf fmt "%S" s
@@ -23,55 +22,49 @@ module Emit = struct
   let list x = Fmt.Dump.list x
 end
 
-module Conv = struct
+module Arg = struct
 
-  type 'a t = {
-    description: string;
-    serializer : Format.formatter -> 'a -> unit;
-    converter  : 'a Cmdliner.Arg.converter;
+  type 'a converter = {
+    configure: 'a Cmdliner.Arg.converter;
+    emit     : Format.formatter -> 'a -> unit;
+    runtime  : string;
   }
-
-  let serializer d = d.serializer
-  let description d = d.description
-  let converter d = d.converter
-  let create ~serializer ~converter ~description =
-    { description; serializer; converter }
+  let conv configure emit runtime = { configure; emit; runtime }
+  let conv_at_configure t = t.configure
+  let conv_at_runtime t = t.runtime
+  let conv_emit t = t.emit
 
   let string = {
-    description = "Cmdliner.Arg.string";
-    serializer = (fun fmt -> Format.fprintf fmt "%S");
-    converter = Arg.string;
+    configure = Cmdliner.Arg.string;
+    emit      = (fun fmt -> Format.fprintf fmt "%S");
+    runtime   = "Cmdliner.Arg.string";
   }
 
   let bool = {
-    description = "Cmdliner.Arg.bool";
-    serializer = (fun fmt -> Format.fprintf fmt "%b");
-    converter = Arg.bool;
+    configure  = Cmdliner.Arg.bool;
+    emit       = (fun fmt -> Format.fprintf fmt "%b");
+    runtime    ="Cmdliner.Arg.bool";
   }
 
   let int = {
-    description = "Cmdliner.Arg.int";
-    serializer = (fun fmt -> Format.fprintf fmt "%i");
-    converter = Arg.int;
+    configure = Cmdliner.Arg.int;
+    emit      = (fun fmt -> Format.fprintf fmt "%i");
+    runtime   = "Cmdliner.Arg.int";
   }
 
   let list d = {
-    description = Fmt.strf "(Cmdliner.Arg.list %s)" d.description;
-    serializer = Emit.list d.serializer;
-    converter = Arg.list d.converter;
+    configure = Cmdliner.Arg.list d.configure;
+    emit      = Emit.list d.emit;
+    runtime   = Fmt.strf "(Cmdliner.Arg.list %s)" d.runtime;
   }
 
-  let option d = {
-    description = Fmt.strf "(Cmdliner.Arg.some %s)" d.description;
-    serializer = Emit.option d.serializer;
-    converter = Arg.some d.converter;
+  let some d = {
+    configure = Cmdliner.Arg.some d.configure;
+    emit      = Emit.option d.emit;
+    runtime   = Fmt.strf "(Cmdliner.Arg.some %s)" d.runtime;
   }
 
-end
-
-module Info = struct
-
-  type t = {
+  type info = {
     doc  : string option;
     docs : string;
     docv : string option;
@@ -79,19 +72,19 @@ module Info = struct
     env  : string option;
   }
 
-  let create ?(docs="UNIKERNEL PARAMETERS") ?docv ?doc ?env names =
+  let info ?(docs="UNIKERNEL PARAMETERS") ?docv ?doc ?env names =
     { doc; docs; docv; names; env }
 
-  let to_cmdliner { docs; docv; doc; env; names } =
+  let info_at_configure { docs; docv; doc; env; names } =
     let env = match env with
-      | Some s -> Some (Arg.env_var s)
-      | None -> None
+      | Some s -> Some (Cmdliner.Arg.env_var s)
+      | None   -> None
     in
-    Arg.info ~docs ?docv ?doc ?env names
+    Cmdliner.Arg.info ~docs ?docv ?doc ?env names
 
   let emit_env fmt = Fmt.pf fmt "(Cmdliner.Arg.env_var %a)" Emit.string
 
-  let emit fmt { docs; docv; doc; env; names } =
+  let info_emit fmt { docs; docv; doc; env; names } =
     Format.fprintf fmt
       "(Cmdliner.Arg.info@ ~docs:%a@ ?docv:%a@ ?doc:%a@ ?env:%a@ %a)"
       Emit.string docs
@@ -100,40 +93,35 @@ module Info = struct
       Emit.(option emit_env) env
       Emit.(list string) names
 
+  type 'a t =
+    | Opt : 'a converter -> 'a t
+    | Flag: bool t
+
+  let pp: type a . a t -> a Fmt.t = function
+    | Opt c -> snd c.configure
+    | Flag  -> Fmt.bool
+
+  let at_configure (type a) ~default ~info:i (t: a t) (f :a -> _) =
+    let f_desc v z = match v with
+      | Some v -> f v z
+      | None -> z
+    in
+    match t with
+    | Flag     -> Cmdliner.Term.(app @@ pure f) Cmdliner.Arg.(value @@ flag i)
+    | Opt desc ->
+      let none = Fmt.strf "%a" (snd desc.configure) default in
+      Cmdliner.Term.(app @@ pure f_desc)
+        Cmdliner.Arg.(value @@ opt (some ~none desc.configure) None i)
+
+  let at_runtime: type a . a t -> _ = function
+    | Flag  -> "Functoria_runtime.Conv.flag"
+    | Opt c -> Fmt.strf "(Functoria_runtime.Conv.opt %s)" c.runtime
+
+  let emit: type a . a t -> a Fmt.t = function
+    | Flag  -> Fmt.fmt "%b"
+    | Opt c -> c.emit
+
 end
-
-(* {2 Keys} *)
-
-type 'a opt_or_flag =
-  | Opt : 'a Conv.t -> 'a opt_or_flag
-  | Flag: bool opt_or_flag
-
-let pp : type a . a opt_or_flag -> a Fmt.t = function
-  | Opt c -> snd c.converter
-  | Flag  -> Fmt.bool
-
-let cmdliner (type a) ~default ~info:i (t: a opt_or_flag) (f :a -> _) =
-  let f_desc v z = match v with
-    | Some v -> f v z
-    | None -> z
-  in
-  match t with
-  | Flag -> Term.(app @@ pure f) Arg.(value @@ flag i)
-  | Opt desc ->
-    let none = Fmt.strf "%a" (snd desc.converter) default in
-    Term.(app @@ pure f_desc)
-      Arg.(value @@ opt (some ~none desc.converter) None i)
-
-let runtime_conv s = "" ^ s
-
-let describe: type a . a opt_or_flag -> _ = function
-  | Flag  -> "Functoria_runtime.Conv.flag"
-  | Desc c -> Fmt.strf "(Functoria_runtime.Conv.opt %s)" c.description
-
-  let serialize: type a . a t_or_flag -> a Fmt.t = function
-    | Flag -> Fmt.fmt "%b"
-    | Desc c -> c.serializer
-
 
 type stage = [
   | `Configure
@@ -144,8 +132,8 @@ type stage = [
 type 'a key = {
   name   : string;
   stage  : stage;
-  doc    : Doc.t;
-  conv   : 'a Conv.t_or_flag;
+  info   : Arg.info;
+  arg    : 'a Arg.t;
   default: 'a;
   key    : 'a Univ.key;
   setters: 'a setter list;
@@ -174,34 +162,31 @@ module Set = struct
   let pp = Fmt.iter ~sep:(Fmt.unit ",@ ") iter
 
 end
+
 type t = Set.elt = Any: 'a key -> t
 
 module Setters = struct
   type 'a t = 'a setter list
-
   let empty = []
-
   let add k f setters = Setter (k, f) :: setters
 
-  let apply_one v map (Setter (k,f)) =
-    match f v with
+  let apply_one v map (Setter (k,f)) = match f v with
+    | None   -> map
     | Some v ->
       if Univ.mem k.key map then map
       else Univ.add k.key v map
-    | None -> map
 
   let apply v l map = List.fold_left (apply_one v) map l
-
   let keys l = Set.of_list @@ List.map (fun (Setter (k,_)) -> Any k) l
 
 end
 
-let hidden x = Any x
-let conv k = k.conv
+let v x = Any x
+let arg k = k.arg
 let setters (Any k) = Setters.keys k.setters
 let name (Any k) = k.name
 let stage (Any k) = k.stage
-let doc (Any k) = k.doc
+let info (Any k) = k.info
 
 let is_runtime k = match stage k with
   | `Run | `Both -> true
@@ -217,28 +202,23 @@ let filter_stage ~stage set =
   | `Configure | `NoEmit -> Set.filter is_configure set
   | `Both -> set
 
-
 (* Key Map *)
 
 type map = Univ.t
 
-let get map { key; default } =
-  match Univ.find key map with
+let get map { key; default; _ } = match Univ.find key map with
   | Some x -> x
-  | None -> default
+  | None   -> default
 
 let mem map t = Univ.mem t.key map
 
 (* {2 Values} *)
 
-type +'a value = {
-  deps: Set.t;
-  v   : map -> 'a;
-}
+type +'a value = { deps: Set.t; v: map -> 'a }
 
-let eval map { v } = v map
-
+let eval map v = v.v map
 let pure x = { deps = Set.empty; v = fun _ -> x }
+
 let app f x = {
   deps = Set.union f.deps x.deps;
   v = fun map -> (eval map f) (eval map x);
@@ -246,76 +226,60 @@ let app f x = {
 
 let map f x = app (pure f) x
 let pipe x f = map f x
-let if_ c t e =
-  pipe c @@ fun b -> if b then t else e
-
+let if_ c t e = pipe c @@ fun b -> if b then t else e
 let ($) = app
-let with_deps ~keys { deps; v } =
-  { deps = Set.(union deps keys); v }
-
-let value k =
-  let v map = get map k in
-  { deps = Set.singleton (Any k); v }
-
+let with_deps ~keys { deps; v } = { deps = Set.(union deps keys); v }
+let value k = let v map = get map k in { deps = Set.singleton (Any k); v }
 let deps k = k.deps
-
-let is_resolved map { deps } =
-  Set.for_all (fun (Any x) -> mem map x) deps
-
-let peek map v =
-  if is_resolved map v then Some (eval map v) else None
-
-let default v =
-  eval Univ.empty v
+let is_resolved map v = Set.for_all (fun (Any x) -> mem map x) v.deps
+let peek map v = if is_resolved map v then Some (eval map v) else None
+let default v = eval Univ.empty v
 
 (* {2 Pretty printing} *)
 
 let pp fmt k = Fmt.string fmt (name k)
-
-let pp_deps fmt v =
-  Set.pp pp fmt v.deps
+let pp_deps fmt v = Set.pp pp fmt v.deps
 
 let pp_map map =
   let f fmt (Any k) =
     let default = if mem map k then Fmt.nop else Fmt.unit " (default)" in
     Fmt.pf fmt "%a=%a%a"
       Fmt.(styled `Bold string) k.name
-      (Conv.pp k.conv) (get map k)
+      (Arg.pp k.arg) (get map k)
       default ()
   in
   Set.pp f
 
 (* {2 Automatic documentation} *)
 
-let doc_setters setters (docu:Doc.t) =
+let info_setters setters (info: Arg.info) =
   let f fmt k = Fmt.pf fmt "$(b,%s)" (name k) in
   let doc_s = if setters = [] then "" else
       Fmt.strf "\nWill automatically set the following keys: %a."
         (Set.pp f) (Setters.keys setters)
   in
-  let doc = match docu.doc with
+  let doc = match info.Arg.doc with
     | None -> doc_s
     | Some s -> s ^ doc_s
   in
-  {docu with doc = Some doc}
+  { info with Arg.doc = Some doc }
 
 (* {2 Key creation} *)
 
 (* Use internally only *)
-let create_raw ~stage ~setters ~doc ~default ~name ~conv =
+let create_raw ~stage ~setters ~doc ~default ~name ~arg =
   let key = Univ.new_key name in
-  let doc = doc_setters setters doc in
-  { doc; stage; default; setters; conv; name; key }
+  let info = info_setters setters doc in
+  { info; stage; default; setters; arg; name; key }
 
 (* Use internally only *)
 let flag_raw ~stage ~setters ~doc ~name =
-  create_raw ~stage ~setters ~doc ~default:false ~name ~conv:Flag
+  create_raw ~stage ~setters ~doc ~default:false ~name ~arg:Arg.Flag
 
-
-let create ?(stage=`Both) ~doc ~default name desc =
+let create ?(stage=`Both) ~doc ~default name arg =
   let setters = Setters.empty in
-  let desc = Conv.Desc desc in
-  create_raw ~stage ~setters ~doc ~default ~name ~conv
+  let arg = Arg.Opt arg in
+  create_raw ~stage ~setters ~doc ~default ~name ~arg
 
 let flag ?(stage=`Both) ~doc name =
   let setters = Setters.empty in
@@ -326,9 +290,9 @@ let proxy ~doc ~setters name =
 
 (* {2 Cmdliner interface} *)
 
-let term_key { doc; desc; default } =
-  let info = Doc.to_cmdliner doc in
-  Desc.cmdliner ~default ~info desc
+let term_key { info; arg; default; _ } =
+  let info = Arg.info_at_configure info in
+  Arg.at_configure ~default ~info arg
 
 let term ?(stage=`Both) l =
   let gather (Any k) rest =
@@ -336,22 +300,21 @@ let term ?(stage=`Both) l =
       Univ.add k.key v map
       |> Setters.apply v k.setters
     in
-    Term.(term_key k f $ rest)
+    Cmdliner.Term.(term_key k f $ rest)
   in
-  Set.fold gather (filter_stage ~stage l) (Term.pure Univ.empty)
+  Set.fold gather (filter_stage ~stage l) (Cmdliner.Term.pure Univ.empty)
 
-let term_value ?stage { deps; v } =
-  Term.(pure v $ term ?stage deps)
+let term_value ?stage { deps; v } = Cmdliner.Term.(pure v $ term ?stage deps)
 
 (* {2 Code emission} *)
 
 let module_name = "Bootvar_gen"
 
-let serialize map fmt (Any k) =
-  Format.fprintf fmt "%a" (Conv.serialize @@ desc k) @@ get map k
+let emit map fmt (Any k) =
+  Format.fprintf fmt "%a" (Arg.emit @@ arg k) @@ get map k
 
-let describe fmt (Any { conv; _ }) =
-  Format.fprintf fmt "%s" (Conv.describe conv)
+let at_runtime fmt (Any { arg; _ }) =
+  Format.fprintf fmt "%s" (Arg.at_runtime arg)
 
 let ocaml_name k = Name.ocamlify (name k)
 
@@ -363,14 +326,12 @@ let emit_rw map fmt k =
     "@[<2>let %s =@ Functoria_runtime.Key.create@ ~doc:%a@ ~default:%a %a@]@,\
      @[<2>let %s_t =@ Functoria_runtime.Key.term %s@]@,\
      @[<2>let %s () =@ Functoria_runtime.Key.get %s@]@,"
-    (ocaml_name k)   Doc.emit (doc k)  (serialize map) k  describe k
+    (ocaml_name k)  Arg.info_emit (info k) (emit map) k at_runtime k
     (ocaml_name k)  (ocaml_name k)
     (ocaml_name k)  (ocaml_name k)
 
 let emit_ro map fmt k =
-  Format.fprintf fmt
-    "@[<2>let %s () =@ %a@]@,"
-    (ocaml_name k)  (serialize map) k
+  Format.fprintf fmt "@[<2>let %s () =@ %a@]@," (ocaml_name k) (emit map) k
 
 let emit map fmt k =
   if is_runtime k
--- src/functoria_key.mli
+++ src/functoria_key.mli
@@ -16,87 +16,95 @@
 
 (** Configuration and runtime command-line arguments. *)
 
-(** Cross-stage argument converters. *)
-module Conv: sig
+(** Cross-stage command-line arguments. *)
+module Arg: sig
+  (** {1 Cross-stage argument converters}
 
-  type 'a t
+      This module extends
+      {{!http://erratique.ch/software/cmdliner/doc/Cmdliner.Arg.html}
+      Cmdliner.Arg} to allow MetaOCaml-style typed cross-stage
+      persistency of command-line arguments. *)
+
+  type 'a converter
   (** The type for argument converters. *)
 
-  val create:
+  val conv:
     'a Cmdliner.Arg.converter -> (Format.formatter -> 'a -> unit) -> string ->
-    'a t
-  (** [create conf emit run] is the argument converter using [conf] to
+    'a converter
+  (** [conv conf emit run] is the argument converter using [conf] to
       convert argument into OCaml value, [emit] to convert OCaml
       values into interpretable strings, and the function named [run]
       to transform these strings into OCaml values again. See
       {!configure}, {!emit} and {!runtime} for details.*)
 
-  (** {2 Predefined descriptions} *)
+  (** {2 Predefined Converters} *)
 
-  val string: string t
+  val string: string converter
   (** [string] converts strings. *)
 
-  val bool: bool t
+  val bool: bool converter
   (** [bool] converts booleans. *)
 
-  val int: int t
+  val int: int converter
   (** [int] converts integers. *)
 
-  val list: 'a t -> 'a list t
+  val list: 'a converter -> 'a list converter
   (** [list t] converts lists of [t]s. *)
 
-  val some: 'a t -> 'a option t
+  val some: 'a converter -> 'a option converter
   (** [some t] converts [t] options. *)
 
   (** {2 Accessors} *)
 
-  val configure: 'a t -> 'a Cmdliner.Arg.converter
-  (** [configure t] converts the command-line arguments passed during
-      the configuration step into OCaml values. *)
+  val conv_at_configure: 'a converter -> 'a Cmdliner.Arg.converter
+  (** [conv_at_configure t] is the converter of configuration time's
+      command-line arguments into OCaml values. *)
 
-  val emit: 'a t -> Format.formatter -> 'a -> unit
-  (** [emit] allows to persist OCaml values across stages, ie. it
+  val conv_emit: 'a converter -> Format.formatter -> 'a -> unit
+  (** [conv_emit] allows to persist OCaml values across stages, ie. it
       takes values (which might be parsed with {!configure} at
       configuration time) and produce a valid string representation
       which can be used at runtime, once the generated code is
       compiled. *)
 
-  val runtime: 'a t -> string
-  (** [runtime] is the name of the function called at runtime to parse
-      the command-line arguments. Usually, it is a [Cmdliner]'s
-      combinator such as {i Cmdliner.Arg.string}. *)
+  val conv_at_runtime: 'a converter -> string
+  (** [conv_at_runtime] is the name of the function called at runtime
+      to parse the command-line arguments. Usually, it is a
+      [Cmdliner]'s combinator such as {i Cmdliner.Arg.string}. *)
 
-end
+  (** {1 Information} *)
 
-(** Cross-stage argument information. See
-    {{!http://erratique.ch/software/cmdliner/doc/Cmdliner.Arg.html#arginfo}Cmdliner.Arg}
-    for the context. *)
-module Info: sig
+  type info
+  (** The type for information about cross-stage command-line
+      arguments. See
+      {{!http://erratique.ch/software/cmdliner/doc/Cmdliner.Arg.html#arginfo}
+      Cmdliner.Arg}.*)
 
-  type t
-  (** The type for information about command-line arguments. *)
-
-  val create:
+  val info:
     ?docs:string -> ?docv:string -> ?doc:string -> ?env:string ->
-    string list -> t
-  (** Define cross-stage information for an argument. See {!Cmdliner.Arg.info} for details. *)
+    string list -> info
+  (** Define cross-stage information for an argument. See
+      {!Cmdliner.Arg.info} for details. *)
 
-  val to_cmdliner: t -> Cmdliner.Arg.info
+  val info_at_configure: info -> Cmdliner.Arg.info
+  (** [cmdliner_info i] is the projection of [i] to
+      {{!http://erratique.ch/software/cmdliner/doc/Cmdliner.Arg.html#TYPEinfo}
+      Cmdliner.Arg.info} *)
 
-  (** Emit the documentation as OCaml code. *)
-  val emit: Format.formatter -> t -> unit
+  val info_emit: Format.formatter -> info -> unit
+  (** [info_emit] allows to persist information about command-line
+      arguments across stages. *)
 
 end
-
-module Set: Functoria_misc.SET
-(** A Set of keys. *)
 
 type +'a value
-(** Value available at configure time.
-    Values have dependencies, which are a set of keys.
+(** The type for configure and runtime values. Values can be parsed
+    from the command-line at configure or runtime, or can be provided
+    at configuration time. A value might have evaluation dependencies,
+    provided as a set of {{!key}keys}: a value is always evaluated
+    after its dependencies.
 
-    Values are resolved to their content when all
-    their dependencies are resolved.
+    FIXME(samoht): example
 *)
 
 val pure: 'a -> 'a value
@@ -118,98 +126,88 @@ val pipe: 'a value -> ('a -> 'b) -> 'b value
 val if_: bool value -> 'a -> 'a -> 'a value
 (** [if_ v x y] is [pipe v @@ fun b -> if b then x else y]. *)
 
-val with_deps: keys:Set.t -> 'a value -> 'a value
-(** [with_deps deps v] is the value [v] with added dependencies. *)
-
 val default: 'a value -> 'a
 (** [default v] returns the default value for [v]. *)
 
+(** {1 Keys} *)
+
 type 'a key
-(** Keys are dynamic values that can be used to
-    - Set options at configure and runtime on the command line.
-    - Switch implementation dynamically, using {!Functoria_dsl.if_impl}.
+(** The type for configuration keys. Keys are dynamic values that can
+    be used to
 
-    Their content is then made available at runtime in the [Bootvar_gen] module.
+    {ul
+    {- Set {{!value}values} at configure and runtime by parsing
+       arguments on the command line.}
+    {- Switch implementation dynamically, using {!Functoria_dsl.if_impl}.
+       FIXME(samoht): not sure what this mean (yet)}
+    }
 
-    Keys are resolved to their content during command line parsing.
+    Keys' content is made persistent by emitting code in module called
+    [Bootvar_gen]. FIXME(implementation details?)
 *)
 
 val value: 'a key -> 'a value
-(** [value k] is the value which depends on [k] and will take its content. *)
+(** [value k] is the value parsed by [k]. *)
 
 type stage = [
   | `Configure
   | `Run
   | `Both
 ]
-(** The stage at which a key is available.
-    - [`Configure] means writable at configure time and readable at runtime.
-    - [`Run] means writable and readable only at runtime
-    - [`Both] means writable and readable at configure and run time.
+(** The type for specifying at which stage a key is available.
+
+    {ul
+
+    {- [`Configure] means that the key's value is read on the
+       command-line at configuration-time and is available in the
+       [Bootvar_gen] module at runtime.}
+    {- [`Run] means that the key's value is read on the command-line
+       at runtime.}
+    {- [`Both] means that the key's value is read on the command-line
+       both at configuration-time and runtime. If the keys is present
+       on the command-line at configuration-time it is available in
+       the [Bootvar_gen] module at runtime.}
+    }
 *)
 
 val create:
-  ?stage:stage -> doc:Doc.t -> default:'a -> string -> 'a Conv.t -> 'a key
+  ?stage:stage -> doc:Arg.info -> default:'a -> string -> 'a Arg.converter ->
+  'a key
 (** [create ~doc ~stage ~default name conv] creates a new
     configuration key with docstring [doc], default value [default],
     name [name] and type descriptor [desc]. Default [stage] is
     [`Both].  It is an error to use more than one key with the same
     [name]. *)
 
-val flag: ?stage:stage -> doc:Doc.t -> string -> bool key
+val flag: ?stage:stage -> doc:Arg.info -> string -> bool key
 (** [flag ~stage ~doc name] creates a new flag. A flag is a key that doesn't
     take argument. The boolean value represents if the flag was passed
     on the command line.
 *)
 
-(** {3 Proxy} *)
+type t
+(** The type for untyped keys. *)
 
-(** Setters allow to set other keys. *)
-module Setters: sig
-
-  type 'a t
-
-  val empty: 'a t
-  (** The empty setter. *)
-
-  val add: 'b key -> ('a -> 'b option) -> 'a t -> 'a t
-  (** [add k f setters] Add a new setter to [setters].
-      It will set [k] to the value generated by [f].
-      If [f] returns [None], no value is set.
-  *)
-
-end
-
-val proxy: doc:Doc.t -> setters:bool Setters.t -> string -> bool key
-(** [proxy ~doc ~setters name] creates a new flag that will call [setters]
-    when enabled.
-
-    Proxies are only available during configuration.
-*)
-
-(** {2 Oblivious keys} *)
-
-type t = Set.elt
-(** Keys which types has been forgotten. *)
-
-val hidden: 'a key -> t
-(** Hide the type of keys. Allows to put them in a set/list. *)
-
-(** {2 Advanced functions} *)
+val v: 'a key -> t
+(** [v key] is [key] by with all type information erased. This allows
+    to put them in a set/list. *)
 
 val pp: t Fmt.t
 (** [pp fmt k] prints the name of [k]. *)
 
-val pp_deps: 'a value Fmt.t
-(** [pp_deps fmt v] prints the name of the dependencies of [v]. *)
+module Set: Set.S with type elt = t
+(** Sets of keys. *)
+
+val with_deps: keys:Set.t -> 'a value -> 'a value
+(** [with_deps deps v] is the value [v] with added dependencies. *)
 
 val deps: 'a value -> Set.t
 (** [deps v] is the dependencies of [v]. *)
 
-val setters: t -> Set.t
-(** [setters k] returns the set of keys for which [k] has a setter. *)
+val pp_deps: 'a value Fmt.t
+(** [pp_deps fmt v] prints the name of the dependencies of [v]. *)
 
-(** {3 Accessors} *)
+(** {1 Stages} *)
 
 val is_runtime: t -> bool
 (** [is_runtime k] is true if [k]'s stage is [`Run] or [`Both]. *)
@@ -220,9 +218,45 @@ val is_configure: t -> bool
 val filter_stage: stage:stage -> Set.t -> Set.t
 (** [filter_stage ~stage set] filters [set] with the appropriate keys. *)
 
-(** {3 Key resolution} *)
+(** {1 Proxy} *)
+
+(** Setters allow to set other keys. *)
+module Setters: sig
+
+  type 'a t
+  (** The type for key setters. *)
+
+  val empty: 'a t
+  (** [empty] is the empty setter. *)
+
+  val add: 'b key -> ('a -> 'b option) -> 'a t -> 'a t
+  (** [add k f setters] Add a new setter to [setters]. It will set [k]
+      to the value generated by [f]. If [f] returns [None], no value
+      is set. *)
+
+end
+
+val proxy: doc:Arg.info -> setters:bool Setters.t -> string -> bool key
+(** [proxy ~doc ~setters name] creates a new flag that will call [setters]
+    when enabled.
+
+    Proxies are only available during configuration.
+*)
+
+val setters: t -> Set.t
+(** [setters k] returns the set of keys for which [k] has a setter. *)
+
+(** {1 Key resolution} *)
 
 type map
+(** The type for parsed command-line arguments. *)
+
+val term: ?stage:stage -> Set.t -> map Cmdliner.Term.t
+(** [term l] is a [Cmdliner.Term.t] that, when evaluated, sets the value of the
+    the keys in [l]. *)
+
+val term_value: ?stage:stage -> 'a value -> 'a Cmdliner.Term.t
+(** [term_value v] is [term @@ deps v] and returns the content of [v]. *)
 
 val is_resolved: map -> 'a value -> bool
 (** [is_reduced map v] returns [true] iff all the dependencies of [v] have
@@ -241,7 +275,7 @@ val get: map -> 'a key -> 'a
 val pp_map: map -> Set.t Fmt.t
 (** [pp_map map fmt set] prints the keys in [set] with the values in [map]. *)
 
-(** {3 Code emission} *)
+(** {1 Code emission} *)
 
 val ocaml_name: t -> string
 (** [ocaml_name k] is the ocaml name of [k]. *)
@@ -251,16 +285,6 @@ val emit_call: t Fmt.t
 
 val emit: map -> t Fmt.t
 (** [emit fmt k] prints the OCaml code needed to define [k]. *)
-
-
-(** {3 Cmdliner} *)
-
-val term: ?stage:stage -> Set.t -> map Cmdliner.Term.t
-(** [term l] is a [Cmdliner.Term.t] that, when evaluated, sets the value of the
-    the keys in [l]. *)
-
-val term_value: ?stage:stage -> 'a value -> 'a Cmdliner.Term.t
-(** [term_value v] is [term @@ deps v] and returns the content of [v]. *)
 
 (**/**)
 

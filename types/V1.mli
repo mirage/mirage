b4db1a00f3b1ab0becf0ec06e6957387eb797753
Matt Gray <matthew.thomas.gray@gmail.com>
2015-11-08 14:32:10 -0800

    make current_tz_offset_s be an int option

--- types/V1.mli
+++ types/V1.mli
@@ -136,8 +136,8 @@ module type PCLOCK = sig
       1970-01-01 00:00:00 UTC. [ps] is in the range
       \[[0];[86_399_999_999_999_999L]\]. *)
 
-  val current_tz_offset_s : unit -> int
-  (** [current_tz_offset_s ()] is the clock's current local time zone
+  val current_tz_offset_s : unit -> int option
+  (** [current_tz_offset_s ()] is if available the clock's current local time zone
       offset to UTC in seconds. *)
 
   val period_d_ps : unit -> (int * int64) option

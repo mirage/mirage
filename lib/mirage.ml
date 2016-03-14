84421537d26a60c1ad69a0cf5e666f7ffbd7085f
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-03-14 14:24:55 +0000

    Add -safe_string to the list of warnings

--- lib/mirage.ml
+++ lib/mirage.ml
@@ -1297,7 +1297,7 @@ let configure_makefile ~target ~root ~name info =
   append fmt "PKGS   = %s" packages;
   let default_tags =
     "warn_error(+1..49),warn(A-4-41-44),debug,bin_annot,\
-     strict_sequence,principal"
+     strict_sequence,principal,safe_string"
   in
   begin match target with
     | `Xen  ->

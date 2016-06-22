3b8a71ef8581b4406c3f951390b38b344c784f40
David Scott <dave@recoil.org>
2016-06-22 10:59:40 +0100

    docs: use the present tense and active voice for the rest of FLOW
    This is for consistency with `close`
    
    Signed-off-by: David Scott <dave@recoil.org>

--- types/V1.mli
+++ types/V1.mli
@@ -125,26 +125,24 @@ module type FLOW = sig
       logging. *)
 
   val read: flow -> [`Ok of buffer | `Eof | `Error of error ] io
-  (** [read flow] will block until it either successfully reads a
-      segment of data from the current flow, receives an [Eof]
-      signifying that the connection is now closed, or an [Error]. *)
+  (** [read flow] reads some data from the flow and returns a
+      fresh buffer (of arbitrary size), an [`Eof] if the other side has
+      called [close] or an [`Error]. *)
 
   val write: flow -> buffer -> [`Ok of unit | `Eof | `Error of error ] io
-  (** [write flow buffer] will block until [buffer] has been added to
-      the send queue. There is no indication when the buffer has
-      actually been read and, therefore, it must not be reused.  The
-      contents may be transmitted in separate packets, depending on
-      the underlying transport. The result [`Ok ()] indicates success,
-      [`Eof] indicates that the connection is now closed and [`Error]
-      indicates some other error. *)
+  (** [write flow buffer] writes a buffer to the flow. There is no
+      indication when the buffer has actually been read and, therefore,
+      it must not be reused.  The contents may be transmitted in
+      separate packets, depending on the underlying transport. The
+      result [`Ok ()] indicates success, [`Eof] indicates that the
+      connection is now closed and [`Error] indicates some other error. *)
 
   val writev: flow -> buffer list -> [`Ok of unit | `Eof | `Error of error ] io
-  (** [writev flow buffers] will block until the buffers have all been
-      added to the send queue. There is no indication when the buffers
-      have actually been read and, therefore, they must not be reused.
-      The result [`Ok ()] indicates success, [`Eof] indicates that the
-      connection is now closed and [`Error] indicates some other
-      error. *)
+  (** [writev flow buffers] writes a sequence of buffers to the flow.
+      There is no indication when the buffers have actually been read and,
+      therefore, they must not be reused. The result [`Ok ()] indicates
+      success, [`Eof] indicates that the connection is now closed and
+      [`Error] indicates some other error. *)
 
   val close: flow -> unit io
   (** [close flow] flushes all pending writes and signals the remote

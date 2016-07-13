ae8f4000169d6c9668a64679874782b1bb0e8b7f
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-07-13 15:41:29 +0100

    Switch to topkg

---
+++ pkg/pkg.ml
@@ -0,0 +1,36 @@
+#!/usr/bin/env ocaml
+#use "topfind"
+#require "topkg"
+open Topkg
+
+let metas = [
+  Pkg.meta_file "pkg/META.mirage";
+  Pkg.meta_file "pkg/META.mirage-types";
+]
+
+let opams =
+  let lint_deps_excluding = None in
+  [
+    Pkg.opam_file "mirage.opam" ~lint_deps_excluding;
+    Pkg.opam_file "mirage-types.opam" ~lint_deps_excluding;
+    Pkg.opam_file "mirage-types-lwt.opam" ~lint_deps_excluding;
+  ]
+
+let () =
+  Pkg.describe ~metas ~opams "mirage" @@ fun c ->
+  match Conf.pkg_name c with
+  | "mirage" ->
+    Ok [ Pkg.lib "pkg/META.mirage" ~dst:"META";
+         Pkg.mllib "lib/mirage.mllib";
+         Pkg.mllib "lib_runtime/mirage-runtime.mllib";
+         Pkg.bin "lib/main" ~dst:"mirage"; ]
+  | "mirage-types" ->
+    Ok [ Pkg.lib "pkg/META.mirage-types" ~dst:"META";
+         Pkg.lib "types/V1.mli";
+         Pkg.lib "types/V1.cmi"; ]
+  | "mirage-types-lwt" ->
+    Ok [ Pkg.lib "pkg/META.mirage-types-lwt" ~dst:"META";
+         Pkg.lib "types/V1_LWT.mli";
+         Pkg.lib "types/V1_LWT.cmi"; ]
+  | other ->
+    R.error_msgf "unknown package name: %s" other
--- types/V1_LWT.mli
+++ types/V1_LWT.mli
@@ -16,6 +16,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
+(** lwt-compatible MirageOS signatures *)
+
 open V1
 
 module type TIME = TIME

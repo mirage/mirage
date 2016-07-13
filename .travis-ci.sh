ca97126221f02b0ce471b123476e787e7541c8e0
Mindy Preston <mindy.preston@docker.com>
2016-07-13 16:33:24 +0100

    use mirage-skeleton mirage-dev branch for testing, not a private branch

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,7 +2,7 @@ bash -ex .travis-opam.sh
 
 eval `opam config env`
 opam install mirage -y
-git clone -b remove-console https://github.com/yomimono/mirage-skeleton.git
+git clone -b mirage-dev https://github.com/mirage/mirage-skeleton.git
 cd mirage-skeleton
 MODE=unix make
 MODE=xen  make

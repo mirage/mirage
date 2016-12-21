9d2004bc19375a9d8aad906cd5cc2c4f93e4a1fc
Hannes Mehnert <hannes@mehnert.org>
2016-12-21 15:13:28 +0100

    style

--- app/functoria_command_line.ml
+++ app/functoria_command_line.ml
@@ -17,8 +17,7 @@
 let setup_log style_renderer level =
   Fmt_tty.setup_std_outputs ?style_renderer ();
   Logs.set_level level;
-  Logs.set_reporter (Logs_fmt.reporter ());
-  ()
+  Logs.set_reporter (Logs_fmt.reporter ())
 
 open Cmdliner
 

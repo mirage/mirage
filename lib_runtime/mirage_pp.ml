e4a0c97135e53f92d8a1ef14e83af24c6cfba060
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-12-28 13:22:08 +0100

    Use mirage-kv and mirage-fs

--- lib_runtime/mirage_pp.ml
+++ lib_runtime/mirage_pp.ml
@@ -19,18 +19,3 @@ let pp_tcp_error ppf = function
 let pp_tcp_write_error ppf = function
   | #Mirage_flow.write_error as e -> Mirage_flow.pp_write_error ppf e
   | #V1.Tcp.error as e            -> pp_tcp_error ppf e
-
-let pp_fs_error ppf = function
-    | `Is_a_directory      -> Fmt.string ppf "is a directory"
-    | `Not_a_directory     -> Fmt.string ppf "is not a directory"
-    | `No_directory_entry  ->
-      Fmt.string ppf "a directory in the path does not exist"
-
-let pp_fs_write_error ppf = function
-  | #Fs.error as e       -> pp_fs_error ppf e
-  | `Directory_not_empty -> Fmt.string ppf "directory is not empty"
-  | `File_already_exists -> Fmt.string ppf "file already exists"
-  | `No_space            -> Fmt.string ppf "device has no more free space"
-
-let pp_kv_ro_error ppf = function
-  | `Unknown_key k -> Fmt.pf ppf "key %s not present in the store" k

6c42bcc390e0966621137eb4125185c964bb9020
Mindy Preston <mindy.preston@docker.com>
2017-03-04 04:28:01 -0600

    clone the primary branch of mirage-skeleton to test it

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
 eval `opam config env`
 opam depext -uiy mirage
-git clone -b mirage-dev https://github.com/mirage/mirage-skeleton.git
+git clone https://github.com/mirage/mirage-skeleton.git
 make -C mirage-skeleton && rm -rf mirage-skeleton

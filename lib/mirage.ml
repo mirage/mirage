18227cc2a2afd30061923e1773329ea093942669
David Scott <dave@recoil.org>
2017-06-15 14:34:19 +0100

    Revert "Link io-page-unix on Ukvm and Virtio too"
    This reverts commit 3e3b86c399648cd8d36757d96e3a4d7f6ab002af.

--- lib/mirage.ml
+++ lib/mirage.ml
@@ -1936,11 +1936,9 @@ module Project = struct
         | `Xen | `Qubes -> [ package ~min:"3.0.0" "mirage-xen";
                              package "io-page-xen" ] @ common
         | `Virtio -> [ package ~min:"0.2.1" ~ocamlfind:[] "solo5-kernel-virtio" ;
-                       package ~min:"0.2.0" "mirage-solo5";
-                       package "io-page-unix" ] @ common
+                       package ~min:"0.2.0" "mirage-solo5" ] @ common
         | `Ukvm -> [ package ~min:"0.2.1" ~ocamlfind:[] "solo5-kernel-ukvm" ;
-                     package ~min:"0.2.0" "mirage-solo5";
-                     package "io-page-unix"; ] @ common
+                     package ~min:"0.2.0" "mirage-solo5" ] @ common
         | `Unix | `MacOSX -> [ package ~min:"3.0.0" "mirage-unix" ;
                                package "io-page-unix"; ] @ common
 

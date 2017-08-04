e2dad5c37ba0b1cca8c215054102a020141a2898
Mindy Preston <meetup@yomimono.org>
2017-08-04 17:59:39 -0500

    test against a mirage-skeleton with name change fixes

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,6 @@
 eval `opam config env`
 opam depext -uiy mirage
 cd ~
-git clone https://github.com/mirage/mirage-skeleton.git
+# needed for dns.mirage fix - revert to master from mirage when fixed -yomimono
+git clone -b name-changes https://github.com/yomimono/mirage-skeleton.git
 make -C mirage-skeleton && rm -rf mirage-skeleton

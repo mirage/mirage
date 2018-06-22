62b72c5b9250251ddcd2138f0551cdc961584785
Hannes Mehnert <hannes@mehnert.org>
2018-06-22 09:26:54 +0200

    use mirage-skeleton master again

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,5 @@
 eval `opam config env`
 opam depext -uiy mirage
 cd ~
-# revert to `git clone -b master https://github.com/mirage/mirage-skeleton.git`
-# once mirage-skeleton#257 or equivalent is merged
-git clone -b muen-ready https://github.com/yomimono/mirage-skeleton.git
+git clone -b master https://github.com/mirage/mirage-skeleton.git
 make -C mirage-skeleton && rm -rf mirage-skeleton

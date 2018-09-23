16c1bc5f5fab11d7095b751202295ef7922d5148
Hannes Mehnert <hannes@mehnert.org>
2018-09-23 17:00:12 +0200

    travis: use mirage-skeleton master

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 eval `opam config env`
 opam depext -uiy mirage
 cd ~
-git clone -b solo5.0.4.0-renaming https://github.com/mato/mirage-skeleton.git
+git clone -b master https://github.com/mirage/mirage-skeleton.git
 make -C mirage-skeleton && rm -rf mirage-skeleton

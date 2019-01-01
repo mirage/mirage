5ece1ff198ddf6d4bc3b9b3e22c6c85a5d3e4af3
Hannes Mehnert <hannes@mehnert.org>
2019-01-02 00:32:03 +0100

    mirage-clock 2.x only API change is type t = unit, which is fine with mirage

--- lib/mirage_impl_pclock.ml
+++ lib/mirage_impl_pclock.ml
@@ -10,8 +10,8 @@ let posix_clock_conf = object
   method module_name = "Pclock"
   method! packages =
     Mirage_key.(if_ is_unix)
-      [ package ~min:"1.2.0" ~max:"2.0.0" "mirage-clock-unix" ]
-      [ package ~min:"1.2.0" ~max:"2.0.0" "mirage-clock-freestanding" ]
+      [ package ~min:"1.2.0" ~max:"3.0.0" "mirage-clock-unix" ]
+      [ package ~min:"1.2.0" ~max:"3.0.0" "mirage-clock-freestanding" ]
   method! connect _ modname _args = Fmt.strf "%s.connect ()" modname
 end
 

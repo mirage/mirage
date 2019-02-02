c3155d5c31d8e5a7682ec685235018dd443b8f40
Hannes Mehnert <hannes@mehnert.org>
2019-02-02 11:48:10 +0100

    temporarily use ethernet branch and mirage-dev arp branch remote

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 eval `opam config env`
 opam depext -uiy mirage
 cd ~
-git clone -b master https://github.com/mirage/mirage-skeleton.git
+git clone -b ethernet https://github.com/hannesm/mirage-skeleton.git
 make -C mirage-skeleton && rm -rf mirage-skeleton

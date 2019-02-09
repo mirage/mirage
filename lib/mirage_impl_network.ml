fa45f32a153f2d4fccec6da4d21d10ceacb4233c
Hannes Mehnert <hannes@mehnert.org>
2019-02-09 18:08:03 +0100

    adjust constraints

--- lib/mirage_impl_network.ml
+++ lib/mirage_impl_network.ml
@@ -17,14 +17,14 @@ let network_conf (intf : string Key.key) =
     method! keys = [ key ]
     method! packages =
       Key.match_ Key.(value target) @@ function
-      | `Unix -> [ package ~min:"2.3.0" ~max:"3.0.0" "mirage-net-unix" ]
-      | `MacOSX -> [ package ~min:"1.4.0" ~max:"2.0.0" "mirage-net-macosx" ]
-      | `Xen -> [ package ~min:"1.7.0" ~max:"2.0.0" "mirage-net-xen"]
+      | `Unix -> [ package ~min:"2.6.0" ~max:"3.0.0" "mirage-net-unix" ]
+      | `MacOSX -> [ package ~min:"1.6.0" ~max:"2.0.0" "mirage-net-macosx" ]
+      | `Xen -> [ package ~min:"1.10.0" ~max:"2.0.0" "mirage-net-xen"]
       | `Qubes ->
-        [ package ~min:"1.7.0" ~max:"2.0.0" "mirage-net-xen" ;
+        [ package ~min:"1.10.0" ~max:"2.0.0" "mirage-net-xen" ;
           Mirage_impl_qubesdb.pkg ]
       | `Virtio | `Hvt | `Muen | `Genode ->
-        [ package ~min:"0.4.0" ~max:"0.5.0" "mirage-net-solo5" ]
+        [ package ~min:"0.4.2" ~max:"0.5.0" "mirage-net-solo5" ]
     method! connect _ modname _ =
       Fmt.strf "%s.connect %a" modname Key.serialize_call key
     method! configure i =

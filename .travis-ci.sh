bbe2e16688afa425b4ef12219cf0f4fff3622143
Hannes Mehnert <hannes@mehnert.org>
2019-02-28 12:01:04 +0100

    try mirage 3.5.0 universe

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 eval `opam config env`
 opam depext -uiy mirage
 cd ~
-git clone -b layering https://github.com/hannesm/mirage-skeleton.git
+git clone -b releng-3.5.0 https://github.com/hannesm/mirage-skeleton.git
 make -C mirage-skeleton && rm -rf mirage-skeleton

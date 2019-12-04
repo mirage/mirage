f6026bc6ca2e96e52d265135ea1383c0b54ab1a8
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-12-04 14:30:25 +0100

    Externalize clean_dune* functions
    See update into Mirage_clean and give a proper way to delete all generated files.

--- lib/mirage_configure.mli
+++ lib/mirage_configure.mli
@@ -14,3 +14,7 @@ val clean_opam : name:string -> Mirage_key.mode -> (unit, [> R.msg ]) result
 val configure_makefile : no_depext:bool -> opam_name:string -> (unit, [> R.msg ]) result
 
 val configure : Functoria.Info.t -> (unit, [> R.msg ]) result
+
+val clean_dune : unit -> (unit, [> R.msg ]) result
+val clean_dune_project : unit -> (unit, [> R.msg ]) result
+val clean_dune_workspace : unit -> (unit, [> R.msg ]) result

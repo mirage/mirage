82c7ee68ebfd52b88f97873c3a7e29c786c894ba
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-12-04 13:52:05 +0100

    Add Mirage_configure_unix
    This patch adds a way to configure the UNIX target where we define a dune's
    alias: dune build @default and an underlying rule to _generate_ the unikernel.

---
+++ lib/mirage_configure_unix.ml
@@ -0,0 +1,24 @@
+open Mirage_impl_misc
+
+module Key = Mirage_key
+
+let configure_unix ~name ~binary_location ~target =
+  let target_name = Fmt.strf "%a" Key.pp_target target in
+  let alias =
+    sexp_of_fmt
+      {sexp|
+      (alias
+        (name %s)
+        (enabled_if (= %%{context_name} "default"))
+        (deps %s))
+      |sexp} target_name name in
+  let rule =
+    sexp_of_fmt
+      {sexp|
+      (rule
+        (target %s)
+        (deps %s)
+        (mode promote)
+        (action (run ln -nfs %s %s)))
+      |sexp} name binary_location binary_location name in
+  Ok [ alias; rule; ]

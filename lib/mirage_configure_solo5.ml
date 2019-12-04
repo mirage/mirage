90810053883bce22c2c0392d1ca8ee8b69cbf155
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-12-04 14:13:27 +0100

    Emit dune's configuration for Solo5 target
    This patch loads into `pwd`/.mirage.solo5/ some well-formed files about
    flags (used by `cc` and `ld`). They will be tracked by `dune` then according
    emitted dune configuration.
    
    At least, these files and specially `cflags.sexp` will replace the expansion
    variable :standard available into `c_flags` dune's field. Compilation of
    manifest.o will use these flags.
    
    This part depends on `pkg-config` to load flags from *.pc files.

--- lib/mirage_configure_solo5.ml
+++ lib/mirage_configure_solo5.ml
@@ -5,7 +5,7 @@ open Mirage_impl_misc
 module Codegen = Functoria_app.Codegen
 module Log = Mirage_impl_misc.Log
 
-let solo5_manifest_path = Fpath.v "_build/manifest.json"
+let solo5_manifest_path = Fpath.v "manifest.json"
 
 let clean_manifest () =
   Bos.OS.File.delete solo5_manifest_path
@@ -65,3 +65,121 @@ let compile_manifest target =
   in
   Log.info (fun m -> m "executing %a" Bos.Cmd.pp cmd);
   Bos.OS.Cmd.run cmd
+
+(* Process to get & save into files flags. *)
+
+open Sexplib
+
+let cflags_sexp_path = Fpath.((v ".mirage" + "solo5") / "cflags.sexp")
+let lflags_path = Fpath.((v ".mirage" + "solo5") / "lflags")
+let libs_sexp_path = Fpath.((v ".mirage" + "solo5") / "libs.sexp")
+let libdir_path = Fpath.((v ".mirage" + "solo5") / "libdir")
+
+let cflags ~target =
+  let pkg, _ = solo5_pkg target in
+  pkg_config pkg [ "--cflags" ] >>= fun cflags ->
+  let sexp =
+    sexp_of_fmt
+      {sexp|(%a)|sexp}
+      Fmt.(list ~sep:(always " ") string) cflags in
+  Bos.OS.File.write_lines
+    cflags_sexp_path
+    (List.map (fun x -> Sexp.to_string_hum x ^ "\n") [ sexp ])
+
+let lflags ~target =
+  let pkg, _ = solo5_pkg target in
+  pkg_config pkg [ "--variable=ldflags" ] >>= fun lflags ->
+  Bos.OS.File.write_lines
+    lflags_path lflags
+
+let libs ~target =
+  let pkg, _ = solo5_pkg target in
+  pkg_config pkg [ "--libs" ] >>= fun libs ->
+  let sexp =
+    sexp_of_fmt
+      {sexp|(%a)|sexp}
+      Fmt.(list ~sep:(always " ") string) libs in
+  Bos.OS.File.write_lines
+    libs_sexp_path
+    (List.map (fun x -> Sexp.to_string_hum x ^ "\n") [ sexp ])
+
+let libdir ~target =
+  let pkg, _ = solo5_pkg target in
+  pkg_config pkg [ "--variable=libdir" ] >>= fun libdir ->
+  Bos.OS.File.write_lines
+    libdir_path libdir
+
+(* Generate configure part of Solo5 target. *)
+
+let hijack_dune () =
+  let lines =
+    [ "(include dune.config)"
+    ; "(dirs .mirage.solo5)"
+    ; "(include dune.build)" ] in
+  Bos.OS.File.write_lines (Fpath.v "dune") lines
+
+let configure_solo5 ~name ~binary_location ~target =
+  hijack_dune () >>= fun () ->
+  Bos.OS.Dir.create ~path:true Fpath.(v ".mirage" + "solo5") >>= fun _ ->
+  cflags ~target >>= fun () ->
+  lflags ~target >>= fun () ->
+  libs ~target >>= fun () ->
+  libdir ~target >>= fun () ->
+  generate_manifest_json () >>= fun () ->
+
+  let _, post = solo5_pkg target in
+  let out = name ^ post in
+  let alias =
+    sexp_of_fmt
+      {sexp|
+      (alias
+        (name %a)
+         (enabled_if (= %%{context_name} "mirage-freestanding"))
+         (deps %s))
+      |sexp} Key.pp_target target out in
+  let rule_unikernel =
+    sexp_of_fmt
+      {sexp|
+      (rule
+        (targets %s)
+        (mode promote)
+        (deps %s manifest.o)
+        (action (run ld %%{read-lines:lflags} manifest.o %s -o %s)))
+      |sexp} out binary_location binary_location out in
+  let rule_libs =
+    sexp_of_fmt
+      {sexp|(rule (copy %a libs.sexp))|sexp}
+      Fpath.pp libs_sexp_path in
+  let rule_lflags =
+    sexp_of_fmt
+      {sexp|(rule (copy %a lflags))|sexp}
+      Fpath.pp lflags_path in
+  let rule_cflags =
+    sexp_of_fmt
+      {sexp|(rule (copy %a cflags.sexp))|sexp}
+      Fpath.pp cflags_sexp_path in
+  let rule_libdir =
+    sexp_of_fmt
+      {sexp|(rule (copy %a libdir))|sexp}
+      Fpath.pp libdir_path in
+  let rule_manifest_c =
+    sexp_of_fmt
+      {sexp|
+      (rule
+        (targets manifest.c)
+        (deps manifest.json)
+        (action (run solo5-elftool gen-manifest manifest.json manifest.c)))
+      |sexp} in
+  let rule_manifest_o =
+    sexp_of_fmt
+      {sexp|
+      (library
+        (name manifest)
+        (modules manifest)
+        (foreign_stubs (language c) (names manifest)
+          (flags (:include cflags.sexp))))
+      |sexp} in
+  Bos.OS.File.write (Fpath.v "manifest.ml") "" >>= fun () ->
+  Ok ( rule_lflags :: rule_cflags :: rule_libs :: rule_libdir
+       :: rule_manifest_c :: rule_manifest_o
+       :: alias :: rule_unikernel :: [] )

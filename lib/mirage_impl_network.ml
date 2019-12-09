fcc0aa08235a0fc82848bbf4941f160c48ba871c
Ubuntu <ubuntu@ip-172-31-37-98.us-east-2.compute.internal>
2019-12-09 20:13:20 +0000

    Lfns

--- lib/mirage_impl_network.ml
+++ lib/mirage_impl_network.ml
@@ -33,7 +33,6 @@ let network_conf (intf : string Key.key) =
   end
 
 let netif ?group dev = impl (network_conf @@ Key.interface ?group dev)
-Log.info (fun m -> m "netif: %s" netif);
 let default_network =
   match_impl Key.(value target) [
     `Unix   , netif "tap0";

mirage/mirage

commit 167d14fbc489bed071ef6f97b200588193402abf

functoria: remove objects and classes
Also rename Functoria.configurable into Functoria.Device and add
various helpers functions.

Remove objects and classes and clean-up few related things.

functoria/lib/functoria_engine.ml
@@ -16,12 +16,12 @@
  *)
 
 open Functoria_misc
-open Astring
 open Rresult
 
 module Graph = Functoria_graph
 module Key = Functoria_key
 module Package = Functoria_package
+module Device = Functoria.Device
 
 type t = Graph.t
 
@@ -29,12 +29,12 @@ let if_keys =
   let open Graph in
   Graph.collect (module Key.Set) @@ function
   | If cond      -> Key.deps cond
-  | App | Impl _ -> Key.Set.empty
+  | App | Dev _ -> Key.Set.empty
 
 let all_keys =
   let open Graph in
   Graph.collect (module Key.Set) @@ function
-  | Impl c  -> Key.Set.of_list c#keys
+  | Dev c   -> Key.Set.of_list (Device.keys c)
   | If cond -> Key.deps cond
   | App     -> Key.Set.empty
 
@@ -47,86 +47,57 @@ end
 let packages =
   let open Graph in
   Graph.collect (module M) @@ function
-  | Impl c     -> c#packages
+  | Dev c     -> Device.packages c
   | If _ | App -> M.empty
-
-(* Return a unique variable name holding the state of the given
-   module construction. *)
-let name c id =
-  let prefix = Name.ocamlify c#name in
-  Name.create (Fmt.strf "%s%i" prefix id) ~prefix
 
 (* [module_expresion tbl c args] returns the module expression of
    the functor [c] applies to [args]. *)
-let module_expression tbl fmt (c, args) =
+let module_expression fmt (c, args) =
   Fmt.pf fmt "%s%a"
-    c#module_name
-    Fmt.(list (parens @@ of_to_string @@ Graph.Tbl.find tbl))
+    (Device.module_name c)
+    Fmt.(list (parens @@ of_to_string @@ Graph.impl_name))
     args
 
-(* [module_name tbl c args] return the module name of the result of
-   the functor application. If [args = []], it returns
-   [c#module_name]. *)
-let module_name c id args =
-  let base = c#module_name in
-  if args = [] then base
-  else
-    let prefix = match String.cut ~sep:"." base with
-      | Some (l, _) -> l
-      | None -> base
-    in
-    let prefix = Name.ocamlify prefix in
-    Name.create (Fmt.strf "%s%i" prefix id) ~prefix
-
-(* FIXME: Can we do better than lookup by name? *)
-let find_device info g i =
+let find_all_devices info g i =
   let ctx = Functoria.Info.context info in
   let open Functoria in
-  let rec name: type a . a impl -> string = fun impl ->
+  let rec id: type a . a impl -> int = fun impl ->
     match explode impl with
-    | `Impl c              -> c#name
-    | `App (Abstract x, _) -> name x
-    | `If (b, x, y)        -> if Key.eval ctx b then name x else name y
+    | `Dev c               -> Device.id c
+    | `App (Abstract x, _) -> id x
+    | `If (b, x, y)        -> if Key.eval ctx b then id x else id y
   in
-  let name = name i in
-  let open Graph in
+  let id = id i in
   let p = function
-    | Impl c     -> c#name = name
+    | Graph.Dev d -> Device.id d = id
     | App | If _ -> false
   in
-  match Graph.find_all g p with
-  | []  -> invalid_arg "Functoria.find_device: no device"
-  | [x] -> x
-  | _   -> invalid_arg "Functoria.find_device: too many devices."
+  Graph.find_all g p
 
 let build info t =
   let f v = match Graph.explode t v with
-    | `App _ | `If _ -> R.ok ()
-    | `Impl (c, _, _) -> c#build info
+    | `App _ | `If _ -> assert false
+    | `Dev (Graph.D c, _, _) -> Device.build c info
   in
   let f v res = res >>= fun () -> f v in
   Graph.fold f t @@ R.ok ()
 
 let configure info t =
-  let tbl = Graph.Tbl.create 17 in
   let f v = match Graph.explode t v with
     | `App _ | `If _ -> assert false
-    | `Impl (c, `Args args, `Deps _) ->
-      let modname = module_name c (Graph.hash v) args in
-      Graph.Tbl.add tbl v modname;
-      c#configure info >>| fun () ->
+    | `Dev (Graph.D c, `Args args, `Deps _) ->
+      Device.configure c info >>| fun () ->
       if args = [] then ()
       else begin
         Codegen.append_main
           "@[<2>module %s =@ %a@]"
-          modname
-          (module_expression tbl) (c,args);
+          (Graph.impl_name v)
+          module_expression (c, args);
         Codegen.newline_main ();
       end
   in
   let f v res = res >>= fun () -> f v in
-  Graph.fold f t @@ R.ok () >>| fun () ->
-  tbl
+  Graph.fold f t @@ R.ok ()
 
 let meta_init fmt (connect_name, result_name) =
   Fmt.pf fmt "let _%s =@[@ Lazy.force %s @]in@ " result_name connect_name
@@ -160,36 +131,31 @@ let emit_run init main =
      in run t@]"
     Fmt.(list ~sep:nop force) init main
 
-let connect ?(init=[]) ~modules info t =
-  let tbl = Graph.Tbl.create 17 in
+let connect ?(init=[]) info t =
   let f v = match Graph.explode t v with
     | `App _ | `If _ -> assert false
-    | `Impl (c, `Args args, `Deps deps) ->
-      let ident = name c (Graph.hash v) in
-      let modname = Graph.Tbl.find modules v in
-      Graph.Tbl.add tbl v ident;
-      let names = List.map (Graph.Tbl.find tbl) (args @ deps) in
+    | `Dev (Graph.D c, `Args args, `Deps deps) ->
+      let var_name = Graph.var_name v in
+      let impl_name = Graph.impl_name v in
+      let arg_names = List.map Graph.var_name (args @ deps) in
       Codegen.append_main "%a"
-        emit_connect (ident, names, c#connect info modname);
+        emit_connect (var_name, arg_names, Device.connect c info impl_name);
   in
   Graph.fold (fun v () -> f v) t ();
-  let main_name = Graph.Tbl.find tbl @@ Graph.find_root t in
+  let main_name = Graph.var_name (Graph.find_root t) in
   let init_names =
-    List.map (fun name -> Graph.Tbl.find tbl @@ find_device info t name) init
+    List.fold_left (fun acc i ->
+        match find_all_devices info t i with
+        | [] -> assert false
+        | ds -> List.map Graph.var_name ds @ acc
+      ) [] init
   in
-  emit_run init_names main_name;
-  ()
-
-type modules = string Graph.Tbl.t
-
-let configure_and_connect ?init info g =
-  configure info g >>| fun modules ->
-  connect ~modules info ?init g
+  emit_run init_names main_name
 
 let clean i g =
   let f v = match Graph.explode g v with
-    | `Impl (c,_,_) -> c#clean i
-    | _ -> R.ok ()
+    | `App _ | `If _ -> assert false
+    | `Dev (Graph.D c,_,_) -> Device.clean c i
   in
   let f v res = res >>= fun () -> f v in
   Graph.fold f g @@ R.ok ()

test/functoria/gen-1/main.ml.expected
@@ -2,31 +2,31 @@ let (>>=) x f = f x
 let return x = x
 let run x = x
 
-module App1 = App.Make(Key_gen)(Info_gen)
+module App_make__8 = App.Make(Key_gen)(Info_gen)
 
-let argv1 = lazy (
+let sys__2 = lazy (
   return Sys.argv
   )
 
-let key1 = lazy (
-  let __argv1 = Lazy.force argv1 in
-  __argv1 >>= fun _argv1 ->
-  return (Functoria_runtime.with_argv (List.map fst Key_gen.runtime_keys) "foo" _argv1)
+let key_gen__3 = lazy (
+  let __sys__2 = Lazy.force sys__2 in
+  __sys__2 >>= fun _sys__2 ->
+  return (Functoria_runtime.with_argv (List.map fst Key_gen.runtime_keys) "foo" _sys__2)
   )
 
-let info1 = lazy (
+let info_gen__5 = lazy (
   return Info_gen.info
   )
 
-let f11 = lazy (
-  let __key1 = Lazy.force key1 in
-  let __info1 = Lazy.force info1 in
-  __key1 >>= fun _key1 ->
-  __info1 >>= fun _info1 ->
-  App1.start _key1 _info1
+let app_make__8 = lazy (
+  let __key_gen__3 = Lazy.force key_gen__3 in
+  let __info_gen__5 = Lazy.force info_gen__5 in
+  __key_gen__3 >>= fun _key_gen__3 ->
+  __info_gen__5 >>= fun _info_gen__5 ->
+  App_make__8.start _key_gen__3 _info_gen__5
   )
 
 let () =
   let t =
-  Lazy.force f11
+  Lazy.force app_make__8
   in run t

test/functoria/gen-1/test.ml
@@ -1,3 +1,5 @@
+open Rresult
+
 (* yiikes *)
 let () =
   Functoria_misc.Codegen.set_main_ml "main.ml";
@@ -5,10 +7,6 @@ let () =
   Functoria_misc.Codegen.append_main "let return x = x";
   Functoria_misc.Codegen.append_main "let run x = x";
   Functoria_misc.Codegen.newline_main ()
-
-let ok msg = function
-  | Ok () -> ()
-  | Error (`Msg e) -> Fmt.failwith "%s: %s" msg e
 
 let test_device context device =
   let t = Functoria_graph.create device in
@@ -21,10 +19,9 @@ let test_device context device =
       ~name:"foo"
       ~build_dir:Fpath.(v ".")
   in
-  Functoria_engine.configure_and_connect ~init:[] info t
-  |> ok "configure_and_connect";
+  Functoria_engine.configure info t >>= fun () ->
+  Functoria_engine.connect info t;
   Functoria_engine.build info t
-  |> ok "build"
 
 let opam_deps = [
   "base-bigarray", "base";
@@ -48,10 +45,12 @@ let test () =
   let context = Functoria_key.empty_context in
   let sigs = Functoria.(job @-> info @-> job) in
   let keys =
-    Functoria.(foreign "App.Make" sigs
+    Functoria.(main "App.Make" sigs
                $ keys sys_argv
                $ app_info ~opam_deps ())
   in
   test_device context keys
 
-let () = test ()
+let () = match test () with
+  | Ok () -> ()
+  | Error (`Msg e) -> failwith e

test/functoria/test.ml
@@ -1,4 +1,5 @@
 let () = Alcotest.run "functoria" [
     "cli"    , Test_cli.suite;
     "package", Test_package.suite;
+    "graph"  , Test_graph.suite;
   ]

functoria/app/functoria_app.ml
@@ -522,7 +522,8 @@ module Make (P: S) = struct
     Codegen.append_main "let _ = Printexc.record_backtrace true";
     Codegen.newline_main ();
     Cache.save ~argv (Info.build_dir i) >>= fun () ->
-    Engine.configure_and_connect ~init i jobs >>| fun () ->
+    Engine.configure i jobs >>| fun () ->
+    Engine.connect i ~init jobs;
     Codegen.newline_main ()
 
   let clean_main i jobs =

functoria/app/functoria_app.mli
@@ -75,7 +75,7 @@ module Make (P: S): sig
 
   val register:
     ?packages:package list ->
-    ?keys:key list ->
+    ?keys:abstract_key list ->
     ?init:job impl list ->
     string -> job impl list -> unit
   (** [register name jobs] registers the application named by [name]
@@ -103,10 +103,6 @@ module Name: sig
       characters outside of ['a'-'z''A'-'Z''0''9''_''-'], and
       replacing '-' with '_'.  If the resulting string starts with a
       digit or is empty then it raises [Invalid_argument]. *)
-
-  val create: string -> prefix:string -> string
-  (** [name key ~prefix] is an deterministic name starting by
-      [prefix]. The name is derived from [key].  *)
 
 end
 

commit 8f5ba2a82b494be389750614893d9016b1c4d2a0

Fix ordering of inits to unbreak unikernels runtimes
Currently, starting a mirage app results in the following error:

    Fatal error: exception Invalid_argument("Key.get: Called too early. Please delay this call after cmdliner's evaluation.")

This is a regression introduced in 167d14fbc489bed071ef6f97b200588193402abf.
I have added a test to make sure this won't happen anymore.

functoria/lib/functoria_engine.ml
@@ -148,6 +148,7 @@ let connect ?(init = []) info t =
         | [] -> assert false
         | ds -> List.map Graph.var_name ds @ acc)
       [] init
+    |> List.rev
   in
   emit_run init_names main_name
 

test/functoria/gen-2/dune
@@ -1,0 +1,18 @@
+(executable
+ (name test)
+ (modules test)
+ (libraries fmt functoria))
+
+(rule
+ (targets key_gen.ml info_gen.ml main.ml)
+ (action
+  (run ./test.exe)))
+
+(rule
+ (alias runtest)
+ (package functoria)
+ (action
+  (progn
+   (diff key_gen.ml.expected key_gen.ml)
+   (diff info_gen.ml.expected info_gen.ml)
+   (diff main.ml.expected main.ml))))

test/functoria/gen-2/info_gen.ml.expected
@@ -1,0 +1,22 @@
+let info = Functoria_runtime.{
+  name = "foo";
+  packages = [
+    "base-bigarray", "base";
+    "base-threads", "base";
+    "base-unix", "base";
+    "cmdliner", "1.0.4";
+    "conf-m4", "1";
+    "dune", "2.0.0";
+    "fmt", "0.8.8";
+    "ocaml", "4.08.1";
+    "ocaml-base-compiler", "4.08.1";
+    "ocaml-config", "1";
+    "ocamlbuild", "0.14.0";
+    "ocamlfind", "1.8.1";
+    "seq", "base";
+    "stdlib-shims", "0.1.0";
+    "topkg", "1.0.1"]
+  ;
+  libraries = [
+    "functoria-runtime"]
+  }

test/functoria/gen-2/key_gen.ml.expected
@@ -1,0 +1,11 @@
+(* Generated by "./test.exe". *)
+
+let hello =Functoria_runtime.Key.create
+  (Functoria_runtime.Arg.opt Cmdliner.Arg.string "Hello World!" (Cmdliner.Arg.info
+   ~docs:"APPLICATION OPTIONS" ?docv:(None) ?doc:(Some "How to say hello.  ")
+   ?env:(None) ["hello"]))
+let hello_t = Functoria_runtime.Key.term hello
+let hello () = Functoria_runtime.Key.get hello
+
+let runtime_keys = List.combine [hello_t] ["hello"]
+

test/functoria/gen-2/main.ml.expected
@@ -1,0 +1,40 @@
+let (>>=) x f = f x
+let return x = x
+let run x = x
+
+module App_make__11 = App.Make(Key_gen)(Unit)(Info_gen)
+
+let sys__2 = lazy (
+  return Sys.argv
+  )
+
+let key_gen__3 = lazy (
+  let __sys__2 = Lazy.force sys__2 in
+  __sys__2 >>= fun _sys__2 ->
+  return (Functoria_runtime.with_argv (List.map fst Key_gen.runtime_keys) "foo" _sys__2)
+  )
+
+let unit__5 = lazy (
+  return ()
+  )
+
+let info_gen__7 = lazy (
+  return Info_gen.info
+  )
+
+let app_make__11 = lazy (
+  let __key_gen__3 = Lazy.force key_gen__3 in
+  let __unit__5 = Lazy.force unit__5 in
+  let __info_gen__7 = Lazy.force info_gen__7 in
+  __key_gen__3 >>= fun _key_gen__3 ->
+  __unit__5 >>= fun _unit__5 ->
+  __info_gen__7 >>= fun _info_gen__7 ->
+  App_make__11.start _key_gen__3 _unit__5 _info_gen__7
+  )
+
+let () =
+  let t =
+  Lazy.force key_gen__3 >>= fun _ ->
+    Lazy.force unit__5 >>= fun _ ->
+    Lazy.force app_make__11
+  in run t

test/functoria/gen-2/test.ml
@@ -1,0 +1,68 @@
+open Rresult
+module Codegen = Functoria_misc.Codegen
+module Graph = Functoria_graph
+module Key = Functoria_key
+module Engine = Functoria_engine
+
+(* yiikes *)
+let () =
+  Codegen.set_main_ml "main.ml";
+  Codegen.append_main "let (>>=) x f = f x";
+  Codegen.append_main "let return x = x";
+  Codegen.append_main "let run x = x";
+  Codegen.newline_main ()
+
+let i1 = Functoria.(keys sys_argv)
+
+let i2 = Functoria.noop
+
+let test_device context device =
+  let t = Graph.create device in
+  let t = Graph.normalize t in
+  let keys = Key.Set.elements (Engine.all_keys t) in
+  let packages = Key.eval context (Engine.packages t) in
+  let info =
+    Functoria.Info.v ~packages ~context ~keys
+      ~build_dir:Fpath.(v ".")
+      ~build_cmd:[ "build"; "me" ] ~src:`None "foo"
+  in
+  Engine.configure info t >>= fun () ->
+  Engine.connect info ~init:[ i1; i2 ] t;
+  Engine.build info t
+
+let opam_deps =
+  [
+    ("base-bigarray", "base");
+    ("base-threads", "base");
+    ("base-unix", "base");
+    ("cmdliner", "1.0.4");
+    ("conf-m4", "1");
+    ("dune", "2.0.0");
+    ("fmt", "0.8.8");
+    ("ocaml", "4.08.1");
+    ("ocaml-base-compiler", "4.08.1");
+    ("ocaml-config", "1");
+    ("ocamlbuild", "0.14.0");
+    ("ocamlfind", "1.8.1");
+    ("seq", "base");
+    ("stdlib-shims", "0.1.0");
+    ("topkg", "1.0.1");
+  ]
+
+let key =
+  let doc = Key.Arg.info ~doc:"How to say hello." [ "hello" ] in
+  Key.(create "hello" Arg.(opt string "Hello World!" doc))
+
+let test () =
+  let context = Key.empty_context in
+  let sigs = Functoria.(job @-> job @-> info @-> job) in
+  let keys =
+    Functoria.(
+      main ~keys:[ Key.abstract key ] "App.Make" sigs
+      $ i1
+      $ i2
+      $ app_info ~opam_deps ())
+  in
+  test_device context keys
+
+let () = match test () with Ok () -> () | Error (`Msg e) -> failwith e

test/functoria/gen-2/test.mli
@@ -1,0 +1,1 @@
+(* empty *)
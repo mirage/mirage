6e62237ddb46a525b2b74205877e8a32863607a3
Hannes Mehnert <hannes@mehnert.org>
2020-02-24 19:15:18 +0100

    use Mirage_crypto_rng_unix.initialize on unix target

--- lib/mirage_impl_random.ml
+++ lib/mirage_impl_random.ml
@@ -1,4 +1,5 @@
 open Functoria
+open Mirage_impl_misc
 
 type random = RANDOM
 let random = Type RANDOM
@@ -9,13 +10,17 @@ let random_conf = object
   method name = "random"
   method module_name = "Mirage_crypto_rng"
   method! packages =
-    Mirage_key.pure [
-      package "mirage-crypto-rng" ;
-      package ~min:"1.0.0" "mirage-entropy" ;
-    ]
-  method! connect _ _ _ =
-    (* here we could use the boot argument to select the RNG! *)
-    "Entropy.connect (module Mirage_crypto_rng.Fortuna)"
+    Mirage_key.(if_ is_unix)
+      [ package ~sublibs:["unix"] "mirage-crypto-rng" ]
+      [ package "mirage-crypto-rng" ;
+        package ~min:"1.0.0" "mirage-entropy" ]
+  method! connect i _ _ =
+    match get_target i with
+    | #Mirage_key.mode_unix ->
+      "Lwt.return (Mirage_crypto_rng_unix.initialize ())"
+    | _ ->
+      (* here we could use the boot argument to select the RNG! *)
+      "Entropy.connect (module Mirage_crypto_rng.Fortuna)"
 end
 
 let default_random = impl random_conf

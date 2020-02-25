2b9b5c27f5302b794d58b56b4efa645a462aa538
Hannes Mehnert <hannes@mehnert.org>
2020-02-25 13:27:56 +0100

    revise to use mirage-crypto-entropy for initializing the default RNG

--- lib/mirage_impl_random.ml
+++ lib/mirage_impl_random.ml
@@ -12,15 +12,14 @@ let random_conf = object
   method! packages =
     Mirage_key.(if_ is_unix)
       [ package ~sublibs:["unix"] "mirage-crypto-rng" ]
-      [ package "mirage-crypto-rng" ;
-        package ~min:"1.0.0" "mirage-entropy" ]
+      [ package "mirage-crypto-entropy" ]
   method! connect i _ _ =
     match get_target i with
     | #Mirage_key.mode_unix ->
       "Lwt.return (Mirage_crypto_rng_unix.initialize ())"
     | _ ->
       (* here we could use the boot argument to select the RNG! *)
-      "Entropy.connect (module Mirage_crypto_rng.Fortuna)"
+      "Mirage_crypto_entropy.initialize (module Mirage_crypto_rng.Fortuna)"
 end
 
 let default_random = impl random_conf

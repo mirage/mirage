fc42b35d3c18bcec3ffa76697285e1a1364d37ee
Thomas Gazagnaire <thomas@gazagnaire.org>
2020-03-27 16:47:54 +0100

    functoria: autogen the dune file for query tests

---
+++ test/functoria/query/gen.ml
@@ -0,0 +1,27 @@
+type t = { cmd : string; file : string }
+
+let v x = { cmd = x; file = x }
+
+let gen t =
+  Format.printf
+    {|
+(rule
+ (target %s)
+ (action
+  (with-stdout-to
+   %%{target}
+   (run ./config.exe query %s))))
+
+(rule
+ (alias runtest)
+ (package functoria)
+ (action
+  (diff %s.expected %s)))
+|}
+    t.cmd t.file t.file t.file
+
+let () =
+  List.iter gen
+    [
+      v "opam"; v "packages"; v "install"; v "files-configure"; v "files-build";
+    ]

5fc3fd72e5c22f5da44d789cdd05d72fd91d8864
Thomas Gazagnaire <thomas@gazagnaire.org>
2021-02-05 15:18:08 +0100

    WIP: try to fix TravisCI

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,18 @@
 eval `opam config env`
 opam depext -uiy mirage
 cd ~
-git clone https://github.com/mirage/mirage-skeleton.git
+
+# pin conduit
+CONDUIT=https://github.com/samoht/ocaml-conduit.git#simplify-conduit-mirage
+opam pin -n add conduit.2.3.0 $CONDUIT
+opam pin -n add conduit-lwt.2.3.0 $CONDUIT
+opam pin -n add conduit-mirage.2.3.0 $CONDUIT
+
+# pin cohttp
+COHTTP=https://github.com/samoht/ocaml-cohttp.git#conduit-2.3
+opam pin -n add cohttp.2.3.0 $COHTTP
+opam pin -n add cohttp-lwt.2.3.0 $COHTTP
+opam pin -n add cohttp-mirage.2.3.0 $COHTTP
+
+git clone https://github.com/samoht/mirage-skeleton.git#conduit-2.3
 make -C mirage-skeleton && rm -rf mirage-skeleton

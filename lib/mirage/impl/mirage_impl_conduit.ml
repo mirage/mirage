8c0a05a991b2ed79a54175fbf59b37e72605de39
Lucas Pluvinage <lucas@tarides.com>
2021-03-02 17:35:47 +0100

    Use conduit 2.3.0

--- lib/mirage/impl/mirage_impl_conduit.ml
+++ lib/mirage/impl/mirage_impl_conduit.ml
@@ -1,25 +1,30 @@
 open Functoria
-open Mirage_impl_conduit_connector
+open Mirage_impl_stack
+open Mirage_impl_misc
+open Mirage_impl_random
 
 type conduit = Conduit
 
 let conduit = Type.v Conduit
 
-let conduit_with_connectors connectors =
-  let packages = [ pkg ] in
-  let extra_deps = List.map dep connectors in
-  let connect _ _ connectors =
-    let pp_connector = Fmt.fmt "%s >>=@ " in
-    let pp_connectors = Fmt.list ~sep:Fmt.nop pp_connector in
-    Fmt.strf "Lwt.return Conduit_mirage.empty >>=@ %afun t -> Lwt.return t"
-      pp_connectors connectors
-  in
-  impl ~packages ~extra_deps ~connect "Conduit_mirage" conduit
+let pkg = package ~min:"2.3.0" ~max:"3.0.0" "conduit-mirage"
 
-let conduit_direct ?(tls = false) s =
-  (* TCP must be before tls in the list. *)
-  let connectors = [ tcp_conduit_connector $ s ] in
-  let connectors =
-    if tls then connectors @ [ tls_conduit_connector ] else connectors
+let tcp =
+  let packages = [ pkg ] in
+  let connect _ _ = function
+    | [ stack ] -> Fmt.strf "Lwt.return %s@;" stack
+    | _ -> failwith (connect_err "tcp_conduit" 1)
   in
-  conduit_with_connectors connectors
+  impl ~packages ~connect "Conduit_mirage.TCP" (stackv4 @-> conduit)
+
+let tls random =
+  let packages = [ pkg; package ~min:"0.12.0" ~max:"0.13.0" "tls-mirage" ] in
+  let extra_deps = [ dep random ] in
+  let connect _ _ = function
+    | [ stack; _random ] -> Fmt.strf "Lwt.return %s@;" stack
+    | _ -> failwith (connect_err "tls_conduit" 1)
+  in
+  impl ~packages ~connect ~extra_deps "Conduit_mirage.TLS" (conduit @-> conduit)
+
+let conduit_direct ?tls:(use_tls = false) ?(random = default_random) s =
+  if use_tls then tls random $ (tcp $ s) else tcp $ s
--- lib/mirage/impl/mirage_impl_http.ml
+++ lib/mirage/impl/mirage_impl_http.ml
@@ -1,19 +1,40 @@
 open Functoria
+open Mirage_impl_pclock
 open Mirage_impl_misc
+open Mirage_impl_conduit
+open Mirage_impl_resolver
 
 type http = HTTP
 
 let http = Type.v HTTP
 
+type http_client = HTTP_client
+
+let http_client = Type.v HTTP_client
+
 let connect err _i modname = function
-  | [ conduit ] -> Fmt.strf "%s.connect %s" modname conduit
+  | [ conduit ] -> Fmt.strf "Lwt.return (%s.listen %s)" modname conduit
   | _ -> failwith (connect_err err 1)
 
-let cohttp_server conduit =
-  let packages = [ package ~min:"2.1.0" ~max:"3.0.0" "cohttp-mirage" ] in
-  let extra_deps = [ dep conduit ] in
-  impl ~packages ~connect:(connect "http") ~extra_deps
-    "Cohttp_mirage.Server_with_conduit" http
+let cohttp_server =
+  let packages = [ package ~min:"3.0.0" ~max:"4.0.0" "cohttp-mirage" ] in
+  impl ~packages ~connect:(connect "http") "Cohttp_mirage.Server.Make"
+    (conduit @-> http)
+
+let cohttp_server conduit = cohttp_server $ conduit
+
+let cohttp_client =
+  let packages = [ package ~min:"3.0.0" ~max:"4.0.0" "cohttp-mirage" ] in
+  let connect _i modname = function
+    | [ _pclock; resolver; conduit ] ->
+        Fmt.strf "Lwt.return (%s.ctx %s %s)" modname resolver conduit
+    | _ -> failwith (connect_err "http" 2)
+  in
+  impl ~packages ~connect "Cohttp_mirage.Client.Make"
+    (pclock @-> resolver @-> conduit @-> http_client)
+
+let cohttp_client ?(pclock = default_posix_clock) resolver conduit =
+  cohttp_client $ pclock $ resolver $ conduit
 
 let httpaf_server conduit =
   let packages = [ package "httpaf-mirage" ] in

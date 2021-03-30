4db9cdb4097e6c7c1afdd2da7bd00a790a98f475
Hannes Mehnert <hannes@mehnert.org>
2021-03-30 13:35:02 +0200

    Revert "WIP: try to fix TravisCI"
    This reverts commit 5fc3fd72e5c22f5da44d789cdd05d72fd91d8864.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,18 +1,5 @@
 eval `opam config env`
 opam depext -uiy mirage
 cd ~
-
-# pin conduit
-CONDUIT=https://github.com/samoht/ocaml-conduit.git#simplify-conduit-mirage
-opam pin -n add conduit.2.3.0 $CONDUIT
-opam pin -n add conduit-lwt.2.3.0 $CONDUIT
-opam pin -n add conduit-mirage.2.3.0 $CONDUIT
-
-# pin cohttp
-COHTTP=https://github.com/samoht/ocaml-cohttp.git#conduit-2.3
-opam pin -n add cohttp.2.3.0 $COHTTP
-opam pin -n add cohttp-lwt.2.3.0 $COHTTP
-opam pin -n add cohttp-mirage.2.3.0 $COHTTP
-
-git clone https://github.com/samoht/mirage-skeleton.git#conduit-2.3
+git clone https://github.com/mirage/mirage-skeleton.git
 make -C mirage-skeleton && rm -rf mirage-skeleton

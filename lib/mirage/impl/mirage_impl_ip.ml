e67e3cb2d128e81072de34fc0bd3b2498d62b1e4
Hannes Mehnert <hannes@mehnert.org>
2021-03-30 15:40:47 +0200

    remove tcpip.unix dependency, which is now superfluous (since 6.1.0)
    adapts 977b8dd93aba6276511e2624f44216d6cf1ca2fb to master

--- lib/mirage/impl/mirage_impl_ip.ml
+++ lib/mirage/impl/mirage_impl_ip.ml
@@ -49,12 +49,8 @@ let ( @?? ) x y = opt_map Key.v x @? y
 
 (* convenience function for linking tcpip.unix for checksums *)
 let right_tcpip_library ?libs ~sublibs pkg =
-  let min = "6.0.0" and max = "7.0.0" in
-  Key.match_ Key.(value target) @@ function
-  | #Mirage_key.mode_unix ->
-      [ package ~min ~max ?libs ~sublibs:("unix" :: sublibs) pkg ]
-  | #Mirage_key.mode_xen | #Mirage_key.mode_solo5 ->
-      [ package ~min ~max ?libs ~sublibs pkg ]
+  let min = "6.1.0" and max = "7.0.0" in
+  Key.pure [ package ~min ~max ?libs ~sublibs pkg ]
 
 let ipv4_keyed_conf ~ip ?gateway ?no_init () =
   let packages_v = right_tcpip_library ~sublibs:[ "ipv4" ] "tcpip" in

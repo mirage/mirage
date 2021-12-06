84df08eb4cee2eadedad85091f6be2ff8e074e33
Hannes Mehnert <hannes@mehnert.org>
2021-12-06 19:50:43 +0100

    mirage 3: allow newer mirage-clock

--- lib/mirage_impl_mclock.ml
+++ lib/mirage_impl_mclock.ml
@@ -10,8 +10,8 @@ let monotonic_clock_conf = object
   method module_name = "Mclock"
   method! packages =
     Mirage_key.(if_ is_unix)
-      [ package ~min:"3.0.0" ~max:"4.0.0" "mirage-clock-unix" ]
-      [ package ~min:"3.0.0" ~max:"4.0.0" "mirage-clock-freestanding" ]
+      [ package ~min:"3.0.0" ~max:"5.0.0" "mirage-clock-unix" ]
+      [ package ~min:"3.0.0" ~max:"5.0.0" "mirage-clock-freestanding" ]
 end
 
 let default_monotonic_clock = impl monotonic_clock_conf

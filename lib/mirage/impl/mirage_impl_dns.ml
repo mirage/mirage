9c116ad0ae4fe2eba97e474c8c553fd932f226f3
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-03-12 18:29:49 +0100

    Add dns_client device

---
+++ lib/mirage/impl/mirage_impl_dns.ml
@@ -0,0 +1,32 @@
+open Functoria
+open Mirage_impl_time
+open Mirage_impl_mclock
+open Mirage_impl_pclock
+open Mirage_impl_stack
+open Mirage_impl_random
+
+type dns_client = Dns_client
+
+let dns_client = Type.v Dns_client
+
+let generic_dns_client ?(timeout = 5000000000L) nameservers =
+  let packages =
+    [ package "dns-client" ~sublibs:[ "mirage" ] ~min:"6.2.0" ~max:"6.3.0" ]
+  in
+  let keys =
+    match nameservers with
+    | None -> []
+    | Some nameservers -> [ Key.v nameservers ]
+  in
+  let connect _info modname = function
+    | [ _random; _time; _mclock; _pclock; stackv4v6 ] ->
+        let pp_nameservers ppf = function
+          | None -> Fmt.string ppf "[]"
+          | Some nameservers -> Key.serialize_call ppf (Key.v nameservers)
+        in
+        Fmt.str {ocaml|%s.connect ~nameservers:%a ~timeout:%aL %s|ocaml} modname
+          pp_nameservers nameservers Fmt.int64 timeout stackv4v6
+    | _ -> assert false
+  in
+  impl ~keys ~packages ~connect "Dns_client_mirage.Make"
+    (random @-> time @-> mclock @-> pclock @-> stackv4v6 @-> dns_client)

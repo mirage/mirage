8254f5d114ba6ed1edfeb3dbfe278e66f97852e4
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-03-15 17:32:37 +0100

    Delete the deprecation of Cmdliner.Term.eval_choice and replace it by Cmd.group (@samoht)

--- lib/functoria/cli.ml
+++ lib/functoria/cli.ml
@@ -296,6 +296,7 @@ let pp_action pp_a ppf = function
   | Clean c -> Fmt.pf ppf "@[clean:@ @[<2>%a@]@]" (pp_clean pp_a) c
   | Help h -> Fmt.pf ppf "@[help:@ @[<2>%a@]@]" (pp_help pp_a) h
 
+(*
 let args ~with_setup context mname =
   Term.(
     const (fun () config_file context_file dry_run output context ->
@@ -306,23 +307,37 @@ let args ~with_setup context mname =
     $ dry_run
     $ output
     $ context)
+*)
 
 (*
  * Subcommand specifications
  *)
 
 module Subcommands = struct
-  [@@@warning "-d"]
+  type 'a t = { with_setup : bool; mname : string; context : 'a Term.t }
+
+  module T = struct
+    let args { with_setup; context; mname } =
+      Term.(
+        const (fun () config_file context_file dry_run output context ->
+            { config_file; context_file; dry_run; output; context })
+        $ setup ~with_setup
+        $ config_file
+        $ context_file mname
+        $ dry_run
+        $ output
+        $ context)
+  end
 
   (** The 'configure' subcommand *)
-  let configure ~with_setup mname context =
+  let configure t =
     ( Term.(
         const (fun args depext extra_repo ->
             Configure { args; depext; extra_repo })
-        $ args ~with_setup context mname
+        $ T.args t
         $ depext configuration_section
         $ extra_repos configuration_section),
-      Term.info "configure" ~doc:"Configure a $(mname) application."
+      Cmd.info "configure" ~doc:"Configure a $(mname) application."
         ~man:
           [
             `S "DESCRIPTION";
@@ -331,15 +346,15 @@ module Subcommands = struct
                application.";
           ] )
 
-  let query ~with_setup mname context =
+  let query t =
     ( Term.(
         const (fun kind args depext extra_repo ->
             Query { kind; args; depext; extra_repo })
         $ kind
-        $ args ~with_setup context mname
+        $ T.args t
         $ depext query_section
         $ extra_repos query_section),
-      Term.info "query" ~doc:"Query information about the $(mname) application."
+      Cmd.info "query" ~doc:"Query information about the $(mname) application."
         ~man:
           [
             `S "DESCRIPTION";
@@ -349,14 +364,14 @@ module Subcommands = struct
           ] )
 
   (** The 'describe' subcommand *)
-  let describe ~with_setup mname context =
+  let describe t =
     ( Term.(
         const (fun args eval dotcmd dot -> Describe { args; eval; dotcmd; dot })
-        $ args ~with_setup context mname
+        $ T.args t
         $ full_eval
         $ dotcmd
         $ dot),
-      Term.info "describe" ~doc:"Describe a $(mname) application."
+      Cmd.info "describe" ~doc:"Describe a $(mname) application."
         ~man:
           [
             `S "DESCRIPTION";
@@ -384,19 +399,19 @@ module Subcommands = struct
           ] )
 
   (** The 'build' subcommand *)
-  let build ~with_setup mname context =
+  let build t =
     let doc = "Build a $(mname) application." in
-    ( Term.(const (fun args -> Build args) $ args ~with_setup context mname),
-      Term.info "build" ~doc ~man:[ `S "DESCRIPTION"; `P doc ] )
+    ( Term.(const (fun args -> Build args) $ T.args t),
+      Cmd.info "build" ~doc ~man:[ `S "DESCRIPTION"; `P doc ] )
 
   (** The 'clean' subcommand *)
-  let clean ~with_setup mname context =
+  let clean t =
     let doc = "Clean the files produced by $(mname) for a given application." in
-    ( Term.(const (fun args -> Clean args) $ args ~with_setup context mname),
-      Term.info "clean" ~doc ~man:[ `S "DESCRIPTION"; `P doc ] )
+    ( Term.(const (fun args -> Clean args) $ T.args t),
+      Cmd.info "clean" ~doc ~man:[ `S "DESCRIPTION"; `P doc ] )
 
   (** The 'help' subcommand *)
-  let help ~with_setup mname context =
+  let help t =
     let topic =
       let doc = Arg.info [] ~docv:"TOPIC" ~doc:"The topic to get help on." in
       Arg.(value & pos 0 (some string) None & doc)
@@ -417,12 +432,12 @@ module Subcommands = struct
     in
     ( Term.(
         const (fun args _ _ _ () -> Help args)
-        $ args ~with_setup context mname
+        $ T.args t
         $ depext configuration_section
         $ extra_repos configuration_section
         $ full_eval
-        $ ret (const help $ Term.man_format $ Term.choice_names $ topic)),
-      Term.info "help" ~doc:"Display help about $(mname) commands."
+        $ ret (const help $ Arg.man_format $ Term.choice_names $ topic)),
+      Cmd.info "help" ~doc:"Display help about $(mname) commands."
         ~man:
           [
             `S "DESCRIPTION";
@@ -432,8 +447,8 @@ module Subcommands = struct
 
   let default ~with_setup ~name ~version =
     let usage = `Help (`Plain, None) in
-    ( Term.(ret (pure usage) $ setup ~with_setup),
-      Term.info name ~version ~doc:"The $(mname) application builder"
+    ( Term.(ret (const usage) $ setup ~with_setup),
+      Cmd.info name ~version ~doc:"The $(mname) application builder"
         ~man:
           [
             `S "DESCRIPTION";
@@ -458,23 +473,33 @@ let peek_output argv =
   match Cmd.eval_peek_opts ~argv output with _, Ok (`Ok b) -> b | _ -> None
 
 let peek_args ?(with_setup = false) ~mname argv =
-  match Cmd.eval_peek_opts ~argv (args ~with_setup (Term.const ()) mname) with
+  let args =
+    Subcommands.T.args { with_setup; mname; context = Term.const () }
+  in
+  match Cmd.eval_peek_opts ~argv args with
   | _, Ok (`Ok b) | Some b, _ -> b
   | _ -> assert false
 
 let eval ?(with_setup = true) ?help_ppf ?err_ppf ~name ~version ~configure
     ~query ~describe ~build ~clean ~help ~mname argv =
-  (Cmdliner.Term.eval_choice [@warning "-d"]) ?help:help_ppf ?err:err_ppf ~argv
-    ~catch:false
-    (Subcommands.default ~with_setup ~name ~version)
-    [
-      Subcommands.configure ~with_setup mname configure;
-      Subcommands.describe ~with_setup mname describe;
-      Subcommands.query ~with_setup mname query;
-      Subcommands.build ~with_setup mname build;
-      Subcommands.clean ~with_setup mname clean;
-      Subcommands.help ~with_setup mname help;
-    ]
+  let default, info = Subcommands.default ~with_setup ~name ~version in
+  let args context = { Subcommands.with_setup; mname; context } in
+  let group =
+    Cmd.group ~default info
+      (List.map
+         (fun (term, info) -> Cmd.v info term)
+         [
+           Subcommands.configure (args configure);
+           Subcommands.describe (args describe);
+           Subcommands.query (args query);
+           Subcommands.build (args build);
+           Subcommands.clean (args clean);
+           Subcommands.help (args help);
+         ])
+  in
+  match Cmd.eval_value ?help:help_ppf ?err:err_ppf ~argv ~catch:false group with
+  | Ok (#Cmd.eval_ok as v) -> v
+  | Error (#Cmd.eval_error as e) -> `Error e
 
 let args = function
   | Configure { args; _ } -> args
@@ -560,14 +585,17 @@ let peek ?(with_setup = false) ~mname argv : unit result =
         `Ok (Help args)
     | Some v, _ | _, Ok (`Ok v) -> `Ok v
   in
-  let peek_cmd t = peek (t niet) in
+  let peek_cmd f =
+    let args = { Subcommands.with_setup; mname; context = niet } in
+    peek (f args)
+  in
   match peek_choice argv with
-  | `Ok `Configure -> peek_cmd (Subcommands.configure ~with_setup mname)
-  | `Ok `Build -> peek_cmd (Subcommands.build ~with_setup mname)
-  | `Ok `Clean -> peek_cmd (Subcommands.clean ~with_setup mname)
-  | `Ok (`Query _) -> peek_cmd (Subcommands.query ~with_setup mname)
-  | `Ok `Describe -> peek_cmd (Subcommands.describe ~with_setup mname)
-  | `Ok `Help -> peek_cmd (Subcommands.help ~with_setup mname)
+  | `Ok `Configure -> peek_cmd Subcommands.configure
+  | `Ok `Build -> peek_cmd Subcommands.build
+  | `Ok `Clean -> peek_cmd Subcommands.clean
+  | `Ok (`Query _) -> peek_cmd Subcommands.query
+  | `Ok `Describe -> peek_cmd Subcommands.describe
+  | `Ok `Help -> peek_cmd Subcommands.help
   | `Default ->
       peek (Subcommands.default ~with_setup ~name:"<name>" ~version:"<version>")
   | `Error e -> `Error (peek_args ~mname argv, e)

cdcd84f370e46fb23c26b9d64b8abb85b7a3ed28
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-03-17 11:32:12 +0100

    The timeout argument becomes optional for the generic_dns_client

--- lib/mirage/impl/mirage_impl_dns.ml
+++ lib/mirage/impl/mirage_impl_dns.ml
@@ -14,9 +14,11 @@ let generic_dns_client timeout nameservers =
     [ package "dns-client" ~sublibs:[ "mirage" ] ~min:"6.2.0" ~max:"7.0.0" ]
   in
   let keys =
-    match nameservers with
-    | None -> [ Key.v timeout ]
-    | Some nameservers -> [ Key.v timeout; Key.v nameservers ]
+    match (nameservers, timeout) with
+    | None, None -> []
+    | None, Some timeout -> [ Key.v timeout ]
+    | Some nameservers, None -> [ Key.v nameservers ]
+    | Some nameservers, Some timeout -> [ Key.v nameservers; Key.v timeout ]
   in
   let connect _info modname = function
     | [ _random; _time; _mclock; _pclock; stackv4v6 ] ->
@@ -24,9 +26,13 @@ let generic_dns_client timeout nameservers =
           | None -> Fmt.string ppf "[]"
           | Some nameservers -> Key.serialize_call ppf (Key.v nameservers)
         in
-        Fmt.str {ocaml|%s.connect ~nameservers:%a ~timeout:%a %s|ocaml} modname
-          pp_nameservers nameservers Key.serialize_call (Key.v timeout)
-          stackv4v6
+        let pp_timeout ppf = function
+          | None -> ()
+          | Some timeout ->
+              Fmt.pf ppf "~timeout:%a " Key.serialize_call (Key.v timeout)
+        in
+        Fmt.str {ocaml|%s.connect ~nameservers:%a %a%s|ocaml} modname
+          pp_nameservers nameservers pp_timeout timeout stackv4v6
     | _ -> assert false
   in
   impl ~keys ~packages ~connect "Dns_client_mirage.Make"

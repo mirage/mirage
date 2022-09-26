03b9f20f56def05b35f4ed2036a059a60cd567c9
Hannes Mehnert <hannes@mehnert.org>
2022-09-26 14:38:44 +0200

    upgrade internal constraints to 4.3.0

--- lib/mirage/mirage.ml
+++ lib/mirage/mirage.ml
@@ -352,7 +352,7 @@ let run t = %s.Main.run t ; exit 0|ocaml}
     let keys = Key.[ v target ] in
     let packages_v =
       (* XXX: use %%VERSION_NUM%% here instead of hardcoding a version? *)
-      let min = "4.2.0" and max = "4.3.0" in
+      let min = "4.3.0" and max = "4.4.0" in
       let common =
         [
           package ~scope:`Switch ~build:true ~min:"4.08.0" "ocaml";

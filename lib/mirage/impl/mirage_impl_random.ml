0b43b25c4ee181306025b360bcb961372e59a5eb
Hannes Mehnert <hannes@mehnert.org>
2023-02-06 11:56:35 +0100

    mirage-crypto 0.11 is compatible with mirage' use thereof

--- lib/mirage/impl/mirage_impl_random.ml
+++ lib/mirage/impl/mirage_impl_random.ml
@@ -9,7 +9,7 @@ let random = Type.v RANDOM
 let rng ?(time = default_time) ?(mclock = default_monotonic_clock) () =
   let packages =
     [
-      package ~min:"0.8.0" ~max:"0.11.0" "mirage-crypto-rng-mirage";
+      package ~min:"0.8.0" ~max:"0.12.0" "mirage-crypto-rng-mirage";
       package ~min:"3.0.0" ~max:"4.0.0" "mirage-random";
     ]
   in

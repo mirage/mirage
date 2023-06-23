bd9eebfcae5134545ee3989835000108cbd35d93
Thomas Gazagnaire <thomas@gazagnaire.org>
2023-06-23 09:45:54 +0200

    Demonstrate how to use runtime keys defined in the unikernel

--- test/functoria/e2e/app/app.ml
+++ test/functoria/e2e/app/app.ml
@@ -1,4 +1,15 @@
+let arg_t =
+  Cmdliner.Arg.(
+    value
+      (opt string "-"
+         (info ~docs:"APPLICATION OPTIONS" ~doc:"A runtime argument." [ "arg" ])))
+
+let arg = Functoria_runtime.Key.register arg_t
+
 module Make (_ : sig end) = struct
   let start () =
-    Fmt.pr "Success: vote=%s hello=%s\n%!" Key_gen.(vote ()) Key_gen.(hello ())
+    Fmt.pr "Success: vote=%s hello=%s arg=%s\n%!"
+      Key_gen.(vote ())
+      Key_gen.(hello ())
+      (arg ())
 end

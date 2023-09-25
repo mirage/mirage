a325056d0dadd25e61e873967bf63423d9d592de
Hannes Mehnert <hannes@mehnert.org>
2023-09-25 18:09:08 +0200

    default to best-fit allocation policy

--- lib/mirage/mirage_key.ml
+++ lib/mirage/mirage_key.ml
@@ -177,7 +177,7 @@ let allocation_policy =
   let doc =
     Arg.info ~docs:ocaml_section ~docv:"ALLOCATION" ~doc [ "allocation-policy" ]
   in
-  let key = Arg.opt conv `Next_fit doc in
+  let key = Arg.opt conv `Best_fit doc in
   Key.create "allocation-policy" key
 
 let minor_heap_size =

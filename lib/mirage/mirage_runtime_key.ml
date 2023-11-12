ec9a23ac362c044cbe4e42932d096dd62b4407d1
Hannes Mehnert <hannes@mehnert.org>
2023-11-12 23:49:32 +0100

    move runtime keys back (as strings) to mirage_runtime_key

--- lib/mirage/mirage_runtime_key.ml
+++ lib/mirage/mirage_runtime_key.ml
@@ -19,35 +19,233 @@ include Runtime_key
 
 (** {2 OCaml runtime} *)
 
-let runtime_key name =
+let ocaml_section = "OCAML RUNTIME PARAMETERS"
+let unikernel_section = "UNIKERNEL PARAMETERS"
+
+let runtime_key ~name data =
   Runtime_key.create ~name
     ~packages:[ package "mirage-runtime" ]
-    (Fmt.str "Mirage_runtime.%s" name)
+    ("(" ^ data ^ ")")
 
 let runtime_keyf ~name fmt =
   Fmt.kstr
     (Runtime_key.create ~name ~packages:[ package "mirage-runtime" ])
-    ("Mirage_runtime." ^^ fmt)
+    ("Mirage_runtime.key" ^^ fmt)
+
+let backtrace =
+  runtime_key ~name:"backtrace"
+    (Fmt.str
+       {|
+  let doc =
+    "Trigger the printing of a stack backtrace when an uncaught exception \
+     aborts the unikernel."
+  in
+  let doc = Cmdliner.Arg.info ~docs:%S ~docv:"BOOL" ~doc [ "backtrace" ] in
+  Cmdliner.Arg.(value & opt bool true doc)|}
+       ocaml_section)
+
+let randomize_hashtables =
+  runtime_key ~name:"randomize-hashtables"
+    (Fmt.str
+       {|
+  let doc = "Turn on randomization of all hash tables by default." in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"BOOL" ~doc [ "randomize-hashtables" ]
+  in
+  Cmdliner.Arg.(value & opt bool true doc)|}
+       ocaml_section)
+
+let allocation_policy =
+  runtime_key ~name:"allocation-policy"
+    (Fmt.str
+       {|
+    let allocation_policy, allocation_policy_doc_alts =
+      let enum =
+        [
+          ("next-fit", `Next_fit);
+          ("first-fit", `First_fit);
+          ("best-fit", `Best_fit);
+        ]
+      in
+      (Cmdliner.Arg.enum enum, Cmdliner.Arg.doc_alts_enum enum)
+    in
+    let doc =
+      Printf.sprintf
+        "The policy used for allocating in the OCaml heap. Possible values are: \
+         %%s. Best-fit is only supported since OCaml 4.10."
+        allocation_policy_doc_alts
+    in
+    let doc =
+      Cmdliner.Arg.info ~docs:%S ~docv:"ALLOCATION" ~doc [ "allocation-policy" ]
+    in
+    Cmdliner.Arg.(value & opt allocation_policy `Best_fit doc)|}
+       ocaml_section)
+
+let minor_heap_size =
+  runtime_key ~name:"minor-heap-size"
+    (Fmt.str
+       {|
+  let doc = "The size of the minor heap (in words). Default: 256k." in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"MINOR SIZE" ~doc [ "minor-heap-size" ]
+  in
+  Cmdliner.Arg.(value & opt (some int) None doc)|}
+       ocaml_section)
+
+let major_heap_increment =
+  runtime_key ~name:"major-heap-increment"
+    (Fmt.str
+       {|
+  let doc =
+    "The size increment for the major heap (in words). If less than or equal \
+     1000, it is a percentage of the current heap size. If more than 1000, it \
+     is a fixed number of words. Default: 15."
+  in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"MAJOR INCREMENT" ~doc
+      [ "major-heap-increment" ]
+  in
+  Cmdliner.Arg.(value & opt (some int) None doc)|}
+       ocaml_section)
+
+let space_overhead =
+  runtime_key ~name:"space-overhead"
+    (Fmt.str
+       {|
+  let doc =
+    "The percentage of live data of wasted memory, due to GC does not \
+     immediately collect unreachable blocks. The major GC speed is computed \
+     from this parameter, it will work more if smaller. Default: 80."
+  in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"SPACE OVERHEAD" ~doc
+      [ "space-overhead" ]
+  in
+  Cmdliner.Arg.(value & opt (some int) None doc)|}
+       ocaml_section)
+
+let max_space_overhead =
+  runtime_key ~name:"max-space-overhead"
+    (Fmt.str
+       {|
+  let doc =
+    "Heap compaction is triggered when the estimated amount of wasted memory \
+     exceeds this (percentage of live data). If above 1000000, compaction is \
+     never triggered. Default: 500."
+  in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"MAX SPACE OVERHEAD" ~doc
+      [ "max-space-overhead" ]
+  in
+  Cmdliner.Arg.(value & opt (some int) None doc)|}
+       ocaml_section)
+
+let gc_verbosity =
+  runtime_key ~name:"gc-verbosity"
+    (Fmt.str
+       {|
+  let doc =
+    "GC messages on standard error output. Sum of flags. Check GC module \
+     documentation for details."
+  in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"VERBOSITY" ~doc [ "gc-verbosity" ]
+  in
+  Cmdliner.Arg.(value & opt (some int) None doc)|}
+       ocaml_section)
+
+let gc_window_size =
+  runtime_key ~name:"gc-window-size"
+    (Fmt.str
+       {|
+  let doc =
+    "The size of the window used by the major GC for smoothing out variations \
+     in its workload. Between 1 adn 50, default: 1."
+  in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"WINDOW SIZE" ~doc [ "gc-window-size" ]
+  in
+  Cmdliner.Arg.(value & opt (some int) None doc)|}
+       ocaml_section)
+
+let custom_major_ratio =
+  runtime_key ~name:"custom-major-ratio"
+    (Fmt.str
+       {|
+  let doc =
+    "Target ratio of floating garbage to major heap size for out-of-heap \
+     memory held by custom values. Default: 44."
+  in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"CUSTOM MAJOR RATIO" ~doc
+      [ "custom-major-ratio" ]
+  in
+  Cmdliner.Arg.(value & opt (some int) None doc)|}
+       ocaml_section)
+
+let custom_minor_ratio =
+  runtime_key ~name:"custom-minor-ratio"
+    (Fmt.str
+       {|
+  let doc =
+    "Bound on floating garbage for out-of-heap memory held by custom values in \
+     the minor heap. Default: 100."
+  in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"CUSTOM MINOR RATIO" ~doc
+      [ "custom-minor-ratio" ]
+  in
+  Cmdliner.Arg.(value & opt (some int) None doc)|}
+       ocaml_section)
+
+let custom_minor_max_size =
+  runtime_key ~name:"custom-minor-max-size"
+    (Fmt.str
+       {|
+  let doc =
+    "Maximum amount of out-of-heap memory for each custom value allocated in \
+     the minor heap. Default: 8192 bytes."
+  in
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~docv:"CUSTOM MINOR MAX SIZE" ~doc
+      [ "custom-minor-max-size" ]
+  in
+  Cmdliner.Arg.(value & opt (some int) None doc)|}
+       ocaml_section)
+
+let logs =
+  runtime_key ~name:"logs"
+    (Fmt.str
+       {|
+  let logs = Cmdliner.Arg.list Mirage_runtime.Conv.log_threshold in
+  let doc =
+    "Be more or less verbose. $(docv) must be of the form \
+     $(b,*:info,foo:debug) means that that the log threshold is set to \
+     $(b,info) for every log sources but the $(b,foo) which is set to \
+     $(b,debug)."
+  in
+  let doc = Cmdliner.Arg.info ~docv:"LEVEL" ~doc ~docs:%S [ "l"; "logs" ] in
+  Cmdliner.Arg.(value & opt logs [] doc)|}
+       unikernel_section)
+
+(** {3 Initial delay} *)
+
+let delay =
+  runtime_key ~name:"delay"
+    (Fmt.str
+       {|
+  let doc =
+    Cmdliner.Arg.info ~docs:%S ~doc:"Delay n seconds before starting up"
+      [ "delay" ]
+  in
+  Cmdliner.Arg.(value & opt int 0 doc)|}
+       unikernel_section)
 
 let runtime_network_key ~name fmt =
   Fmt.kstr
     (Runtime_key.create ~name
        ~packages:[ package "mirage-runtime" ~sublibs:[ "network" ] ])
     ("(Mirage_runtime_network." ^^ fmt ^^ ")")
-
-let delay = runtime_key "delay"
-let backtrace = runtime_key "backtrace"
-let randomize_hashtables = runtime_key "randomize_hashtables"
-let allocation_policy = runtime_key "allocation_policy"
-let minor_heap_size = runtime_key "minor_heap_size"
-let major_heap_increment = runtime_key "major_heap_increment"
-let space_overhead = runtime_key "space_overhead"
-let max_space_overhead = runtime_key "max_space_overhead"
-let gc_verbosity = runtime_key "gc_verbosity"
-let gc_window_size = runtime_key "gc_window_size"
-let custom_major_ratio = runtime_key "custom_major_ratio"
-let custom_minor_ratio = runtime_key "custom_minor_ratio"
-let custom_minor_max_size = runtime_key "custom_minor_max_size"
 
 let pp_group ppf = function
   | None | Some "" -> ()
@@ -136,4 +334,24 @@ let syslog_port default =
 let syslog_hostname default =
   runtime_keyf ~name:"syslog_hostname" "syslog_hostname %S" default
 
-let logs = runtime_key "logs"
+(*
+(** {3 Blocks} *)
+
+let disk =
+  let doc =
+    Arg.info ~docs:unikernel_section
+      ~doc:
+        "Name of the docteur disk (for Solo5 targets, the name must contains \
+         only alpanumeric characters)."
+      [ "disk" ]
+  in
+  Arg.(value & opt string "disk" doc)
+
+let analyze =
+  let doc =
+    Arg.info ~docs:unikernel_section
+      ~doc:"Analyze at the boot time the given docteur disk." [ "analyze" ]
+  in
+  Arg.(value & opt bool true doc)
+
+  *)

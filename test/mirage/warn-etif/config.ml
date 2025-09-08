521a8824552bbd7f68dc8d9fa0dd792dd7aa60be
Hannes Mehnert <hannes@mehnert.org>
2025-09-08 15:18:57 +0200

    warn-etif: fix test by re-adding the type ipv4_config

--- test/mirage/warn-etif/config.ml
+++ test/mirage/warn-etif/config.ml
@@ -11,6 +11,11 @@ end
 let rec eth = etif default_network
 let main = main "App.Make" ~pos:__POS__ (ethernet @-> job)
 
+type ipv4_config = {
+  network : Ipaddr.V4.Prefix.t;
+  gateway : Ipaddr.V4.t option;
+}
+
 let () =
   let ramdisk (conf : ipv4_config) =
     match conf with { network } -> ramdisk "secrets\f42"
